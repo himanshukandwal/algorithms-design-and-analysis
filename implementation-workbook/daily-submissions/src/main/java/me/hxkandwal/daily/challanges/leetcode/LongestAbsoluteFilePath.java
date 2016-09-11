fdef5f0427e0860084f9aa60a660d42692ae4a30
Himanshu Kandwal <himanshuk.19@gmail.com>
2016-09-11 02:12:12 -0500

    added daily submission codes

--- implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/LongestAbsoluteFilePath.java
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/LongestAbsoluteFilePath.java
@@ -1,10 +1,9 @@
 package me.hxkandwal.daily.challanges.leetcode;
 
-import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
 
@@ -13,26 +12,39 @@ import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
  * 
  * Suppose we abstract our file system by a string in the following manner:
  * 
- * The string "dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext" represents:
- * 
- * dir subdir1 subdir2 file.ext
- * 
- * The directory dir contains an empty sub-directory subdir1 and a sub-directory
- * subdir2 containing a file file.ext.
- * 
  * The string
- * "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"
+ *  
+ * 		"dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext"
+ *  
  * represents:
  * 
- * dir subdir1 file1.ext subsubdir1 subdir2 subsubdir2 file2.ext
- *
- * The directory dir contains two sub-directories subdir1 and subdir2. subdir1
- * contains a file file1.ext and an empty second-level sub-directory subsubdir1.
- * subdir2 contains a second-level sub-directory subsubdir2 containing a file
- * file2.ext.
+ * dir 
+ * 		subdir1 
+ * 		subdir2 
+ * 			file.ext
  * 
- * We are interested in finding the longest (number of characters) absolute path
- * to a file within our file system.
+ * The directory dir contains an empty sub-directory subdir1 and a sub-directory subdir2 containing a file file.ext.
+ * 
+ * The string
+ * 
+ * 		"dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"
+ * 
+ * represents:
+ * 
+ * dir 
+ * 		subdir1 
+ * 			file1.ext 
+ * 			subsubdir1 
+ *		subdir2 
+ *			subsubdir2 
+ *				file2.ext
+ *
+ * The directory dir contains two sub-directories subdir1 and subdir2. subdir1 contains a file file1.ext and an empty second-level 
+ * sub-directory subsubdir1. 
+ * 
+ * subdir2 contains a second-level sub-directory subsubdir2 containing a file file2.ext.
+ * 
+ * We are interested in finding the longest (number of characters) absolute path to a file within our file system.
  * 
  * For example, in the second example above, the longest absolute path is "dir/subdir2/subsubdir2/file2.ext", and its length is 32
  *  
@@ -45,37 +57,159 @@ public class LongestAbsoluteFilePath extends AbstractCustomTestRunner {
 	
 	private static LongestAbsoluteFilePath _instance = new LongestAbsoluteFilePath();
 	
-	private Map<Integer, Integer> levelMaxMap = new HashMap<>();
-	
-	private static int index = 0;
-	
 	private LongestAbsoluteFilePath() {}
-
-	public int lengthLongestPath(String input) {
-		Pattern pattern = Pattern.compile("\\n");
-		Matcher matcher = pattern.matcher(input);
+	
+	/**
+	 * Notes : use index only while working with big strings.
+	 * 		   process information required while building the tree (avoid re-recursion for process only)
+	 * 		   if have to recurse, make it one pass.
+	 * 
+	 * Other approaches (avoided for too much string processing)
+	 * 	- substrings
+	 * 	- stringTokenizer
+	 * 	- Regex processing
+	 * 
+	 * @author Hxkandwal
+	 */
+	// data-structure to store the information
+	public static class DirectoryTreeNode {
+		public DirectoryTreeNode parent;
+		public String name;
+		public List<DirectoryTreeNode> children = new ArrayList<>();
+		public int depth;
+		public int maxArmLength;	// this is max of children maxArmLength + name.length()
 		
-		if (matcher.find()) {
-			levelMaxMap.put(0, matcher.end() - matcher.start());
-			index = matcher.end();
-			return lengthLongestPathInner(0, matcher);
+		public DirectoryTreeNode(String name, int depth) { 
+			this.name = name;
+			this.depth = depth;
 		}
 		
-		return input.length();
+		public DirectoryTreeNode(String name) { 
+			this(name, 0);
+		}
+		
+		@Override
+		public String toString() { return this.name; }
+	}
+
+	public int _lengthLongestPath(String input) {
+		if (input == null || input.length() == 0)
+			return 0;
+	
+		int idx = 0;
+		int maxLength = 0;
+		
+		while (idx < input.length()) {
+			boolean isFile = false;
+		
+			StringBuilder sb = new StringBuilder();
+			for (; idx < input.length(); idx ++) {
+				char ch = input.charAt(idx) ;
+				if (ch == '\n')
+					break;
+				
+				sb.append(ch);
+				
+				if (ch == '.')
+					isFile = true;
+			}
+			
+			DirectoryTreeNode root = new DirectoryTreeNode(sb.toString());
+			
+			int localMaxLength = 0;
+			if (!isFile) {
+				idx = generateDirectoryTreeRecursively (input, idx + 1, root);
+				localMaxLength = root.maxArmLength;
+			} else
+				localMaxLength = sb.length();
+			
+			if (localMaxLength > maxLength)
+				maxLength = localMaxLength;
+		}
+		
+		return maxLength;
 	}
 	
-	public int lengthLongestPathInner(int parentLevel, Matcher matcher) {
-		if (matcher.find()) {
+	// generates the complete directory tree from the string representation.
+	private static int generateDirectoryTreeRecursively(String input, int startIdx, DirectoryTreeNode parentNode) {
+		int depth = 0;
+		int idx = startIdx;
+		StringBuilder sb = new StringBuilder();
+		
+		boolean isFile = false, foundtab = false;
+		int spacecount = 0;
+		for (; idx < input.length(); idx ++) {
+			char ch = input.charAt(idx);
+			if (ch == '\n')
+				break;	
 			
-		} else {
-//			matcher.region(matcher.end(), 
+			if (ch == '\t') {
+				foundtab = true;
+				depth ++;
+			} else if (ch == ' ' && spacecount < 4 &&  !foundtab) {
+				spacecount ++;
+				
+				if (spacecount == 4) 
+					depth ++;
+			} else {
+				sb.append(ch);
+				
+				if (ch == '.')
+					isFile = true;
+			}
 		}
-		return 0;
+		
+		DirectoryTreeNode validParent = parentNode;
+		while (validParent != null && depth != validParent.depth + 1)
+			validParent = validParent.parent;
+		
+		if (validParent != null) {
+			DirectoryTreeNode node = new DirectoryTreeNode(sb.toString(), depth);
+			
+			if (isFile) {
+				int currentLength = sb.length(); 		
+				
+				DirectoryTreeNode parentHierarchyTraverser = validParent;
+				
+				while (parentHierarchyTraverser != null && 
+						parentHierarchyTraverser.maxArmLength < (currentLength + parentHierarchyTraverser.name.length() + 1)) { // + 1 for / in the path.
+					currentLength = parentHierarchyTraverser.maxArmLength = currentLength + parentHierarchyTraverser.name.length() + 1;
+					parentHierarchyTraverser = parentHierarchyTraverser.parent;
+				}
+			}
+			
+			// attach parent to child and child to parent.
+			validParent.children.add(node);
+			node.parent = validParent;
+			
+			if (idx + 1 < input.length())
+				 return generateDirectoryTreeRecursively (input, idx + 1, node);
+		}
+		
+		return startIdx;
 	}
-
-	@Override
-	public Object coreTestRun(Method method, Object[] externalVariables) {	
-		return null;
+	
+	// driver method
+	public static void main(String[] args) {
+		_instance.runTest("", 0);
+		_instance.runTest("a", 0);
+		_instance.runTest("dir\n	file.txt", 12);
+		_instance.runTest("dir\n    file.txt", 12);
+		_instance.runTest("dir\n        file.txt", 16);
+		_instance.runTest("dir\n\t        file.txt\n\tfile2.txt", 20);
+		_instance.runTest("a\n\tb.txt\na2\n\tb2.txt", 9);
+		_instance.runTest("dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext", "dir/subdir2/file.ext".length());
+		_instance.runTest("dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext", 
+				"dir/subdir2/subsubdir2/file2.ext".length());
+	}
+	
+	public void runTest(final String s, final int expectedOutput) {
+		List<Object> answers = runAll(getClass(), new Object[] { s });
+		
+		for (Object answer : answers) 
+			assertThat((Integer) answer).isEqualTo(expectedOutput);
+		
+		System.out.println("ok!");
 	}
 	
 }
