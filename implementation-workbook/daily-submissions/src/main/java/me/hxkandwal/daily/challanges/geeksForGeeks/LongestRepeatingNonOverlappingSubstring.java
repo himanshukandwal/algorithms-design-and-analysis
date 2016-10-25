757d1f931cc34eef33330c1d71a04c8c7145ce12
Himanshu Kandwal <himanshuk.19@gmail.com>
2016-10-25 04:01:34 -0500

    daily submission codes

--- implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/geeksForGeeks/LongestRepeatingNonOverlappingSubstring.java
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/geeksForGeeks/LongestRepeatingNonOverlappingSubstring.java
@@ -39,8 +39,26 @@ public class LongestRepeatingNonOverlappingSubstring extends AbstractCustomTestR
 	private LongestRepeatingNonOverlappingSubstring() {}
 	
 	public static String _longestRepeatedSubstring(String input) {
+		int[][] dp = new int [input.length()][input.length()];
+		int position_index = -1, max_length = 0;
 		
-		return null;
+		for (int i = 0; i < input.length(); i ++) {
+			for (int j = i + 1; j < input.length(); j++) {
+				if (input.charAt(i) == input.charAt(j)) { 
+					dp [i][j] = 1;
+					
+					if ((i - 1) >= 0 && ((j - i) > dp [i - 1][j - 1]))
+						dp[i][j] = dp [i - 1][j - 1] + 1;
+					
+					if (max_length < dp [i][j]) {
+						max_length = dp [i][j];
+						position_index = j;
+					}
+				}
+			}
+		}
+		
+		return input.substring(position_index - max_length + 1, position_index + 1);
 	}
 	
 	// driver method
