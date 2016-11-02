ead9db7d25c274683d42bbac5dd9a53727b887f2
Himanshu Kandwal <himanshuk.19@gmail.com>
2016-11-02 01:21:16 -0500

    daily submission code

---
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/LongestConsecutiveSequence.java
@@ -0,0 +1,82 @@
+package me.hxkandwal.daily.challanges.leetcode;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
+
+/**
+ * 128. Longest Consecutive Sequence
+ * 
+ * Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
+ * 
+ * For example, Given [100, 4, 200, 1, 3, 2],
+ * 
+ * The longest consecutive elements sequence is [1, 2, 3, 4]. Return its length: 4.
+ * 
+ * Your algorithm should run in O(n) complexity.
+ * 
+ * @author Hxkandwal
+ *
+ */
+public class LongestConsecutiveSequence extends AbstractCustomTestRunner {
+	
+	private static LongestConsecutiveSequence _instance = new LongestConsecutiveSequence();
+	
+	private LongestConsecutiveSequence() {}
+	
+	// expand window as much as possible, and find the max-size window, 
+    public static int longestConsecutive(int[] nums) {
+    	if (nums.length == 0)
+    		return 0;
+    	
+    	Set<Integer> set = new HashSet<Integer>();
+    	int max = 1;
+     
+    	// add all
+    	for (int e : nums)
+    		set.add(e);
+     
+    	for (int e : nums) {
+    		int left = e - 1;
+    		int right = e + 1;
+    		int count = 1;
+     
+    		// eat till extreme left
+    		while (set.contains(left)) {
+    			count++;
+    			set.remove(left);
+    			left--;
+    		}
+    		
+    		// eat till extreme right     
+    		while (set.contains(right)) {
+    			count++;
+    			set.remove(right);
+    			right++;
+    		}
+     
+    		max = Math.max(count, max);
+    	}
+     
+    	return max;
+    }
+    
+	// driver method
+	public static void main(String[] args) {
+		_instance.runTest(new int[] { 100, 4, 200, 1, 3, 2 }, 4);
+	}
+	
+	public void runTest(final int[] nums, final int expectedOutput) {
+		List<Object> answers = runAll(getClass(), new Object[] { nums });
+		
+		for (Object answer : answers) 
+			assertThat((Integer) answer).isEqualTo(expectedOutput);
+		
+		System.out.println("ok!");
+	}
+		
+}
