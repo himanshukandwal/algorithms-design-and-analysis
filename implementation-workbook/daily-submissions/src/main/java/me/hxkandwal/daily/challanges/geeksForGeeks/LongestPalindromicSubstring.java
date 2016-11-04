304376ce4fc373dfbb8992cac9284df0f71f165f
Himanshu Kandwal <himanshuk.19@gmail.com>
2016-11-04 15:36:24 -0500

    daily submission codes

--- implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/geeksForGeeks/LongestPalindromicSubstring.java
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/geeksForGeeks/LongestPalindromicSubstring.java
@@ -24,13 +24,49 @@ public class LongestPalindromicSubstring extends AbstractCustomTestRunner {
 	private LongestPalindromicSubstring() {}
 	
 	public static String _longestPalindromicSubstring(String input) {
+		String maxPalindrome = null;
 		
-		return null;
+		for (int idx = 0; idx < input.length(); idx ++) {
+			StringBuilder localPalindromeOdd = new StringBuilder();
+			circulate (input, idx, 0, false, localPalindromeOdd);
+			
+			StringBuilder localPalindromeEven = new StringBuilder();
+			circulate (input, idx, 0, true, localPalindromeEven);
+			
+			if (maxPalindrome == null || maxPalindrome.length() < Math.max(localPalindromeEven.length(), localPalindromeOdd.length()))
+				maxPalindrome = (localPalindromeEven.length() > localPalindromeOdd.length()) ? localPalindromeEven.toString() : localPalindromeOdd.toString();
+		}
+			
+		return maxPalindrome;
 	}
 	
+	private static void circulate (String input, int center, int radius, boolean even, StringBuilder collector) {
+		if (((even && center + radius + 1 < input.length()) || (!even && center + radius < input.length())) && center - radius >= 0) {
+			if (even) {
+				if (input.charAt(center + radius + 1) == input.charAt(center - radius)) {
+					collector.append(String.valueOf(input.charAt(center + radius + 1)));
+					collector.insert(0, String.valueOf(input.charAt(center - radius)));
+					
+					circulate(input, center, radius + 1, even, collector);
+				}
+			} else {
+				if (input.charAt(center + radius) == input.charAt(center - radius)) {
+					collector.append(String.valueOf(input.charAt(center + radius)));
+					if (center + radius != center - radius) 
+						collector.insert(0, String.valueOf(input.charAt(center - radius)));
+					
+					circulate(input, center, radius + 1, even, collector);
+				}
+			}
+		}
+	}
+
 	// driver method
     public static void main(String[] args) throws FileNotFoundException {
     	_instance.runTest("forgeeksskeegfor", "geeksskeeg");	
+    	_instance.runTest("sabbaerd", "abba");	
+    	_instance.runTest("sabaerd", "aba");
+    	_instance.runTest("ayaxzfbjbkrxiri", "aya");	
     }
 
 	public void runTest(final String input, final String expectedOutput) {
