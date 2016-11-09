01348362fb3f177ceba273b851197ab321877305
Himanshu Kandwal <himanshuk.19@gmail.com>
2016-11-09 00:42:59 -0600

    daily submission codes

---
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/hackerrank/RichieRich.java
@@ -0,0 +1,13 @@
+package me.hxkandwal.daily.challanges.hackerrank;
+
+import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
+
+/**
+ * Richie Rich
+ * 
+ * @author Hxkandwal
+ *
+ */
+public class RichieRich extends AbstractCustomTestRunner {
+
+}
