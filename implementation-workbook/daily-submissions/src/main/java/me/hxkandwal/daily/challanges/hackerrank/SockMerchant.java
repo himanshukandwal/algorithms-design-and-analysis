6fccdfcd4f2c2543c3f769f915c34fb75dbedf3c
Himanshu Kandwal <himanshuk.19@gmail.com>
2016-11-10 19:48:17 -0600

    daily submission codes

---
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/hackerrank/SockMerchant.java
@@ -0,0 +1,7 @@
+package me.hxkandwal.daily.challanges.hackerrank;
+
+import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
+
+public class SockMerchant extends AbstractCustomTestRunner {
+
+}
