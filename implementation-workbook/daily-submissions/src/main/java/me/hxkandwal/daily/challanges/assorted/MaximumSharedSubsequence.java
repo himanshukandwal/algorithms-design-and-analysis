d093fdb2d1e30e0eb9882e36bf3f409f8be1cc55
Himanshu Kandwal <himanshuk.19@gmail.com>
2016-11-12 04:46:55 -0600

    daily submission codes

--- implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/assorted/MaximumSharedSubsequence.java
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/assorted/MaximumSharedSubsequence.java
@@ -1,5 +1,9 @@
 package me.hxkandwal.daily.challanges.assorted;
 
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.List;
+
 import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
 
 /**
@@ -20,5 +24,48 @@ public class MaximumSharedSubsequence extends AbstractCustomTestRunner {
 	
 	public MaximumSharedSubsequence() {}
 
+	public static int[] _getMaximalSharedSubsequence(int[] a, int[] b) {
+		int[][] dp = new int[a.length + 1][b.length + 1];
+		
+		// dp build up.
+		for (int row = 0; row < a.length; row ++)
+			for (int col = 0; col < b.length; col ++)
+				if (a [row] == b [col] && dp [row + 1][col] == dp [row][col + 1])
+					dp [row + 1][col + 1] = Math.max(dp [row + 1][col], dp [row][col + 1]) + 1;
+				else
+					dp [row + 1][col + 1] = Math.max(dp [row + 1][col], dp [row][col + 1]);
+		
+		// result is stored at the very corner of the matrix
+		int idx = dp[a.length][b.length] - 1, row = a.length, col = b.length;
+		int[] result = new int [idx + 1];
+		
+		// result retrieval
+		while (idx >= 0) {
+			if (dp [row][col] > dp [row - 1][col] && dp [row][col] > dp [row][col - 1])
+				result [idx --] = a [row -- - 1];
+			else {
+				while (dp [row][col] == dp [row][col - 1])
+					col --;
+				
+				while (dp [row][col] == dp [row - 1][col])
+					row --;
+			}	
+		}
+		
+		return result;
+	}
 	
+	// driver method
+	public static void main(String[] args) {
+		_instance.runTest(new int[] { 3, 2, 8, 2, 3, 9, 4, 3, 9 }, new int[] { 1, 3, 2, 3, 7, 9 }, new int[] { 3, 2, 3, 9 });
+	}
+
+	public void runTest(final int [] a, final int [] b, final int [] expectedOutput) {
+		List<Object> answers = runAll(getClass(), new Object[] { a, b });
+
+		for (Object answer : answers)
+			assertThat((int []) answer).isEqualTo(expectedOutput);
+
+		System.out.println("ok!");
+	}	
 }
