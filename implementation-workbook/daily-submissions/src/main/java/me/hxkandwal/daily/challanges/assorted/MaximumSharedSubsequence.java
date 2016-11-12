8b8b3bcb63160d351d46f435ac476b350db72e2e
Himanshu Kandwal <himanshuk.19@gmail.com>
2016-11-12 04:00:56 -0600

    daily submission codes

---
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/assorted/MaximumSharedSubsequence.java
@@ -0,0 +1,24 @@
+package me.hxkandwal.daily.challanges.assorted;
+
+import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
+
+/**
+ * Given two sequences, the shared subsequence is a sequence that is a subsequence of both sequences.
+ * 
+ * A maximum-size shared subsequence is a shared subsequence of maximum size.
+ * 
+ * For example, the maximum-size shared subsequence of (3,2,8,2,3,9,4,3,9) and (1,3,2,3,7,9) is (3,2,3,9).
+ * 
+ * link: http://www.cs.ucr.edu/~neal/2004/cs141/index.cgi?ClassS04CS141/Hwk4
+ * 
+ * @author Hxkandwal
+ *
+ */
+public class MaximumSharedSubsequence extends AbstractCustomTestRunner {
+	
+	private static MaximumSharedSubsequence _instance = new MaximumSharedSubsequence();
+	
+	public MaximumSharedSubsequence() {}
+
+	
+}
