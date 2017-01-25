bfd2512be5e35ec2da8d07f62de5767e39e3b726
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-01-25 16:15:12 -0600

    daily submission codes

--- implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/FindAllAnagramsInAString.java
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/FindAllAnagramsInAString.java
@@ -74,6 +74,38 @@ public class FindAllAnagramsInAString extends AbstractCustomTestRunner {
 		return result;
     }
 	
+	public List<Integer> _findAnagrams2(String s, String p) {
+	    List<Integer> list = new ArrayList<>();
+	    
+	    if (s == null || s.length() == 0 || p == null || p.length() == 0) return list;
+	    
+	    int[] hash = new int[256]; //character hash
+	    
+	    //record each character in p to hash
+	    for (char c : p.toCharArray()) hash[c]++;
+	    
+	    //two points, initialize count to p's length
+	    int left = 0, right = 0, count = p.length();
+	    
+	    while (right < s.length()) {
+	        //move right everytime, if the character exists in p's hash, decrease the count
+	        //current hash value >= 1 means the character is existing in p
+	        if (hash[s.charAt(right++)]-- >= 1) count--; 
+	        
+	        //when the count is down to 0, means we found the right anagram
+	        //then add window's left to result list
+	        if (count == 0) list.add(left);
+	    
+	        //if we find the window's size equals to p, then we have to move left (narrow the window) to find the new match window
+	        //++ to reset the hash because we kicked out the left
+	        //only increase the count if the character is in p
+	        //the count >= 0 indicate it was original in the hash, cuz it won't go below 0
+	        if (right - left == p.length() && hash[s.charAt(left++)]++ >= 0) count++;
+	    }
+	    
+	    return list;
+	}
+	
 	private static boolean isEqual(int[] pattern, int[] detected) {
 		for (int idx = 0; idx < detected.length; idx ++) 
 			if (pattern [idx] != detected [idx])
@@ -84,8 +116,8 @@ public class FindAllAnagramsInAString extends AbstractCustomTestRunner {
 	
 	// driver method
 	public static void main(String[] args) {
-//		_instance.runTest("cbaebabacd", "abc", new int[] { 0, 6 });
-//		_instance.runTest("abab", "ab", new int[] { 0, 1, 2 });
+		_instance.runTest("cbaebabacd", "abc", new int[] { 0, 6 });
+		_instance.runTest("abab", "ab", new int[] { 0, 1, 2 });
 		_instance.runTest("abacbabc", "abc", new int[] { 1, 2, 3, 5 });
 	}
 	
