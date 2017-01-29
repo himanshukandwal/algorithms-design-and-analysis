9d6c1aa0c55ba037d116b449940ff4f218a42db2
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-01-29 11:54:30 -0600

    daily submission codes

--- implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/assorted/FlattenMultiLevelLinkedList.java
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/assorted/FlattenMultiLevelLinkedList.java
@@ -1,5 +1,9 @@
 package me.hxkandwal.daily.challanges.assorted;
 
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.List;
+
 import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
 
 /**
@@ -14,7 +18,7 @@ import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
  *                      |
  *                     K---L--NULL
  *                     
- * should become A-B-C-G-H-K-L-I-J-D-E-F-NULL
+ * should become A-B-C-G-H-K-L-I-J-D-E-F
  *
  * link: https://discuss.leetcode.com/topic/51658/flatten-a-multi-level-linked-list
  * 
@@ -64,12 +68,20 @@ public class FlattenMultiLevelLinkedList extends AbstractCustomTestRunner {
 			this.down = down;
 		}
 		
+		@Override
+		public String toString() {
+			return new StringBuilder("[").append(getValue()).append("]").toString();
+		}
+		
 	}
 	
 	public static String _flattenedList(DualDirectedLinkedList head) {
-		StringBuilder answer = new StringBuilder();
+		if (head == null)
+			return null;
 		
-		return null;
+		StringBuilder answer = new StringBuilder();
+		innerRecursion(head, answer);
+		return answer.toString();
 	}
 	
 	private static void innerRecursion(DualDirectedLinkedList node, StringBuilder answer) {
@@ -85,7 +97,42 @@ public class FlattenMultiLevelLinkedList extends AbstractCustomTestRunner {
 				innerRecursion(node.next.down, answer);
 				answer.append("-");
 			}
+			
+			node = node.next;
 		}
 	}
-
+	
+	// driver method
+	public static void main(String[] args) {
+		_instance.runTest(null, null);
+		
+		// Dual-directed Linked list construction.
+		DualDirectedLinkedList head = new DualDirectedLinkedList("A");
+		DualDirectedLinkedList builder = head;
+		builder = builder.next = new DualDirectedLinkedList("B");
+		builder = builder.next = new DualDirectedLinkedList("C");
+		builder = builder.next = new DualDirectedLinkedList("D");
+		DualDirectedLinkedList secondBuilder = builder;
+		builder = builder.next = new DualDirectedLinkedList("E");
+		builder = builder.next = new DualDirectedLinkedList("F");
+		secondBuilder = secondBuilder.down = new DualDirectedLinkedList("G");
+		secondBuilder = secondBuilder.next = new DualDirectedLinkedList("H");
+		secondBuilder = secondBuilder.next = new DualDirectedLinkedList("I");
+		DualDirectedLinkedList thirdBuilder = secondBuilder;
+		secondBuilder = secondBuilder.next = new DualDirectedLinkedList("J");
+		thirdBuilder = thirdBuilder.down = new DualDirectedLinkedList("K");
+		thirdBuilder = thirdBuilder.next = new DualDirectedLinkedList("L");
+		
+		_instance.runTest(head, "A-B-C-G-H-K-L-I-J-D-E-F");
+	}
+	
+	public void runTest(final DualDirectedLinkedList head, final String expectedOutput) {
+		List<Object> answers = runAll(getClass(), new Object[] { head });
+		
+		for (Object answer : answers)
+			assertThat((String) answer).isEqualTo(expectedOutput);
+		
+		System.out.println("ok!");
+	}
+	
 }
