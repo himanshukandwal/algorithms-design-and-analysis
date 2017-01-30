cfa321e30486fed40e1ad90d2004bf36beb86bb7
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-01-30 00:13:50 -0600

    daily submission codes

---
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/assorted/KeyboardWordPrediction.java
@@ -0,0 +1,23 @@
+package me.hxkandwal.daily.challanges.assorted;
+
+import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
+
+/**
+ * Keyboard Word Prediction
+ * 
+ * While typing text on our phone, we will be displayed with next possible word.
+ * 
+ * link: https://discuss.leetcode.com/topic/40443/how-do-you-implement-the-keyboard-word-prediction
+ * 
+ * @author Hxkandwal
+ *
+ */
+public class KeyboardWordPrediction extends AbstractCustomTestRunner {
+	
+	private static KeyboardWordPrediction _instance = new KeyboardWordPrediction();
+	
+	private KeyboardWordPrediction() {}
+	
+	
+
+}
