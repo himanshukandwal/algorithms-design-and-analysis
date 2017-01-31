4295486201470ba32b96a019fd517b63eced23bd
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-01-31 15:26:33 -0600

    daily submission codes

--- implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/assorted/tree/TreeUtilities.java
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/assorted/tree/TreeUtilities.java
@@ -14,29 +14,87 @@ public class TreeUtilities {
 		if (array == null || array.length == 0) 
 			return null;
 		
-		int mid = array.length / 2;
-		
-		BinaryTree root = new BinaryTree(array [mid]); 
-		
-		root.setLeft(innerRecursionGenerateBinarySearchTree (array, 0, mid));
-		root.setRight(innerRecursionGenerateBinarySearchTree (array, mid + 1, array.length - 1));
-		
-		return root;
+		return innerRecursionGenerateBinarySearchTree (array, 0, array.length - 1);
 	}
 	
 	public static String printBinarySearchTreeInorder(BinaryTree root) {
-		
-		return null;
+		StringBuilder output = new StringBuilder();
+		innerRecursionBinarySearchTreeInorder(root, output);
+		return output.toString();
+	}
+
+	public static String printBinarySearchTreePreorder(BinaryTree root) {
+		StringBuilder output = new StringBuilder();
+		innerRecursionBinarySearchTreePreorder(root, output);
+		return output.toString();
+	}
+
+	public static String printBinarySearchTreePostorder(BinaryTree root) {
+		StringBuilder output = new StringBuilder();
+		innerRecursionBinarySearchTreePostorder(root, output);
+		return output.toString();
 	}
 	
 	private static BinaryTree innerRecursionGenerateBinarySearchTree(int[] array, int start, int end) {
 		int mid = (start + end) / 2;
 		
-		BinaryTree node = new BinaryTree(array [mid]); 
-		node.setLeft(innerRecursionGenerateBinarySearchTree (array, start, mid));
-		node.setRight(innerRecursionGenerateBinarySearchTree (array, mid + 1, end));
+		BinaryTree node = new BinaryTree(array [mid]);
+		
+		if (mid > start)
+			node.setLeft(innerRecursionGenerateBinarySearchTree (array, start, mid - 1));
+		
+		if (mid < end)
+			node.setRight(innerRecursionGenerateBinarySearchTree (array, mid + 1, end));
 		
 		return node;
+	}
+	
+	private static void innerRecursionBinarySearchTreeInorder(BinaryTree node, StringBuilder output) {
+		if (node != null) {
+			if (node.getLeft() != null) {
+				innerRecursionBinarySearchTreeInorder(node.getLeft(), output);
+				output.append(",");
+			}
+			
+			output.append(node.getValue());
+			
+			if (node.getRight() != null) {
+				output.append(",");
+				innerRecursionBinarySearchTreeInorder(node.getRight(), output);
+			}
+		}
+	}
+	
+	private static void innerRecursionBinarySearchTreePreorder(BinaryTree node, StringBuilder output) {
+		if (node != null) {
+			output.append(node.getValue());
+			
+			if (node.getLeft() != null) {
+				output.append(",");
+				innerRecursionBinarySearchTreePreorder(node.getLeft(), output);
+			}
+			
+			if (node.getRight() != null) {
+				output.append(",");
+				innerRecursionBinarySearchTreePreorder(node.getRight(), output);
+			}
+		}
+	}
+	
+	private static void innerRecursionBinarySearchTreePostorder(BinaryTree node, StringBuilder output) {
+		if (node != null) {
+			if (node.getLeft() != null) {
+				innerRecursionBinarySearchTreePostorder(node.getLeft(), output);
+				output.append(",");
+			}
+			
+			if (node.getRight() != null) {
+				innerRecursionBinarySearchTreePostorder(node.getRight(), output);
+				output.append(",");
+			}
+			
+			output.append(node.getValue());
+		}
 	}
 	
 }
