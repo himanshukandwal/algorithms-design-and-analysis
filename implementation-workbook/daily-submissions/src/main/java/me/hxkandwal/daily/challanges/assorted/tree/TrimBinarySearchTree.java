b2e432fca4ed1a9ce8650deaa61879ff42b33152
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-01-31 16:12:36 -0600

    daily submission codes

--- implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/assorted/tree/TrimBinarySearchTree.java
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/assorted/tree/TrimBinarySearchTree.java
@@ -1,13 +1,13 @@
 package me.hxkandwal.daily.challanges.assorted.tree;
 
 import static com.google.common.truth.Truth.assertThat;
-import static me.hxkandwal.daily.challanges.Utilities.makeBST;
+import static me.hxkandwal.daily.challanges.assorted.tree.TreeUtilities.generateBinarySearchTree;
+import static me.hxkandwal.daily.challanges.assorted.tree.TreeUtilities.printBinarySearchTreeInorder;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.List;
 
 import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
+import me.hxkandwal.daily.challanges.assorted.tree.model.BinaryTree;
 
 /**
  * Trim Binary Search Tree
@@ -27,35 +27,47 @@ public class TrimBinarySearchTree extends AbstractCustomTestRunner {
 	private static TrimBinarySearchTree _instance = new TrimBinarySearchTree();
 	
 	private TrimBinarySearchTree() {}
-			
-	
-	// driver method
-	public static void main(String[] args) {
-		_instance.runTest(null, null);
-		_instance.runTest(new int[] { 25, 12, 30, 10, 36, 15 }, "10,12,25,30,36,15");
+		
+	public static String _trim(BinaryTree root, int min, int max) {
+		if (root == null)
+			return null;
+		return printBinarySearchTreeInorder(innerRecursiveTrimmingInPostorder(root, min, max));
 	}
 	
-	public void runTest(final int[] array, final String expectedOutput) {
-		List<Object> answers = runAll(getClass(), new Object[] { array });
+	private static BinaryTree innerRecursiveTrimmingInPostorder(BinaryTree node, int min, int max) {
+		if (node.getLeft() != null) {
+			node.setLeft(innerRecursiveTrimmingInPostorder(node.getLeft(), min, max));
+		}
+		
+		if (node.getRight() != null) {
+			node.setRight(innerRecursiveTrimmingInPostorder(node.getRight(), min, max));
+		}
+		
+		if (node.getValue() < min || node.getValue() > max)
+			return (node.getLeft() != null ? node.getLeft() : (node.getRight() != null ? node.getRight() : null));
+		
+		return node;
+	}
+
+	// driver method
+	public static void main(String[] args) {
+		_instance.runTest(null, -100, 100, null);
+		_instance.runTest(generateBinarySearchTree(new int[] { 1, 2, 3, 4, 5, 6, 7 }), -100, 100, "1,2,3,4,5,6,7");
+		_instance.runTest(generateBinarySearchTree(new int[] { 1, 2, 3, 4, 5, 6, 7 }), 0, 100, "1,2,3,4,5,6,7");
+		_instance.runTest(generateBinarySearchTree(new int[] { 1, 2, 3, 4, 5, 6, 7 }), -100, 0, "");
+		_instance.runTest(generateBinarySearchTree(new int[] { 1, 2, 3, 4, 5, 6, 7 }), 2, 100, "2,3,4,5,6,7");
+		_instance.runTest(generateBinarySearchTree(new int[] { 1, 2, 3, 4, 5, 6, 7 }), 0, 6, "1,2,3,4,5,6");
+		_instance.runTest(generateBinarySearchTree(new int[] { 1, 2, 3, 4, 5, 6, 7 }), 3, 5, "3,4,5");
+		_instance.runTest(generateBinarySearchTree(new int[] { 1, 2, 3, 4, 5, 6, 7 }), 3, 3, "3");
+	}
+	
+	public void runTest(final BinaryTree root, final int min, final int max, final String expectedOutput) {
+		List<Object> answers = runAll(getClass(), new Object[] { root, min, max });
 		
 		for (Object answer : answers)
 			assertThat((String) answer).isEqualTo(expectedOutput);
 		
 		System.out.println("ok!");
-	}
-
-	@Override
-	public Object coreTestRun(Method method, Object[] externalVariables) {
-		int[] array = (int[]) externalVariables[0];
-		
-		if (array == null || array.length == 0)
-			return null;
-		
-		try {
-			return method.invoke(_instance, new Object[] { makeBST(array) });
-		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-			return null;
-		}
-	}		
+	}	
 
 }
