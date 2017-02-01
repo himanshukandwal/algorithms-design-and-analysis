2e147af63a6c9f702119b27e1039a823929173fa
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-01-31 18:55:26 -0600

    daily submission codes

--- implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/assorted/TargetSum.java
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/assorted/TargetSum.java
@@ -1,33 +1,49 @@
 package me.hxkandwal.daily.challanges.assorted;
 
-import java.util.Arrays;
+import static com.google.common.truth.Truth.assertThat;
 
-public class TargetSum {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-	// make target sum from array, O(n)
-	public static void main(String[] args) {
-		
-	}
+import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
+
+public class TargetSum extends AbstractCustomTestRunner {
 	
-	public static boolean checkPossible(int[] batteryOne, int[] batteryTwo, int sum) {
-		Arrays.sort(batteryOne);
-		Arrays.sort(batteryTwo);
-
-		int len1 = batteryOne.length;
-		int len2 = batteryTwo.length;
-		
-		if (batteryOne [len1 - 1] + batteryTwo [len2 - 1] < sum)
-			return false;
-
-		int start = 0;
-		int end = len1 -1;
-		
-		while (start < end) {
-			int mid = (start + end) >>> 1;
+	private static TargetSum _instance = new TargetSum();
+	
+	private TargetSum() {}
+	
+	public static boolean _checkPossible(int[] batteryOne, int[] batteryTwo, int sum) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int idx = 0; idx < batteryOne.length; idx ++) {
+			int diff = sum - batteryOne [idx];
 			
+			if (!map.containsKey(diff)) 
+				map.put(diff, batteryOne [idx]);
 		}
 		
+		for (int idx = 0; idx < batteryTwo.length; idx ++)
+			if (map.containsKey(batteryTwo [idx])) 
+				return true;
+		
 		return false;
+	}
+	
+	// driver method
+	public static void main(String[] args) {
+		_instance.runTest(new int[] { 5, 7, 2, 4 }, new int[] { -3, 0, 1 }, 8, true);
+		_instance.runTest(new int[] { 6, 3, 1, 9, 5, 4, 0, 1, -29, 12, 45, 2, 6 }, 
+						  new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 }, 100, false);
+	}
+
+	public void runTest(final int[] batteryOne, final int[] batteryTwo, final int sum, final boolean expectedOutput) {
+		List<Object> answers = runAll(getClass(), new Object[] { batteryOne, batteryTwo, sum });
+
+		for (Object answer : answers)
+			assertThat((Boolean) answer).isEqualTo(expectedOutput);
+		
+		System.out.println("ok!");
 	}
 
 }
