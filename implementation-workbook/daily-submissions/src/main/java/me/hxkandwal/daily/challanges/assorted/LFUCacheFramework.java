dbec3e7fbbe15fbaacfe7725bf74579dad9409f6
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-01-31 21:12:46 -0600

    daily submission codes

---
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/assorted/LFUCacheFramework.java
@@ -0,0 +1,13 @@
+package me.hxkandwal.daily.challanges.assorted;
+
+import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
+
+public class LFUCacheFramework extends AbstractCustomTestRunner {
+	
+	private static LFUCacheFramework _instance = new LFUCacheFramework();
+	
+	private LFUCacheFramework() {}
+	
+	// data-structure to hold the properties of LFU (least-fr) cache
+
+}
