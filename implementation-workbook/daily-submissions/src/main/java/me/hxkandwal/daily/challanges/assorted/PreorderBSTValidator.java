193c656186295b87d46fa03ad9eea07dfcb3913d
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-02-02 20:26:34 -0800

    daily submission codes

---
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/assorted/PreorderBSTValidator.java
@@ -0,0 +1,12 @@
+package me.hxkandwal.daily.challanges.assorted;
+
+import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
+
+/**
+ * 
+ * @author Hxkandwal
+ *
+ */
+public class PreorderBSTValidator extends AbstractCustomTestRunner {
+
+}
