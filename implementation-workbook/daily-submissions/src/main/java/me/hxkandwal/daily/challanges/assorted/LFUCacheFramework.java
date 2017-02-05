a7d89616e8f85ce4da77e89ecde709bb03d8755f
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-02-04 22:21:58 -0800

    daily submission codes

--- implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/assorted/LFUCacheFramework.java
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/assorted/LFUCacheFramework.java
@@ -1,5 +1,8 @@
 package me.hxkandwal.daily.challanges.assorted;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
 
 public class LFUCacheFramework extends AbstractCustomTestRunner {
@@ -9,5 +12,34 @@ public class LFUCacheFramework extends AbstractCustomTestRunner {
 	private LFUCacheFramework() {}
 	
 	// data-structure to hold the properties of LFU (least-frequently used) cache.
+	public class Cache {
+		private int size;
+		private Map<Integer, DataNode> metadata;
+		
+		public Cache(int size) {
+			this.size = size;
+			this.metadata = new HashMap<>(size);
+		}
+		
+		// value holder data structure.
+		public class DataNode {
+			private int data;
+			private int frequency;
+			
+			public DataNode(int data) {
+				this.data = data;
+				this.frequency = 1;
+			}
+			
+			public void increment() {
+				this.frequency ++;
+			}
+			
+			@Override
+			public String toString() {
+				return "(" + this.data + ":" + this.frequency + ")";
+			}
+		}
+	}
 
 }
