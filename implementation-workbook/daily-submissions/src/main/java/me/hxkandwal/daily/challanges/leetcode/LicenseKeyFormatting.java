c50fe84c36003785033e9481c7070a241282223b
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-02-07 21:52:03 -0600

    daily submission codes

--- implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/LicenseKeyFormatting.java
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/LicenseKeyFormatting.java
@@ -1,5 +1,9 @@
 package me.hxkandwal.daily.challanges.leetcode;
 
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.List;
+
 import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
 
 /**
@@ -42,10 +46,58 @@ public class LicenseKeyFormatting extends AbstractCustomTestRunner {
 	
 	private LicenseKeyFormatting() {}
 	
-	public static String licenseKeyFormatting(String s, int k) {
+	public static String _licenseKeyFormatting(String s, int k) {
+		int idx = s.length() - 1, pairLen = 0;
+		StringBuilder answer = new StringBuilder();
 		
+		while (idx >= 0) {
+			char ch = s.charAt(idx);
+			
+			if (pairLen == k) {
+				if (ch == '-') {
+					answer.insert(0, Character.toUpperCase(ch));
+					idx --;
+				} else
+					answer.insert(0, '-');
+				
+				pairLen = 0;
+			} else {
+				if (ch != '-') {
+					answer.insert(0, Character.toUpperCase(ch));
+					pairLen ++;
+				}
+				
+				idx --;
+			}
+		}
 		
-		return null;
+		if (answer.length() > 0 && answer.charAt(0) == '-') answer.deleteCharAt(0);
+		
+		return answer.toString();
     }
 
+	public static String _licenseKeyFormatting2(String s, int k) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = s.length() - 1; i >= 0; i--)
+			if (s.charAt(i) != '-')
+				sb.append(sb.length() % (k + 1) == k ? '-' : "").append(s.charAt(i));
+		return sb.reverse().toString().toUpperCase();
+	}
+	 
+	// driver method
+	public static void main(String[] args) {
+		_instance.runTest("2-4A0r7-4k", 4, "24A0-R74K");
+		_instance.runTest("2-4A0r7-4k", 3, "24-A0R-74K");
+		_instance.runTest("--a-a-a-a--", 2, "AA-AA");
+		_instance.runTest("---", 3, "");
+	}
+
+	public void runTest(final String input, final int k, final String expectedOutput) {
+		List<Object> answers = runAll(getClass(), new Object[] { input, k });
+
+		for (Object answer : answers)
+			assertThat((String) answer).isEqualTo(expectedOutput);
+		
+		System.out.println("ok!");
+	}
 }
