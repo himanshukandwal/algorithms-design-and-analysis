45c1614ae4daf28b772cff684b53d142087b3e09
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-02-07 23:32:44 -0600

    daily submission codes

--- implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/SortCharactersByFrequency.java
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/SortCharactersByFrequency.java
@@ -163,6 +163,37 @@ public class SortCharactersByFrequency extends AbstractCustomTestRunner {
 		return String.valueOf(answer);
 	}
 	
+	// solution using bucket-sort.
+	public String frequencySort2(String s) {
+		if (s.length() < 3) return s;
+		
+		int max = 0;
+		int[] map = new int[256];
+		
+		for (char ch : s.toCharArray()) {
+			map[ch]++;
+			max = Math.max(max, map[ch]);
+		}
+		
+		String[] buckets = new String[max + 1]; // create max buckets
+		
+		for (int i = 0; i < 256; i++) { // join chars in the same bucket
+			String str = buckets[map[i]];
+			if (map[i] > 0)
+				buckets[map[i]] = (str == null) ? "" + (char) i : (str + (char) i);
+		}
+		
+		StringBuilder strb = new StringBuilder();
+		for (int i = max; i >= 0; i--) { // create string for each bucket.
+			if (buckets[i] != null)
+				for (char ch : buckets[i].toCharArray())
+					for (int j = 0; j < i; j++)
+						strb.append(ch);
+		}
+		
+		return strb.toString();
+	}
+	
 	// driver method
 	public static void main(String[] args) {
 		_instance.runTest("tree", "eetr");
