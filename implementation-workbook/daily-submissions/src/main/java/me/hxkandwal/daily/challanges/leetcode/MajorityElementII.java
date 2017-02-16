43872d5e440b83533e84f3b91493aac5274fab64
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-02-15 19:58:30 -0600

    daily submission codes

---
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/MajorityElementII.java
@@ -0,0 +1,28 @@
+package me.hxkandwal.daily.challanges.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
+
+/**
+ * 229. Majority Element II
+ * 
+ * Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times. 
+ * 
+ * The algorithm should run in linear time and in O(1)
+ * 
+ * @author Hxkandwal
+ */
+public class MajorityElementII extends AbstractCustomTestRunner {
+	
+	private static MajorityElementII _instance = new MajorityElementII();
+	
+	private MajorityElementII() {}
+	
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> answer = new ArrayList<>();
+    	
+    	return answer;
+    }
+}
