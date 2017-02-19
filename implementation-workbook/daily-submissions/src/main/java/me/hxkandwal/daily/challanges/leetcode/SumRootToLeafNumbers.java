310c73f02cf3bc65dba81d710c61b1a7687ac8db
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-02-18 18:10:16 -0600

    daily submission codes

--- implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/SumRootToLeafNumbers.java
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/SumRootToLeafNumbers.java
@@ -35,7 +35,19 @@ public class SumRootToLeafNumbers extends AbstractCustomTestRunner {
 		
 		public TreeNode(int x) { val = x; }
 	}
+
+	// better approach, without extra memory.
+	public int sumNumbers(TreeNode root) {
+		return sum(root, 0);
+	}
+
+	public int sum(TreeNode n, int s){
+		if (n == null) return 0;
+		if (n.right == null && n.left == null) return s * 10 + n.val;
+		return sum (n.left, s*10 + n.val) + sum (n.right, s*10 + n.val);
+	}
 	
+	// my take
 	public int _sumNumbers(TreeNode root) {
 		 return count(root, new StringBuilder());
     }
