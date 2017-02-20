64a534984127c2f63e99cb4395670c48bfbe53b9
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-02-20 01:23:43 -0600

    daily submission codes

---
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/LoggerRateLimiter.java
@@ -0,0 +1,65 @@
+package me.hxkandwal.daily.challanges.leetcode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
+
+/**
+ * 359. Logger Rate Limiter
+ * 
+ * Design a logger system that receive stream of messages along with its timestamps, each message should be printed if and only 
+ * if it is not printed in the last 10 seconds.
+ * 
+ * Given a message and a timestamp (in seconds granularity), return true if the message should be printed in the given timestamp, otherwise returns false.
+ * It is possible that several messages arrive roughly at the same time.
+ * 
+ * @author Hxkandwal
+ */
+public class LoggerRateLimiter extends AbstractCustomTestRunner {
+
+	TreeMap<Integer, Set<String>> dict;
+
+	/** Initialize your data structure here. */
+    public LoggerRateLimiter() {
+        dict = new TreeMap<>();
+    }
+
+	/**
+	 * Returns true if the message should be printed in the given timestamp,
+	 * otherwise returns false. If this method returns false, the message will
+	 * not be printed. The timestamp is in seconds granularity.
+	 */
+	public boolean shouldPrintMessage(int timestamp, String message) {
+		boolean status = false;
+		for (Set<String> c : dict.subMap(timestamp - 10, false, timestamp, true).values())
+			if (c.contains(message)) {
+				status = true;
+				break;
+			}
+
+		if (!status) {
+			dict.put(timestamp, dict.getOrDefault(timestamp, new HashSet<>()));
+			dict.get(timestamp).add(message);
+		}
+
+		return !status;
+	}
+    
+}
+
+class Logger {
+
+    private Map<String, Integer> ok = new HashMap<>();
+
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        if (timestamp < ok.getOrDefault(message, 0))
+            return false;
+        ok.put(message, timestamp + 10);
+        return true;
+    }
+}
+
