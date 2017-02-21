68c2f14e93c7147bdf67b0eb033de9f6d45ee19a
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-02-21 12:24:22 -0600

    daily submission codes

--- implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/KthSmallestElementInASortedMatrix.java
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/KthSmallestElementInASortedMatrix.java
@@ -1,5 +1,9 @@
 package me.hxkandwal.daily.challanges.leetcode;
 
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.List;
+
 import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
 
 /**
@@ -25,5 +29,40 @@ import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
  * @author Hxkandwal
  */
 public class KthSmallestElementInASortedMatrix extends AbstractCustomTestRunner {
+	
+	private static KthSmallestElementInASortedMatrix _instance = new KthSmallestElementInASortedMatrix();
+	
+	private KthSmallestElementInASortedMatrix() {}
 
+	public int _kthSmallest(int[][] matrix, int k) {
+        int lo = matrix[0][0], hi = matrix[matrix.length - 1][matrix[0].length - 1] + 1;//[lo, hi]
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            int count = 0,  j = matrix[0].length - 1;
+			for (int i = 0; i < matrix.length; i++) {
+				while (j >= 0 && matrix[i][j] > mid) j--;
+				count += (j + 1);
+			}
+            if (count < k) lo = mid + 1;
+            else hi = mid;
+        }
+        return lo;
+    }
+	
+    
+   	// driver method
+   	public static void main(String[] args) {
+		_instance.runTest(new int [][] { new int[] { 1,  5,  9 },
+										 new int[] { 10, 11, 13 },
+										 new int[] { 12, 13, 15 }}, 8, 13);
+   	}
+
+   	public void runTest(final int[][] matrix, final int k, final int expectedOutput) {
+   		List<Object> answers = runAll(getClass(), new Object[] { matrix, k });
+
+   		for (Object answer : answers)
+   				assertThat((Boolean) answer).isEqualTo(expectedOutput);
+
+   		System.out.println("ok!");
+   	}
 }
