6a07b6ddd6063ff40b16bddeba40dec0bf4ea17d
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-02-23 17:48:30 -0600

    daily submission codes

--- implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/ClosestBinarySearchTreeValueII.java
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/ClosestBinarySearchTreeValueII.java
@@ -3,6 +3,7 @@ package me.hxkandwal.daily.challanges.leetcode;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.PriorityQueue;
+import java.util.Stack;
 
 import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
 
@@ -30,6 +31,62 @@ public class ClosestBinarySearchTreeValueII extends AbstractCustomTestRunner {
 		
 		public TreeNode(int x) { val = x; }
 	}
+	
+	/**
+	 * O (n) solution
+	 * 
+	 * Consider implement these two helper functions:
+	 * 		getPredecessor(N), which returns the next smaller node to N.	
+	 * 		getSuccessor(N), which returns the next larger node to N.
+	 * 
+	 * Try to assume that each node has a parent pointer, it makes the problem much easier.
+	 * Without parent pointer we just need to keep track of the path from the root to the current node using a stack.
+	 * You would need two stacks to track the path in finding predecessor and successor node separately.
+	 */
+	public List<Integer> _closestKValues(TreeNode root, double target, int k) {
+        List<Integer> ans = new ArrayList<>();
+        Stack<TreeNode> s = new Stack<>();
+        Stack<TreeNode> p = new Stack<>();
+        
+        TreeNode t = root;
+        // crawling down (logn)
+        while (t != null) {
+            if (t.val >= target) { s.push (t); t = t.left; }
+            else { p.push (t); t = t.right; }
+        }
+        
+        while (k -- > 0) {
+            if (p.isEmpty() && s.isEmpty()) break;
+            else if (p.isEmpty()) ans.add (getSuccessor(s));
+            else if (s.isEmpty()) ans.add (getPredecessor(p));
+            else {
+                if (Math.abs (p.peek().val - target) > Math.abs (s.peek().val - target))
+                    ans.add (getSuccessor(s));
+                else ans.add (getPredecessor(p));
+            }
+        }
+        return ans;
+    }
+    
+    private Integer getSuccessor (Stack<TreeNode> stk) {
+        TreeNode ret = stk.pop(), t = ret;
+        if (t.right != null) {
+            t = t.right;
+            stk.push (t);
+            while (t.left != null) { t = t.left; stk.push (t); }
+        }
+        return ret.val;														// <<<<<<<<<<<<<<<<<<<<< next nearest would be ret.val
+    }
+    
+    private Integer getPredecessor (Stack<TreeNode> stk) {
+        TreeNode ret = stk.pop(), t = ret;
+        if (t.left != null) {
+            t = t.left;
+            stk.push (t);
+            while (t.right != null) { t = t.right; stk.push (t); }
+        }
+        return ret.val;        												// <<<<<<<<<<<<<<<<<<<<< next nearest would be ret.val
+    }
 	
 	// this is O (nlogn) as full traversal is there and we are making heap operations at-most n times (min K times, best case)
 	public List<Integer> closestKValues(TreeNode root, double target, int k) {
