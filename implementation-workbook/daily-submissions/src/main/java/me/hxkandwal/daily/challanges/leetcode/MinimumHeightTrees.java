d7177aa5f860961ce254d567fa795f00134f833e
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-02-23 11:18:56 -0600

    daily submission codes

--- implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/MinimumHeightTrees.java
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/MinimumHeightTrees.java
@@ -96,7 +96,7 @@ public class MinimumHeightTrees extends AbstractCustomTestRunner {
         }
     }
     
-    public List<Integer> _findMinHeightTrees(int n, int[][] edges) {
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
         List<Integer> ans = new ArrayList<>();
         List<Node> nodes = new ArrayList<>();
         for (int idx = 0; idx < n; idx ++) nodes.add (new Node (idx));
@@ -142,7 +142,7 @@ public class MinimumHeightTrees extends AbstractCustomTestRunner {
     
 	// driver method
 	public static void main(String[] args) {
-		_instance.runTest(4, new int[][] { new int [] { 1, 0 }, new int [] { 1, 2 }, new int [] { 1, 3 } }, new ArrayList() {{ add (1); }});
+//		_instance.runTest(4, new int[][] { new int [] { 1, 0 }, new int [] { 1, 2 }, new int [] { 1, 3 } }, new ArrayList() {{ add (1); }});
 		_instance.runTest(6, new int[][] { new int [] { 3, 0 }, new int [] { 3, 1 }, new int [] { 3, 2 }, new int [] { 3, 4 }, 
 							 new int [] { 5, 4 } }, new ArrayList() {{ add (3); add (4); }});
 		_instance.runTest(6, new int[][] { new int [] { 0, 1 }, new int [] { 0, 2 }, new int [] { 0, 3 }, new int [] { 3, 4 }, 
