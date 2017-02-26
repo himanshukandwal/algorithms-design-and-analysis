a07c7916becc3fcb6183cd130a658de76b72b204
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-02-25 19:15:29 -0600

    daily submission codes

---
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/GrayCode.java
@@ -0,0 +1,7 @@
+package me.hxkandwal.daily.challanges.leetcode;
+
+import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
+
+public class GrayCode extends AbstractCustomTestRunner {
+
+}
--- implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/PalindromePermutationII.java
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/PalindromePermutationII.java
@@ -15,5 +15,35 @@ import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
  * @author Hxkandwal
  */
 public class PalindromePermutationII extends AbstractCustomTestRunner {
+	
+	public List<String> generatePalindromes(String s) {
+        List<String> ans = new ArrayList<>();
+        int [] count = new int [256]; int odds = 0;
+        for (char c : s.toCharArray()) count [c] ++;
+        for (int c : count) if (c % 2 != 0) odds ++;
+        if (odds <= 1) {
+            Character center = null;
+            for (int idx = 0; idx < count.length; idx ++) 
+                if (count [idx] % 2 == 1) { 
+                    center = ((char) idx); 
+                    count [idx] --; 
+                    break; 
+                }
+                    
+            generate (ans, count, (center != null ? String.valueOf(center) : new String ()), s.length());
+        }
+        return ans; 
+    }
+    
+    private void generate (List<String> ans, int[] count, String build, int len) {
+        for (int idx = 0; idx < count.length; idx ++) {
+            if (count [idx] > 0) {
+                count [idx] -= 2;
+                generate (ans, count, ((char) idx) + build + ((char) idx), len);
+                count [idx] += 2;
+            }
+        }
+        if (build.length() == len) ans.add (new String (build));
+    }
 
 }
