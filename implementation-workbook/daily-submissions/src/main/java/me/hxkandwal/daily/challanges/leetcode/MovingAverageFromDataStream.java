43a31b7b8e9da2ecb6923f9d406c4b9c979c441d
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-02-26 03:31:35 -0600

    daily submission codes

---
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/MovingAverageFromDataStream.java
@@ -0,0 +1,41 @@
+package me.hxkandwal.daily.challanges.leetcode;
+
+import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
+
+/**
+ * 346. Moving Average from Data Stream
+ * 
+ * Given a stream of integers and a window size, calculate the moving average of all integers in the sliding window.
+ * 
+ * For example,
+ * 		MovingAverage m = new MovingAverage(3);
+ * 		m.next(1) = 1
+ * 		m.next(10) = (1 + 10) / 2
+ * 		m.next(3) = (1 + 10 + 3) / 3
+ * 		m.next(5) = (10 + 3 + 5) / 3
+ * 
+ * @author Hxkandwal
+ */
+public class MovingAverageFromDataStream extends AbstractCustomTestRunner {
+	
+    int [] values;
+    int size = 0;
+    int sum = 0;
+    int rindex = 0;
+    
+    /** Initialize your data structure here. */
+    public MovingAverageFromDataStream (int size) {
+        this.values = new int [size];
+    }
+    
+    public double next(int val) {
+        if (size < values.length) return (sum += (values [size ++] = val)) * 1.0d  / size;
+        else {
+            if (rindex == values.length) rindex = 0;
+            double avg = (sum = (sum - values [rindex] + val)) * 1.0d / size;
+            values [rindex ++] = val;
+            return avg;
+        }
+    }
+
+}
