1909ab88fff0447577605dd9b169d04199392c9e
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-02-25 18:45:19 -0600

    daily submission codes

---
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/PalindromePermutationII.java
@@ -0,0 +1,19 @@
+package me.hxkandwal.daily.challanges.leetcode;
+
+import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
+
+/**
+ * 267. Palindrome Permutation II
+ * 
+ * Given a string s, return all the palindromic permutations (without duplicates) of it. Return an empty list if no 
+ * palindromic permutation could be form.
+ * 
+ * For example:
+ * 		Given s = "aabb", return ["abba", "baab"].
+ * 		Given s = "abc", return [].
+ * 
+ * @author Hxkandwal
+ */
+public class PalindromePermutationII extends AbstractCustomTestRunner {
+
+}
