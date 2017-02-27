b69df6c52504031875a4864b6a5726d6f44ac3a2
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-02-27 16:46:31 -0600

    daily submission codes

---
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/TargetSum.java
@@ -0,0 +1,30 @@
+package me.hxkandwal.daily.challanges.leetcode;
+
+import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
+
+/**
+ * 494. Target Sum
+ * 
+ * You are given a list of non-negative integers, a1, a2, ..., an, and a target, S. Now you have 2 symbols + and -. 
+ * For each integer, you should choose one from + and - as its new symbol.
+ * 
+ * Find out how many ways to assign symbols to make sum of integers equal to target S.
+ * 
+ * Example 1:	
+ * 		Input: nums is [1, 1, 1, 1, 1], S is 3.
+ * 		Output: 5
+ * 		Explanation: 
+ * 
+ * 			-1+1+1+1+1 = 3
+ * 			+1-1+1+1+1 = 3
+ * 			+1+1-1+1+1 = 3
+ * 			+1+1+1-1+1 = 3
+ * 			+1+1+1+1-1 = 3
+ * 			
+ * 	There are 5 ways to assign symbols to make the sum of nums be target 3.
+ * 
+ * @author Hxkandwal
+ */
+public class TargetSum extends AbstractCustomTestRunner {
+
+}
