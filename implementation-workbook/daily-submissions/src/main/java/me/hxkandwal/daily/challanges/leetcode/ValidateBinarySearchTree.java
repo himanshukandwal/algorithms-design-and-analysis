b7d81b0b7b022fbf7bb9bba69948864594bb6daa
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-03-01 23:25:54 -0600

    daily submission codes

--- implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/ValidateBinarySearchTree.java
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/ValidateBinarySearchTree.java
@@ -49,6 +49,17 @@ public class ValidateBinarySearchTree extends AbstractCustomTestRunner {
 		public TreeNode(int x) { val = x; }
 	}
 	
+	// better answer
+	public boolean _isValidBSTBetter(TreeNode root) {
+        return isValidBST (root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    private boolean isValidBST (TreeNode root, long min, long max) {
+        if (root == null) return true;
+        if (!(root.val > min && root.val < max)) return false;
+        return isValidBST (root.left, min, root.val) && isValidBST (root.right, root.val, max); 
+    }
+	
 	// manager/dispatcher function.
 	public boolean _isValidBST(TreeNode root) {
 		return (root == null ? true : getMinMax(root) != null);
