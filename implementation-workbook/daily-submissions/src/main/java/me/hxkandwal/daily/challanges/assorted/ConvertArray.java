0fc2fddb685d64e3167c18454ba2440371457535
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-03-02 16:52:31 -0800

    daily submission codes

---
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/assorted/ConvertArray.java
@@ -0,0 +1,44 @@
+package me.hxkandwal.daily.challanges.assorted;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.List;
+
+import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
+
+/**
+ * Convert Array
+ * 
+ * http://www.ardendertat.com/2011/10/18/programming-interview-questions-9-convert-array/
+ * 
+ * @author Hxkandwal
+ */
+public class ConvertArray extends AbstractCustomTestRunner {
+	
+	private static ConvertArray _instance = new ConvertArray();
+	
+	public static int[] _convert (int [] arr) {
+		int iters = arr.length / 3;
+		int [] ans = new int [arr.length];
+		
+		for (int idx = 0; idx < arr.length; idx ++) 
+			ans [idx] = arr [(idx % 3) * 3 + (idx / 3)];
+		
+		return ans;
+	}
+
+	// driver method
+	public static void main(String[] args) {
+		_instance.runTest(new int[] { 1, 2, 3, 1, 2, 3, 1, 2, 3 }, new int[] { 1, 1, 1, 2, 2, 2, 3, 3, 3 });
+	}
+	
+	public void runTest(final int [] arr, final int[] expectedOutput) {
+		List<Object> answers = runAll(getClass(), new Object[] { arr });
+		
+		for (Object answer : answers) 
+			assertThat((int[]) answer).isEqualTo(expectedOutput);
+		
+		System.out.println("ok!");
+	}
+	
+}
---
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/assorted/ReverseWordsInAString.java
@@ -0,0 +1,14 @@
+package me.hxkandwal.daily.challanges.assorted;
+
+import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
+
+/**
+ * Reverse Words in a String
+ * 
+ * http://www.ardendertat.com/2011/10/31/programming-interview-questions-12-reverse-words-in-a-string/
+ * 
+ * @author Hxkandwal
+ */
+public class ReverseWordsInAString extends AbstractCustomTestRunner {
+
+}
