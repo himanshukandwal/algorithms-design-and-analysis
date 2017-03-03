623754447cbee2fe8f5cef65608f84c71ebcd3b7
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-03-02 22:24:01 -0800

    daily submission codes

--- implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/assorted/ReverseWordsInAString.java
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/assorted/ReverseWordsInAString.java
@@ -25,21 +25,59 @@ public class ReverseWordsInAString extends AbstractCustomTestRunner {
 		}
 		return out + parts[0];
 	}
+	
+	public String _reverseWordsBetter(String s) {
+		if (s == null)
+			return null;
 
-	public String _reverseWords(String s) {
-		char [] ans = new char [s.length()];
-		int aidx = ans.length, iidx = 0;
-		
-		for (int idx = 0; idx < s.length(); idx ++) {
-			if (s.charAt(idx) == ' ' || idx == s.length() - 1) {
-				if (idx == s.length() - 1) idx ++;
-				int dist = (idx - iidx);
-				aidx -= dist;
-				for (; iidx < idx; iidx ++) ans [aidx ++] = s.charAt(iidx);
-				aidx -= dist; iidx ++;
-			} 
+		char[] a = s.toCharArray();
+		int n = a.length;
+
+		// step 1. reverse the whole string
+		reverse(a, 0, n - 1);
+		// step 2. reverse each word
+		reverseWords(a, n);
+		// step 3. clean up spaces
+		return cleanSpaces(a, n);
+	}
+
+	void reverseWords(char[] a, int n) {
+		int i = 0, j = 0;
+
+		while (i < n) {
+			while (i < j || i < n && a[i] == ' ')
+				i++; // skip spaces
+			while (j < i || j < n && a[j] != ' ')
+				j++; // skip non spaces
+			reverse(a, i, j - 1); // reverse the word
 		}
-		return String.valueOf(ans);
+	}
+
+	// trim leading, trailing and multiple spaces
+	String cleanSpaces(char[] a, int n) {
+		int i = 0, j = 0;
+
+		while (j < n) {
+			while (j < n && a[j] == ' ')
+				j++; // skip spaces
+			while (j < n && a[j] != ' ')
+				a[i++] = a[j++]; // keep non spaces
+			while (j < n && a[j] == ' ')
+				j++; // skip spaces
+			if (j < n)
+				a[i++] = ' '; // keep only one space
+		}
+
+		return new String(a).substring(0, i);
+	}
+
+	// reverse a[] from a[i] to a[j]
+	private void reverse(char[] a, int i, int j) {
+		while (i < j) {
+			char t = a[i];
+			a[i++] = a[j];
+			a[j--] = t;
+		}
 	}
 	
 	// driver method
---
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/DecodeString.java
@@ -0,0 +1,56 @@
+package me.hxkandwal.daily.challanges.leetcode;
+
+import java.util.Stack;
+
+import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
+
+/**
+ * 394. Decode String
+ * 
+ * Given an encoded string, return it's decoded string.
+ * The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times.
+ * 
+ * Note that k is guaranteed to be a positive integer.
+ * 
+ * You may assume that the input string is always valid; No extra white spaces, square brackets are well-formed, etc.
+ * Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. 
+ * 
+ * For example, there won't be input like 3a or 2[4].
+ * 
+ * Examples:
+ *
+ *	s = "3[a]2[bc]", return "aaabcbc".
+ *	s = "3[a2[c]]", return "accaccacc".
+ *	s = "2[abc]3[cd]ef", return "abcabccdcdcdef".
+ *
+ * @author Hxkandwal
+ */
+public class DecodeString extends AbstractCustomTestRunner {
+
+	 public String decodeString(String s) {
+        if (s.length () == 0) return "";
+        Stack<Object[]> stk = new Stack<>();
+        
+        StringBuilder ans = new StringBuilder();
+        Integer cnum = null; StringBuilder cbuild = new StringBuilder();
+        for (int idx = 0; idx < s.length(); idx ++) {
+            char ch = s.charAt(idx);
+            
+            if (Character.isDigit(ch))
+                cnum = (ch - '0') + ((cnum == null) ? 0 : 10 * cnum); 
+            else if (ch == '[') {
+                stk.push (new Object[] { cnum, cbuild = new StringBuilder() });
+                cnum = null;
+            } else if (ch == ']') {
+                int rep = (Integer) stk.pop () [0];
+                String str = cbuild.toString();
+                for (int loop = 0; loop < rep - 1; loop ++) cbuild.append (str);
+                if (!stk.isEmpty())
+                    cbuild = ((StringBuilder) stk.peek()[1]).append (cbuild.toString());
+                else ans.append(cbuild.toString());
+            } else { if (!stk.isEmpty()) cbuild.append (ch); else ans.append (ch); }
+        }
+        return ans.toString();
+    }
+	
+}
