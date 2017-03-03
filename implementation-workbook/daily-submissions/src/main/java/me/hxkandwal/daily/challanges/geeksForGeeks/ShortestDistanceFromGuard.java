d1b6f82583bc3d168d434cf8cb498176f1ea9c7c
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-03-03 02:04:07 -0800

    daily submission codes

--- implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/geeksForGeeks/ShortestDistanceFromGuard.java
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/geeksForGeeks/ShortestDistanceFromGuard.java
@@ -9,7 +9,10 @@ import java.util.Queue;
 import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
 
 /**
- * Given a matrix that is filled with ‘O’, ‘G’, and ‘W’ where ‘O’ represents open space, ‘G’ represents guards and ‘W’ represents walls in a Bank. Replace all of the O’s in the matrix with their shortest distance from a guard, without being able to go through any walls. Also, replace the guards with 0 and walls with -1 in output matrix. 
+ * Given a matrix that is filled with ‘O’, ‘G’, and ‘W’ where ‘O’ represents open space, ‘G’ represents guards and ‘W’ 
+ * represents walls in a Bank. Replace all of the O’s in the matrix with their shortest distance from a guard, 
+ * without being able to go through any walls. Also, replace the guards with 0 and walls with -1 in output matrix.
+ *  
  * Expected Time complexity is O(MN) for a M x N matrix.
  * 
  * Example :
