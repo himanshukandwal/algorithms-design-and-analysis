f393fffa24da892d73fad5640ac98871c1b094b1
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-03-18 00:24:07 -0500

    daily submission codes

--- implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/TwoSumIIInputArraySorted.java
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/TwoSumIIInputArraySorted.java
@@ -20,4 +20,16 @@ import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
  */
 public class TwoSumIIInputArraySorted extends AbstractCustomTestRunner {
 
+    public int[] twoSum(int[] numbers, int target) {
+        int start = 0, end = numbers.length - 1;
+        
+        while (start < end) {
+            if (numbers [start] + numbers [end] > target) end --;
+            else if (numbers [start] + numbers [end] < target) start ++;
+            else return new int [] { start + 1, end + 1 };
+        }
+        
+        return null;
+    }
+    
 }
