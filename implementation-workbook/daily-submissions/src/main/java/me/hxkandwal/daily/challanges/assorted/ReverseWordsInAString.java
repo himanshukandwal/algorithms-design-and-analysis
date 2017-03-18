182e199c1441b5aba18f10e45abd0ca6466b6983
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-03-18 17:09:47 -0500

    daily submission codes

--- implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/assorted/ReverseWordsInAString.java
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/assorted/ReverseWordsInAString.java
@@ -17,68 +17,20 @@ public class ReverseWordsInAString extends AbstractCustomTestRunner {
 	
 	private static ReverseWordsInAString _instance = new ReverseWordsInAString();
 	
-	public String reverse (String input) {
-		String[] parts = input.trim().split("\\s+");
-		String out = "";
-		for (int i = parts.length - 1; i > 0; i--) {
-		    out += parts[i] + " ";
-		}
-		return out + parts[0];
-	}
-	
-	public String _reverseWordsBetter(String s) {
-		if (s == null)
-			return null;
-
-		char[] a = s.toCharArray();
-		int n = a.length;
-
-		// step 1. reverse the whole string
-		reverse(a, 0, n - 1);
-		// step 2. reverse each word
-		reverseWords(a, n);
-		// step 3. clean up spaces
-		return cleanSpaces(a, n);
-	}
-
-	void reverseWords(char[] a, int n) {
-		int i = 0, j = 0;
-
-		while (i < n) {
-			while (i < j || i < n && a[i] == ' ')
-				i++; // skip spaces
-			while (j < i || j < n && a[j] != ' ')
-				j++; // skip non spaces
-			reverse(a, i, j - 1); // reverse the word
-		}
-	}
-
-	// trim leading, trailing and multiple spaces
-	String cleanSpaces(char[] a, int n) {
-		int i = 0, j = 0;
-
-		while (j < n) {
-			while (j < n && a[j] == ' ')
-				j++; // skip spaces
-			while (j < n && a[j] != ' ')
-				a[i++] = a[j++]; // keep non spaces
-			while (j < n && a[j] == ' ')
-				j++; // skip spaces
-			if (j < n)
-				a[i++] = ' '; // keep only one space
-		}
-
-		return new String(a).substring(0, i);
-	}
-
-	// reverse a[] from a[i] to a[j]
-	private void reverse(char[] a, int i, int j) {
-		while (i < j) {
-			char t = a[i];
-			a[i++] = a[j];
-			a[j--] = t;
-		}
-	}
+	public String reverseWords(String s) {
+        if (s.length() == 0) return s;
+        StringBuilder ans = new StringBuilder();
+        StringBuilder local = new StringBuilder();
+        int idx = 0;
+        while (idx < s.length()) {
+            if (s.charAt(idx) == ' ') {
+                if (local.length() > 0) { ans.append (local.reverse().toString()).append(' '); local.setLength(0); }
+            } else local.append (s.charAt(idx));
+            idx ++;
+        }
+        if (local.length() > 0) ans.append (local.reverse().toString());
+        return ans.reverse().toString().trim();
+    }
 	
 	// driver method
 	public static void main(String[] args) {
