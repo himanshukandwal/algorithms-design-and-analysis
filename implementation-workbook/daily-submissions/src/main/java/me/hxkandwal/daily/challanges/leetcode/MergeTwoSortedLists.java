42085d40cb85ac0548a291a7a1ed83e6afa0ee5c
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-03-18 23:30:02 -0500

    daily submission codes

--- implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/MergeTwoSortedLists.java
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/MergeTwoSortedLists.java
@@ -28,6 +28,18 @@ public class MergeTwoSortedLists extends AbstractCustomTestRunner {
 		ListNode next;
 		public ListNode(int x) { val = x; }
 	}
+	
+	public ListNode _mergeTwoListsBook(ListNode l1, ListNode l2) {
+		ListNode dummyHead = new ListNode(0);
+		ListNode p = dummyHead;
+		while (l1 != null && l2 != null) {
+			if (l1.val < l2.val) { p.next = l1; l1 = l1.next; } 
+			else { p.next = l2; l2 = l2.next; }
+			p = p.next;
+		}
+		p.next = (l1 != null) ? l1 : l2;
+		return dummyHead.next;
+	}
     
 	public ListNode _mergeTwoLists(ListNode l1, ListNode l2) {
 		ListNode traverser1 = l1, traverser2 = l2, ans = null, traverser3 = null;
