1e48dc2bf7bc507ab73f120c5d5dd3211d5df6c3
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-03-19 11:21:41 -0500

    daily submission codes

--- implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/RotateArray.java
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/RotateArray.java
@@ -1,5 +1,9 @@
 package me.hxkandwal.daily.challanges.leetcode;
 
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.List;
+
 import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
 
 /**
@@ -15,5 +19,39 @@ import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
  * @author Hxkandwal
  */
 public class RotateArray extends AbstractCustomTestRunner {
+	
+	private static RotateArray _instance = new RotateArray();
 
+	public int[] _rotate(int[] nums, int k) {
+        if ((k %= nums.length) == 0 || nums.length == 0) return nums;
+        reverse (nums, 0, nums.length - 1);
+        reverse (nums, 0, k - 1);
+        reverse (nums, k, nums.length - 1);
+        return nums;
+    }
+	    
+    private void reverse (int [] nums, int from, int to) {
+        for (int idx = 0; idx <= (to - from) >>> 1; idx ++) {
+            int temp = nums [from + idx];
+            nums [from + idx] = nums [to - idx];
+            nums [to - idx] = temp;
+        }
+    }
+	
+	// driver method
+	public static void main(String[] args) {
+		_instance.runTest(new int[] { 1, 2, 3, 4, 5, 6 }, 2, new int[] { 5, 6, 1, 2, 3, 4 });	
+		_instance.runTest(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 3, new int[] { 5, 6, 7, 1, 2, 3, 4 });	
+		_instance.runTest(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 2, new int[] { 6, 7, 1, 2, 3, 4, 5 });	
+	}
+
+	public void runTest(final int[] nums, final int k, final int[] expectedOutput) {
+		List<Object> answers = runAll(getClass(), new Object[] { nums, k });
+
+		for (Object answer : answers)
+			assertThat((int[]) answer).isEqualTo(expectedOutput);
+
+		System.out.println("ok!");
+	}
+	
 }
