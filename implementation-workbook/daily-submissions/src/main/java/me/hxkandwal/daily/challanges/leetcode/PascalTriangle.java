f1f99ed945d5fbc01eaca7e9586555b9e8304763
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-03-20 03:02:00 -0500

    daily submission codes

---
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/PascalTriangle.java
@@ -0,0 +1,41 @@
+package me.hxkandwal.daily.challanges.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
+
+/**
+ * 118. Pascal's Triangle
+ * 
+ * Given numRows, generate the first numRows of Pascal's triangle.
+ * 
+ * For example, given numRows = 5,
+ * Return
+ * 	
+ * 	[
+ * 	     [1],
+ * 	    [1,1],
+ * 	   [1,2,1],
+ * 	  [1,3,3,1],
+ * 	 [1,4,6,4,1]
+ * 	]
+ * 
+ * @author Hxkandwal
+ */
+public class PascalTriangle extends AbstractCustomTestRunner {
+
+	public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+		List<Integer> row, pre = null;
+		for (int i = 0; i < numRows; ++i) {
+			row = new ArrayList<Integer>();
+			for (int j = 0; j <= i; ++j)
+				if (j == 0 || j == i) row.add(1);
+				else row.add(pre.get(j - 1) + pre.get(j));
+			pre = row; res.add(row);
+		}
+		return res;
+    }
+	
+}
