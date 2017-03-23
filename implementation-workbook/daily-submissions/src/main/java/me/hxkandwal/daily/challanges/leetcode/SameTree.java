850db01b8253fa4f98bbdbbe5219345dd11e4bef
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-03-22 23:37:17 -0500

    added solution for LargestCombinedWord problem

---
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/SameTree.java
@@ -0,0 +1,30 @@
+package me.hxkandwal.daily.challanges.leetcode;
+
+import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
+
+/**
+ * 100. Same Tree
+ * 
+ * Given two binary trees, write a function to check if they are equal or not.
+ * 
+ * Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
+ * 
+ * @author Hxkandwal
+ */
+public class SameTree extends AbstractCustomTestRunner {
+
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		
+		public TreeNode(int x) { val = x; }
+	}
+	
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        return (p.val == q.val) ? isSameTree (p.left, q.left) && isSameTree (p.right, q.right) : false;
+    }
+	
+}
