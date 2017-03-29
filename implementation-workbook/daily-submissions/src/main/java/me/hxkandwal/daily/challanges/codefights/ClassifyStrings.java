81c55d3ec6528565115040ad835610cabcccd2ee
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-03-28 19:05:20 -0500

    added changes

---
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/codefights/ClassifyStrings.java
@@ -0,0 +1,87 @@
+package me.hxkandwal.daily.challanges.codefights;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
+
+/**
+ * You categorize strings into three types: good, bad, or mixed. If a string has 3 consecutive vowels or 5 consecutive consonants, 
+ * or both, then it is categorized as bad. Otherwise it is categorized as good. 
+ * 
+ * Vowels in the English alphabet are ["a", "e", "i", "o", "u"] and all other letters are consonants.
+ * 
+ * The string can also contain the character ?, which can be replaced by either a vowel or a consonant. 
+ * This means that the string "?aa" can be bad if ? is a vowel or good if it is a consonant. This kind of string is categorized as mixed.
+ * 
+ * Implement a function that takes a string s and returns its category: good, bad, or mixed.
+ * 
+ * Example:
+ * 		For s = "aeu", the output should be classifyStrings(s) = "bad";
+ * 		For s = "a?u", the output should be classifyStrings(s) = "mixed";
+ * 		For s = "aba", the output should be classifyStrings(s) = "good".
+ * 
+ * link: https://codefights.com/interview/3kHnLbdwLDmySbCpj/companies/gDDsAwPekpst2TjgW
+ * 
+ * @author Hxkandwal
+ */
+public class ClassifyStrings extends AbstractCustomTestRunner {
+	
+	private static ClassifyStrings _instance = new ClassifyStrings();
+	
+	public String _classifyStrings(String s) {
+		Set<Character> vowels = new HashSet<>();
+	    vowels.add ('a'); vowels.add ('e'); vowels.add ('i'); vowels.add ('o'); vowels.add ('u'); 
+	    
+	    int count = 0; boolean vowelFound = false, qmarkFound = false;
+	    for (int idx = 0; idx < s.length(); idx ++) {
+	        char ch = s.charAt(idx);
+	        if (vowels.contains (ch)) {
+	            if (vowelFound) { 
+	                count ++; 
+	                if (count == 3) 
+	                    return qmarkFound ? "mixed" : "bad"; 
+	            }
+	            else count = 1;
+	            qmarkFound = false; vowelFound = true;
+	        } else if (ch != '?') {
+	            if (!vowelFound) { 
+	                count ++;
+	                if (count == 5) 
+	                    return qmarkFound ? "mixed" : "bad"; 
+	            }
+	            else count = 1;
+	            qmarkFound = false; vowelFound = false;
+	        } else {
+	            if ((vowelFound && count == 2) || (!vowelFound && count == 4)) 
+	                return "mixed";
+	            count ++; qmarkFound = true;
+	        }
+	    }
+	    return "good";
+	}
+	
+	// driver method
+	public static void main(String[] args) {
+		_instance.runTest("aeu", "bad");
+		_instance.runTest("a?u", "mixed");
+		_instance.runTest("aba", "good");
+		_instance.runTest("abaaa", "bad");
+		_instance.runTest("aa??bbb", "mixed");
+		_instance.runTest("aa?bbbb", "bad");
+		_instance.runTest("aa?bbb?a?bbbb", "bad");
+	}
+
+	public void runTest(final String s, final String expectedOutput) {
+		List<Object> answers = runAll(getClass(), new Object[] { s });
+
+		for (Object answer : answers)
+			assertThat((String) answer).isEqualTo(expectedOutput);
+		
+		System.out.println("ok!");
+	}
+
+}
--- implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/codefights/CountPairsWithDifference.java
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/codefights/CountPairsWithDifference.java
@@ -9,7 +9,17 @@ import java.util.Map;
 import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
 
 /**
- * https://codefights.com/interview/embtGKRbwNJaCEpcf/companies/gDDsAwPekpst2TjgW
+ * Given an array a of positive integers, find the number of pairs of integers for which the difference between the two 
+ * numbers is equal to a given number k. 
+ * 
+ * Since the number of pairs could be quite large, take it modulo 109 + 7 for your output.
+ * 
+ * Example
+ * 		For k = 3 and a = [1, 6, 8, 2, 4, 9, 12], the output should be countPairsWithDifference(k, a) = 3.
+ * 
+ * There are three pairs of integers whose difference is equal to 3: (1, 4), (6, 9) and (9, 12).
+ * 
+ * link : https://codefights.com/interview/embtGKRbwNJaCEpcf/companies/gDDsAwPekpst2TjgW
  * 
  * @author Hxkandwal
  */
