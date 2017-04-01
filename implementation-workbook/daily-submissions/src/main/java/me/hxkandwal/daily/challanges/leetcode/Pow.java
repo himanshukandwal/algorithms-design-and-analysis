8c2de7f7b44f7038f1ef26820c9bad0c0f4c693f
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-03-31 20:34:27 -0500

    added changes

---
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/Pow.java
@@ -0,0 +1,43 @@
+package me.hxkandwal.daily.challanges.leetcode;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.List;
+
+import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
+
+/**
+ * 50. Pow(x, n)
+ * 
+ * Implement pow(x, n).
+ * 
+ * @author Hxkandwal
+ */
+public class Pow extends AbstractCustomTestRunner {
+	
+	private static Pow _instance = new Pow();
+
+	public double _myPow (double x, int n) {
+		double result = 1.0;
+        for (int i = n; i != 0; i /= 2, x *= x)
+            if (i % 2 != 0) result *= x;
+        return n < 0 ? 1.0 / result : result;
+    }
+	
+	// driver method
+	public static void main(String[] args) {
+		_instance.runTest(2, 5, 32);
+	}
+
+	public void runTest(final double x, int n, final double expectedOutput) {
+		List<Object> answers = runAll(getClass(), new Object[] { x, n });
+
+		for (Object answer : answers) {
+			assertThat((Double) answer).isAtLeast(expectedOutput);
+			assertThat((Double) answer).isAtMost(expectedOutput);
+		}
+		
+		System.out.println("ok!");
+	}	
+
+}
