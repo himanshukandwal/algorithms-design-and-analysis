f43bfccf8499c2f1a333dcc7cfb8b22e9c6d430f
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-03-31 20:46:10 -0500

    added changes

---
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/CopyListWithRandomPointer.java
@@ -0,0 +1,42 @@
+package me.hxkandwal.daily.challanges.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
+
+/**
+ * 138. Copy List with Random Pointer
+ * 
+ * A linked list is given such that each node contains an additional random pointer 
+ * which could point to any node in the list or null.
+ * 
+ * Return a deep copy of the list.
+ * 
+ * @author Hxkandwal
+ */
+public class CopyListWithRandomPointer extends AbstractCustomTestRunner {
+
+	class RandomListNode {
+		int label;
+		RandomListNode next, random;
+		RandomListNode(int x) { this.label = x; }
+	};
+		 
+	public RandomListNode copyRandomList(RandomListNode head) {
+        Map<RandomListNode, RandomListNode> seen = new HashMap<>();
+        return copyDeepRandomList (head, seen);
+    }
+    
+    private RandomListNode copyDeepRandomList(RandomListNode node, Map<RandomListNode, RandomListNode> seen) {
+        if (node == null) return null;
+        if (seen.containsKey(node)) return seen.get (node);
+        
+        RandomListNode copyNode = new RandomListNode (node.label);
+        seen.put(node, copyNode);
+        copyNode.next = copyDeepRandomList (node.next, seen);
+        copyNode.random = copyDeepRandomList (node.random, seen);
+        return copyNode;
+    }
+    
+}
