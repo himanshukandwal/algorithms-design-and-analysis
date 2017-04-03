7df09697b8cc80c3c969a1f9fbe3852837597ccf
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-04-03 02:09:12 -0500

    added changes

--- implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/RemoveDuplicatesFromSortedListII.java
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/RemoveDuplicatesFromSortedListII.java
@@ -21,5 +21,46 @@ public class RemoveDuplicatesFromSortedListII extends AbstractCustomTestRunner {
 		ListNode next;
 		public ListNode(int x) { val = x; }
 	}
+	
+	public ListNode _deleteDuplicates(ListNode head) {
+        ListNode traverser = head, storage = null, newHead = null, newTraverser = null;
+        boolean toAdd = true;
+        while (traverser != null) {
+            if (storage != null) { 
+                if (traverser.val != storage.val) { 
+                    if (toAdd) {
+                        if (newTraverser == null) newHead = newTraverser = storage;
+                        else { newTraverser.next = storage; newTraverser = newTraverser.next; }
+                    }
+                    toAdd = true;
+                } else toAdd = false;
+            }
+            storage = traverser;
+            traverser = traverser.next;
+            storage.next = null;
+        }
+        if (toAdd) {
+            if (newTraverser == null) newHead = newTraverser = storage;
+            else { newTraverser.next = storage; newTraverser = newTraverser.next; }
+        }
+        return newHead;
+    }
+	
+	// cool idea.
+	public ListNode deleteDuplicates(ListNode head) {
+		if (head == null) return null;
+		ListNode fakeHead = new ListNode(0);
+		fakeHead.next = head;
+		ListNode pre = fakeHead, cur = head;
+        
+        while (cur != null) {
+			while (cur.next != null && cur.val == cur.next.val) cur = cur.next;
+			if (pre.next == cur) pre = pre.next;
+			else pre.next = cur.next;
+			cur = cur.next;
+        }
+        
+        return fakeHead.next;
+    }
 
 }
