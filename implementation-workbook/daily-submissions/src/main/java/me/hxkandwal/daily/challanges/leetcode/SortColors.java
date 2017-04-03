a6dca6824c636905353d732dcc7ea3cdab41d997
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-04-03 18:39:50 -0500

    added changes

--- implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/SortColors.java
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/SortColors.java
@@ -26,12 +26,12 @@ public class SortColors extends AbstractCustomTestRunner {
 		int second = nums.length - 1, zero = 0;
 		for (int i = 0; i <= second; i ++) {
 			while (nums [i] == 2 && i < second) swap (nums, i, second --);
-			while (nums [i] == 0 && i > zero) swap(nums, i, zero ++);
+			while (nums [i] == 0 && i > zero) swap (nums, i, zero ++);
         }
         return nums;
     }
 	
-	public int[] sortColors(int[] nums) {
+	public int[] _sortColors(int[] nums) {
 		int ze = -1, oe = -1, idx = 0;
         while (idx < nums.length) {
             if (nums [idx] == 0) {
