78b247116de3af1851951559a071e1cc718c4fdb
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-04-02 22:00:17 -0500

    added changes

--- implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/MinimumNumberOfArrowsToBurstBalloons.java
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/MinimumNumberOfArrowsToBurstBalloons.java
@@ -1,5 +1,10 @@
 package me.hxkandwal.daily.challanges.leetcode;
 
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+
 import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
 
 /**
@@ -27,8 +32,34 @@ public class MinimumNumberOfArrowsToBurstBalloons extends AbstractCustomTestRunn
 	private static MinimumNumberOfArrowsToBurstBalloons _instance = new MinimumNumberOfArrowsToBurstBalloons();
 
 	public int _findMinArrowShots(int[][] points) {
-		
-		return 0;
+        Arrays.sort(points, (o1, o2) -> o1 [0] - o2 [0] == 0 ? o1 [1] - o2 [1] : o1 [0] - o2 [0]);
+        int idx = 0, res = 0;
+        while (idx < points.length) {
+            int end = points [idx][1];
+            while (idx + 1 < points.length) { 
+            	if (end >= points [idx + 1][0]) { end = Math.min (end, points [idx + 1][1]); idx ++; }
+            	else break;
+            }
+            res ++; idx ++;
+        }
+        return res;
     }
+	
+	// driver method
+	public static void main(String[] args) {
+		_instance.runTest(new int [][] { new int [] { 10, 16 }, new int [] { 2, 8 }, new int [] { 1, 6 }, new int [] { 7, 12 }}, 2);
+		_instance.runTest(new int [][] { new int [] { 9, 12 }, new int [] { 1, 10 }, new int [] { 4, 11 }, 
+										 new int [] { 8, 12 }, new int [] { 3, 9 }, new int [] { 6, 9 }, 
+										 new int [] { 6, 7 }}, 2);
+	}
 
+	public void runTest(final int[][] points, final int expectedOutput) {
+		List<Object> answers = runAll(getClass(), new Object[] { points });
+
+		for (Object answer : answers)
+			assertThat((Integer) answer).isEqualTo(expectedOutput);
+		
+		System.out.println("ok!");
+	}
+	
 }
