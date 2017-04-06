b252b52a1e0d728c3abb0902c7e40523dcca1a3f
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-04-06 16:53:03 -0500

    enhanced code for Intersection of Two Arrays II

--- implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/assorted/ArrangingCoins.java
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/assorted/ArrangingCoins.java
@@ -7,23 +7,24 @@ import java.util.List;
 import static com.google.common.truth.Truth.assertThat;
 
 /**
- * Created by Hxkandwal
+ * 441. Arranging Coins
+ * 
+ * You have a total of n coins that you want to form in a staircase shape, where every k-th row must have exactly k coins.
+ * Given n, find the total number of full staircase rows that can be formed.
+ * 
+ * n is a non-negative integer and fits within the range of a 32-bit signed integer.
+ * 
+ * @author Hxkandwal
  */
-public class ArrangeCoins extends AbstractCustomTestRunner {
+public class ArrangingCoins extends AbstractCustomTestRunner {
 
-    private static ArrangeCoins _instance = new ArrangeCoins();
-
-    private ArrangeCoins() {}
-
+    private static ArrangingCoins _instance = new ArrangingCoins();
+    
     // method 1 : regular hill climbing method.
     public static long _arrangeCoin(int coin) {
-        long step = 0;
-        while (coin >= (step + 1)) {
-            step ++;
-            coin -= step;
-        }
-
-        return step;
+    	int steps = 0;
+        while ((coin -= steps) > 0) steps ++;
+        return coin < 0 ? --steps : steps;
     }
 
     // method 2 : faster
