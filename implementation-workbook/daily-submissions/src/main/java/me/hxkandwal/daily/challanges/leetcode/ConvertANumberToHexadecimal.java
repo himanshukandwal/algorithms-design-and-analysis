baf9a396109e2eab165a4f353336d309bc63db83
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-04-07 13:07:44 -0500

    added solution for Convert a Number to Hexadecimal

--- implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/ConvertANumberToHexadecimal.java
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/ConvertANumberToHexadecimal.java
@@ -34,7 +34,7 @@ public class ConvertANumberToHexadecimal extends AbstractCustomTestRunner {
     
     public String _toHex(int num) {
     	if (num == 0) return "0";
-    	char[] map = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+    	char[] map = "0123456789abcdef".toCharArray();
         String result = "";
 		while (num != 0) { result = map [(num & 15)] + result; num >>>= 4; }
         return result;
