3f28bfb38cb30971a94381ce6ea0c2da9f9ac7f5
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-04-06 21:37:40 -0500

    enhanced code for Intersection of Two Arrays II

--- implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/GroupAnagrams.java
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/leetcode/GroupAnagrams.java
@@ -1,7 +1,5 @@
 package me.hxkandwal.daily.challanges.leetcode;
 
-import static com.google.common.truth.Truth.assertThat;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -28,50 +26,22 @@ import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
  * 
  * @author Hxkandwal
  */
-@SuppressWarnings({ "unchecked", "rawtypes", "serial" })
 public class GroupAnagrams extends AbstractCustomTestRunner {
-	
-	private static GroupAnagrams _instance = new GroupAnagrams();
-	
-	private GroupAnagrams() {}
 	
 	// Simple usage of hashmap.
     public List<List<String>> _groupAnagrams(String[] strs) {
     	List<List<String>> answer = new ArrayList<>();
-    	
-        if (strs != null && strs.length > 0) { 
-        	Map<Integer, List<String>> dp = new HashMap<>();
-        	for (String str : strs) {
-        		int[] hash = new int[26];
-    			for (int idx = 0; idx < str.length(); idx ++) hash [str.charAt(idx) - 'a'] ++;
-    			int hashcode = Arrays.hashCode(hash);
-    			
-    			if (dp.containsKey(hashcode)) 
-    				dp.get(hashcode).add(str);
-    			else
-    				dp.put(hashcode, new ArrayList() {{ add(str); }});
-			}
-        	
-        	for (List<String> response : dp.values()) answer.add(response);
+    	Map<Integer, List<String>> dp = new HashMap<>();
+    	for (String str : strs) {
+    		int[] hash = new int[26];
+    		for (int idx = 0; idx < str.length(); idx ++) hash [str.charAt(idx) - 'a'] ++;
+    		int hashcode = Arrays.hashCode(hash);
+    		
+    		if (!dp.containsKey(hashcode)) dp.put(hashcode, new ArrayList<>());
+    		dp.get(hashcode).add(str);
         }
-        
+        answer.addAll(dp.values());
         return answer;
     }
-
-    // driver method
- 	public static void main(String[] args) {
- 		_instance.runTest(new String[] { "eat", "tea", "tan", "ate", "nat", "bat" }, new ArrayList() {{  add(new ArrayList() {{  add("ate"); add("eat"); add("tea"); }});
- 																										 add(new ArrayList() {{  add("nat"); add("tan"); add("tea"); }});
- 																										 add(new ArrayList() {{  add("bat");  }});}});
- 	}
-
- 	public void runTest(final String[] strs, final List<List<String>> expectedOutput) {
- 		List<Object> answers = runAll(getClass(), new Object[] { strs });
-
- 		for (Object answer : answers)
- 			assertThat((List<List<String>>) answer).isEqualTo(expectedOutput);
- 		
- 		System.out.println("ok!");
- 	}    
-
+    
 }
