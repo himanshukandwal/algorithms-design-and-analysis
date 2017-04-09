66037451005962f13e426b06df5c8dfdc71920c6
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-04-08 22:37:00 -0500

    added solution for HyperMod

--- implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/codefights/HyperMod.java
+++ implementation-workbook/daily-submissions/src/main/java/me/hxkandwal/daily/challanges/codefights/HyperMod.java
@@ -18,28 +18,41 @@ import me.hxkandwal.daily.challanges.AbstractCustomTestRunner;
  * Given integers a, b, n, and m, find (a [n] b) modulo m.
  * 
  * Example :
- * 
- * 		For a = 2, b = 3, n = 3, m = 7, the output should be hyperMod(a,b,n,m) = 1.
+ * 	For a = 2, b = 3, n = 3, m = 7, the output should be hyperMod(a,b,n,m) = 1.
  * 
  * It is 2 [3] 3 = 23 = 8, and 8 mod 7 = 1.
  * 
  * @author Hxkandwal
- *
  */
 public class HyperMod extends AbstractCustomTestRunner {
 	
 	private static HyperMod _instance = new HyperMod();
 	
-	private HyperMod() {}
+	public double hyperMod(int a, int b, int n, int m) {
+		return n < 3 ? (n < 2 ? b + 1 + ~-a * n : a * b) % m
+				: n < 4 ? P (a, b, m)
+						: n < 5 ? H (a, b, m)
+								:  b < 4 ? hyperMod(a, b < 3 ? a : (int) hyperMod (a, b - 1, n, m), n - 1, m) : H (a, 9, m);
+	}
 	
-	public static int _hyperMod(int a, int b, int n, int m) {
-		return (n == 0) ? b + 1 : ((n == 1) ? a + b : (n == 2 ? (a * b) : (b == 0 ? 1 : (_hyperMod (a, _hyperMod (a, b - 1, n, m), n-1, m)) % m )));
+	private long P (long a, long b, int m){    
+		return b < 1 ? 1 : (b % 2 * ~-a + 1) * (a = P (a, b / 2, m)) % m * a % m;
+	}
+
+	private long H (int a, int b, int N) {
+	    int m = N, n = N, i = 1;
+		for (; i++ < n / i;)
+			for (m -= n % i < 1 ? m / i : 0; n % i < 1;)
+	            n /= i;
+
+		m -= n > 1 ? m / n : 0;
+		return b < 2 ? a : P (a, m > 1 ? H (a, b - 1, m) : 0, N);
 	}
 
 	// driver method
 	public static void main(String[] args) {
 		_instance.runTest(2, 3, 3, 7, 1);
-//		_instance.runTest(2, 985974312, 4, 1000000007, 661944226);
+		_instance.runTest(2, 985974312, 4, 1000000007, 661944226);
 	}
 
 	public void runTest(final int a, final int b, final int n, final int m, final int expectedOutput) {
