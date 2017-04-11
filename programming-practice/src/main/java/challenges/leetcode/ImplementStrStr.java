a54d37e66138a5a47e1617b176d4795281d79053
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-04-11 00:05:37 -0500

    added code for Strstr

--- programming-practice/src/main/java/challenges/leetcode/ImplementStrStr.java
+++ programming-practice/src/main/java/challenges/leetcode/ImplementStrStr.java
@@ -20,8 +20,22 @@ public class ImplementStrStr extends AbstractCustomTestRunner {
 
 	private static ImplementStrStr _instance = new ImplementStrStr();
 	
-	private ImplementStrStr() {};
-	
+	public int _strstr (String haystack, String needle) {
+	    int [] pre = new int [needle.length()];
+	    for (int idx = 1, jdx = 0; idx < needle.length(); idx ++) {
+	        while (jdx > 0 && needle.charAt(idx) != needle.charAt(jdx)) jdx = pre [jdx - 1];
+	        if (needle.charAt(idx) == needle.charAt(jdx)) pre [idx] = ++ jdx;
+	    }
+	        
+	    for (int idx = 0, jdx = 0; idx < haystack.length(); idx ++) {
+	        while (jdx > 0 && haystack.charAt(idx) != needle.charAt(jdx)) jdx = pre [jdx - 1];
+	        
+	        if (haystack.charAt(idx) == needle.charAt(jdx) && ++ jdx == needle.length()) 
+	            return idx - jdx + 1;
+	    }
+	    return -1;
+	}
+
     public int _strStr(String haystack, String needle) {
         int res = -1;
         for (int idx = 0; idx <= haystack.length() - needle.length(); idx ++) {
@@ -39,6 +53,7 @@ public class ImplementStrStr extends AbstractCustomTestRunner {
 
 	// driver method
 	public static void main(String[] args) {
+		_instance.runTest("aabaaabaaac", "aabaaac", 4);
 		_instance.runTest("leetcode", "leetcode", 0);
 		_instance.runTest("leetcode", "leetcodes", -1);
 		_instance.runTest("eleetcode", "leetcode", 1);
