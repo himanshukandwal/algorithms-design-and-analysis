d3fd5953c2f7aa03eb47a6b035672c7e5d8ddc0b
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-04-14 10:01:23 -0500

    updated code for GameOfLife

--- programming-practice/src/main/java/challenges/leetcode/GameOfLife.java
+++ programming-practice/src/main/java/challenges/leetcode/GameOfLife.java
@@ -29,25 +29,62 @@ import challenges.AbstractCustomTestRunner;
  */
 public class GameOfLife extends AbstractCustomTestRunner {
 
-	int[][] dir ={{1,-1},{1,0},{1,1},{0,-1},{0,1},{-1,-1},{-1,0},{-1,1}};
-
+	/**
+	 * To solve it in place, we use 2 bits to store 2 states:
+	 * 		[2nd bit, 1st bit] = [next state, current state]
+	 * 			- 00  dead (next) <- dead (current)
+	 * 			- 01  dead (next) <- live (current)
+	 * 			- 10  live (next) <- dead (current)
+	 * 			- 11  live (next) <- live (current)
+	 * 
+	 * - In the beginning, every cell is either 00 or 01. Notice that 1st state is independent of 2nd state.
+	 * - Imagine all cells are instantly changing from the 1st to the 2nd state, at the same time.
+	 * - Let's count # of neighbors from 1st state and set 2nd state bit.
+	 * - Since every 2nd state is by default dead, no need to consider transition 01 -> 00.
+	 * - In the end, delete every cell's 1st state by doing >> 1.
+	 * 
+	 * For each cell's 1st bit, check the 8 pixels around itself, and set the cell's 2nd bit.
+	 * - Transition 01 -> 11: when board == 1 and lives >= 2 && lives <= 3.
+	 * - Transition 00 -> 10: when board == 0 and lives == 3.
+	 * 
+	 * To get the current state, simply do : board[i][j] & 1
+	 * To get the next state, simply do : board[i][j] >> 1
+	 */
 	public void gameOfLife(int[][] board) {
-		for (int i = 0; i < board.length; i++) {
-			for (int j = 0; j < board[0].length; j++) {
-				int live = 0;
-				for (int[] d : dir) {
-					if (d[0] + i < 0 || d[0] + i >= board.length || d[1] + j < 0 || d[1] + j >= board[0].length)
-						continue;
-					if (board[d[0] + i][d[1] + j] == 1 || board[d[0] + i][d[1] + j] == 2)
-						live++;
-				}
-				
-				if (board[i][j] == 0 && live == 3) board[i][j] = 3;
-				if (board[i][j] == 1 && (live < 2 || live > 3)) board[i][j] = 2;
-			}
-		}
-		
-		for (int i = 0; i < board.length; i++) for (int j = 0; j < board[0].length; j++) board[i][j] %= 2;
+	    if (board == null || board.length == 0) return;
+	    int m = board.length, n = board[0].length;
+
+	    for (int i = 0; i < m; i++) {
+	        for (int j = 0; j < n; j++) {
+	            int lives = liveNeighbors(board, m, n, i, j);
+
+	            // In the beginning, every 2nd bit is 0;
+	            // So we only need to care about when will the 2nd bit become 1.
+	            if (board[i][j] == 1 && lives >= 2 && lives <= 3) {  
+	                board[i][j] = 3; // Make the 2nd bit 1: 01 ---> 11
+	            }
+	            if (board[i][j] == 0 && lives == 3) {
+	                board[i][j] = 2; // Make the 2nd bit 1: 00 ---> 10
+	            }
+	        }
+	    }
+
+	    for (int i = 0; i < m; i++) {
+	        for (int j = 0; j < n; j++) {
+	            board[i][j] >>= 1;  // Get the 2nd state.
+	        }
+	    }
+	}
+
+	public int liveNeighbors(int[][] board, int m, int n, int i, int j) {
+	    int lives = 0;
+	    for (int x = Math.max(i - 1, 0); x <= Math.min(i + 1, m - 1); x++) {
+	        for (int y = Math.max(j - 1, 0); y <= Math.min(j + 1, n - 1); y++) {
+	            lives += board[x][y] & 1;
+	        }
+	    }
+	    lives -= board[i][j] & 1;
+	    return lives;
 	}
 	
 }
