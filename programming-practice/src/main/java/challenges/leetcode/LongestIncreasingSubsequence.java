5b01b54d9843ba0859181d03f7e6ba9d4a81d2a2
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-04-16 21:28:59 -0500

    added solution for Longest Increasing Subsequence

---
+++ programming-practice/src/main/java/challenges/leetcode/LongestIncreasingSubsequence.java
@@ -0,0 +1,57 @@
+package challenges.leetcode;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+
+import challenges.AbstractCustomTestRunner;
+
+/**
+ * 300. Longest Increasing Subsequence
+ * 
+ * Given an unsorted array of integers, find the length of longest increasing subsequence.
+ * For example,
+ * 		Given [10, 9, 2, 5, 3, 7, 101, 18],
+ * 		
+ * 		The longest increasing subsequence is [2, 3, 7, 101], therefore the length is 4. Note that there may be more than 
+ * one LIS combination, it is only necessary for you to return the length.
+ * 
+ * Your algorithm should run in O(n^2) complexity.
+ * Follow up: Could you improve it to O(n log n) time complexity?
+ * 
+ * @author Hxkandwal
+ */
+public class LongestIncreasingSubsequence extends AbstractCustomTestRunner {
+	
+	private static LongestIncreasingSubsequence _instance = new LongestIncreasingSubsequence();
+
+	public int _lengthOfLIS(int[] nums) {
+        if (nums.length == 0) return 0;
+        int [] dp = new int [nums.length];
+        Arrays.fill (dp, 1);
+        int max = 1;
+        for (int idx = 1; idx < nums.length; idx ++) {
+            int mmax = dp [idx];
+            for (int iidx = 0; iidx < idx; iidx ++)  if (nums [idx] > nums [iidx]) mmax = Math.max (mmax, dp [iidx] + 1);
+            max = Math.max (max, dp [idx] = mmax);
+        }
+        return max;
+    }
+
+	// driver method
+	public static void main(String[] args) {
+		_instance.runTest(new int [] { -2, -1 }, 2);
+		_instance.runTest(new int [] { 10, 9, 2, 5, 3, 7, 101, 18 }, 4);
+	}
+
+	public void runTest(final int[] nums, final int expectedOutput) {
+		List<Object> answers = runAll(getClass(), new Object[] { nums });
+
+		for (Object answer : answers)
+			assertThat((Integer) answer).isEqualTo(expectedOutput);
+
+		System.out.println("ok!");
+	}
+	
+}
