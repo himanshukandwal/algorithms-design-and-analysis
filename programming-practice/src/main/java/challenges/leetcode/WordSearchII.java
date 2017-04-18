1c7ebae29af3fa7c31e5588584e4cbc2322fe4b4
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-04-18 11:26:14 -0500

    optimized solution for Calculate Word Search II

--- programming-practice/src/main/java/challenges/leetcode/WordSearchII.java
+++ programming-practice/src/main/java/challenges/leetcode/WordSearchII.java
@@ -4,9 +4,7 @@ import static com.google.common.truth.Truth.assertThat;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import challenges.AbstractCustomTestRunner;
 
@@ -53,7 +51,6 @@ public class WordSearchII extends AbstractCustomTestRunner {
     
     public List<String> _findWords(char[][] board, String[] words) {
         List<String> answer = new ArrayList<>();
-        Set<String> ans = new HashSet<>();
         if (board.length == 0 || words.length == 0) return answer;
         Trie root = new Trie (' ');
         for (String word : words) root.add (word);
@@ -62,31 +59,27 @@ public class WordSearchII extends AbstractCustomTestRunner {
             for (int col = 0; col < board [0].length; col ++) {
                 char ch = board [row][col];
                 if (root.children [ch] != null) 
-                    dfs (board, root.children [ch], ans, String.valueOf(ch), row, col);
+                    dfs (board, root.children [ch], answer, String.valueOf(ch), row, col);
             }
         }
-        answer.addAll(ans);
         return answer;
     }
     
     int [] rdir = new int [] { 0, 1, 0, -1 };
     int [] cdir = new int [] { 1, 0, -1, 0 };
     
-    private boolean dfs (char [][] board, Trie node, Set<String> ans, String build, int row, int col) {
-        if (node.terminal) ans.add (build);
+    private void dfs (char [][] board, Trie node, List<String> ans, String build, int row, int col) {
+        if (node.terminal) { ans.add (build); node.terminal = false; }
         char old = board [row][col];
         board [row][col] = '*';
         
-        boolean found = false;
-        for (int idx = 0; idx < 4 && !found; idx ++) {
+        for (int idx = 0; idx < 4; idx ++) {
             int nrow = row + rdir [idx], ncol = col + cdir [idx];
             if (nrow < 0 || nrow >= board.length || ncol < 0 || ncol >= board [0].length) continue;
             char ch = board [nrow][ncol];
-            if (node.children [ch] != null) found = dfs (board, node.children [ch], ans, build + ch, nrow, ncol);
+            if (node.children [ch] != null) dfs (board, node.children [ch], ans, build + ch, nrow, ncol);
         }
-        
         board [row][col] = old;
-        return found;
     }
     
     // driver method
@@ -94,7 +87,7 @@ public class WordSearchII extends AbstractCustomTestRunner {
  		_instance.runTest(new char[][] { "ab".toCharArray(), 
 			 							 "aa".toCharArray() },
  						 new String[] { "aba", "baa", "bab", "aaab", "aaa", "aaaa", "aaba" }, 
- 						 Arrays.asList("aaa", "aba", "aaba", "baa", "aaab"));
+ 						 Arrays.asList("aba", "aaa", "aaab", "baa", "aaba"));
  		_instance.runTest(new char[][] { "aa".toCharArray() }, new String[] { "a" }, Arrays.asList("a"));
 		_instance.runTest(new char[][] { "oaan".toCharArray(), 
 										 "etae".toCharArray(),
