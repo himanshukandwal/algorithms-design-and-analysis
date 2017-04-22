8d7dfdd51691ff5ff99871404e40c07c221b2c02
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-04-22 10:25:32 -0500

    added optimized  solution for Maximum Gap

--- programming-practice/src/main/java/challenges/leetcode/MaximumGap.java
+++ programming-practice/src/main/java/challenges/leetcode/MaximumGap.java
@@ -2,6 +2,7 @@ package challenges.leetcode;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.TreeSet;
 
@@ -24,7 +25,37 @@ public class MaximumGap extends AbstractCustomTestRunner {
 	
 	private static MaximumGap _instance = new MaximumGap();
 
-	public int _maximumGap(int[] nums) {
+	// idea: BucketSort and Radix sort or O (n) not O (nLogn)
+	public int _maximumGapBucketSort(int[] nums) {
+        if (nums.length <= 1) return 0;
+        int max = 0, min = Integer.MAX_VALUE;
+        for (int num : nums) { max = Math.max (max, num); min = Math.min (min, num); }
+        int [] bucketsMin = new int [nums.length - 1];
+        int [] bucketsMax = new int [nums.length - 1];
+        
+        Arrays.fill (bucketsMin, Integer.MAX_VALUE);
+        Arrays.fill (bucketsMax, Integer.MIN_VALUE);
+        int gap = (int) Math.ceil ((double) (max - min)/ (nums.length - 1));
+        
+        for (int num : nums) {
+            if (num == min || num == max) continue;
+            int idx = (num - min)/gap;
+            bucketsMin [idx] = Math.min (num, bucketsMin [idx]);
+            bucketsMax [idx] = Math.max (num, bucketsMax [idx]);
+        }
+        
+        int maxGap = 0;
+        for (int idx = 0; idx < nums.length - 1; idx ++) {
+            if (bucketsMin [idx] == Integer.MAX_VALUE && bucketsMax [idx] == Integer.MIN_VALUE) continue;
+            maxGap = Math.max (maxGap, bucketsMin [idx] - min);
+            min = bucketsMax [idx];
+        }
+        maxGap = Math.max(maxGap, max - min); 
+        return maxGap;
+    }
+	
+	// Using sorting logic of TreeSet.
+	public int _maximumGapLibraray(int[] nums) {
 		if (nums.length <= 1) return 0;
         int max = 0;
         TreeSet<Integer> tset = new TreeSet<>();
@@ -39,7 +70,7 @@ public class MaximumGap extends AbstractCustomTestRunner {
 	
 	// driver method
 	public static void main(String[] args) {
-		_instance.runTest(new int [] { 1,10000000 }, 9999999);
+		_instance.runTest(new int[] { 1, 10000000 }, 9999999);
 	}
 
 	public void runTest(final int[] nums, final int expectedOutput) {
