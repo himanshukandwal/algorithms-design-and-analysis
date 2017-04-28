f841b57c5325f7017dccebb5073972b0887b324c
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-04-28 01:10:08 -0500

    updated solution for Generate Parentheses

--- programming-practice/src/main/java/challenges/leetcode/GenerateParentheses.java
+++ programming-practice/src/main/java/challenges/leetcode/GenerateParentheses.java
@@ -3,6 +3,7 @@ package challenges.leetcode;
 import static com.google.common.truth.Truth.assertThat;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import challenges.AbstractCustomTestRunner;
@@ -22,38 +23,29 @@ import challenges.AbstractCustomTestRunner;
  * 
  * @author Hxkandwal
  */
-@SuppressWarnings({ "rawtypes", "unchecked", "serial" })
 public class GenerateParentheses extends AbstractCustomTestRunner {
 
 	private static GenerateParentheses _instance = new GenerateParentheses();
 	
-	private GenerateParentheses() {}
-	
-	public static List<String> _generateParenthesis(int n) {
-		int left = n, right = n;
-        
-		List<String> collector = new ArrayList<>();
-		innerRecursion (collector, "", left, right);
-		return collector;
+	public List<String> _generateParenthesis(int n) {
+		List<String> ans = new ArrayList<>();
+        generate (ans, "", n, n);
+        return ans;
     }
-	
-	private static void innerRecursion(List<String> collector, String answer, int left, int right) {
-		if (left < 0 || right < left) return;
-		
-		if (left == 0 && right == 0) { 
-			collector.add (answer);
-		} else {
-			if (left > 0) innerRecursion (collector, answer + "(", left - 1, right);
-			
-			if (right > left) innerRecursion (collector, answer + ")", left, right - 1);
-		}
-	}
+    
+    private void generate (List<String> ans, String build, int left, int right) {
+        if (left > right) return;
+        if (left == 0 && right == 0) { ans.add (build); return; }
+        if (left > 0) generate (ans, build + "(", left - 1, right);
+        if (right > left) generate (ans, build + ")", left, right - 1);
+    }
 
 	// driver method
 	public static void main(String[] args) {
-		_instance.runTest(3, new ArrayList() {{ add("((()))");  add("(()())"); add("(())()"); add("()(())"); add("()()()"); }});
+		_instance.runTest(3, Arrays.asList("((()))", "(()())", "(())()", "()(())", "()()()"));
 	}
 
+	@SuppressWarnings("unchecked")
 	public void runTest(final int number, final List<String> expectedOutput) {
 		List<Object> answers = runAll(getClass(), new Object[] { number });
 
