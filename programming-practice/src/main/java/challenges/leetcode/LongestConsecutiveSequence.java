e4f0298c5a853930d99046def610c6daa5249e58
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-04-29 20:09:10 -0500

    updated solution for Longest Consecutive Sequence

--- programming-practice/src/main/java/challenges/leetcode/LongestConsecutiveSequence.java
+++ programming-practice/src/main/java/challenges/leetcode/LongestConsecutiveSequence.java
@@ -31,33 +31,19 @@ public class LongestConsecutiveSequence extends AbstractCustomTestRunner {
 	// expand window as much as possible, and find the max-size window, 
     public static int longestConsecutive(int[] nums) {
     	if (nums.length == 0) return 0;
-    	Set<Integer> set = new HashSet<Integer>();
-    	int max = 1;
-     
-    	// add all
-    	for (int e : nums) set.add(e);
-    	
-    	for (int e : nums) {
-    		int left = e - 1, right = e + 1, count = 1;
-     
-    		// eat till extreme left
-    		while (set.contains(left)) {
-    			count ++;
-    			set.remove(left);
-    			left --;
-    		}
-    		
-    		// eat till extreme right     
-    		while (set.contains(right)) {
-    			count ++;
-    			set.remove(right);
-    			right ++;
-    		}
-     
-    		max = Math.max (count, max);
-    	}
-     
-    	return max;
+        Set<Integer> set = new HashSet <>();
+        int max = 1;
+        
+        for (int num : nums) set.add (num);
+        for (int num : nums) {
+            int left = num - 1, right = num + 1, count = 1;
+            // eat till extreme left
+            while (set.contains (left)) { count ++; set.remove (left --); }
+            // eat till extreme right
+            while (set.contains (right)) { count ++; set.remove (right ++); }
+            max = Math.max (max, count);
+        }
+        return max;
     }
     
     public int _longestConsecutive(int[] nums) {
