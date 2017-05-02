cb8625b8a191b49068d5a38811f1727e7c84a888
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-05-02 18:37:51 -0500

    updated solution for Ransom Note

--- programming-practice/src/main/java/challenges/leetcode/RansomNote.java
+++ programming-practice/src/main/java/challenges/leetcode/RansomNote.java
@@ -25,53 +25,16 @@ public class RansomNote extends AbstractCustomTestRunner {
 
 	private static RansomNote _instance = new RansomNote();
 
-	/* method 1 : using dynamic magazine creation, using a helper function which creates a new string by removing 
-				  the detected position index character. */
     public boolean _canConstruct(String ransomNote, String magazine) {
-    	if (magazine != null && !magazine.isEmpty() && (ransomNote == null || ransomNote.isEmpty())) 
-    		return true;
-    	
-    	if (ransomNote != null && !ransomNote.isEmpty() && (magazine == null || magazine.isEmpty())) 
-    		return false;
-    	
-    	if ((ransomNote == null || ransomNote.isEmpty()) && (magazine == null || magazine.isEmpty())) 
-    		return true;
-    	
-    	int idx = 0;
-    	
-    	for (; idx < magazine.length(); idx ++)
-    		if (magazine.charAt(idx) == ransomNote.charAt(0))
-    			break;
-    	
-    	return (idx == magazine.length()) ? false : _canConstruct(ransomNote.substring(1), filtredString(magazine, idx));
-    }
-    
-    private String filtredString (String string, int pos) {
-    	char[] chArr = new char [string.length() - 1];
-    	
-    	int otherIndex = 0;
-    	for (int idx = 0; idx < string.length(); idx ++) {
-    		if (idx != pos) 
-    			chArr [otherIndex ++] = string.charAt(idx);
-    	}
-    	
-    	return String.valueOf(chArr);
-    }
-    
-	/* method 2 : using a simple and single alphabet character array. */
-    public boolean _canConstructUsingNativeComponents(String ransomNote, String magazine) {
-    	if ((ransomNote == null || ransomNote.isEmpty()) && (magazine == null || magazine.isEmpty())) 
-    		return true;
-    	
-        int[] cnt = new int[26];
-        for (int i = 0; i < magazine.length(); i++) cnt[magazine.charAt(i) - 97]++;
-		for (int i = 0; i < ransomNote.length(); i++) if (--cnt[ransomNote.charAt(i) - 97] < 0) return false;
-		return true;
+    	int [] hash = new int [256];
+        for (char ch : ransomNote.toCharArray()) hash [ch] ++;
+        for (char ch : magazine.toCharArray()) if (hash [ch] > 0) hash [ch] --;
+        for (int h : hash) if (h > 0) return false;
+        return true;
     }
     
 	// driver method
 	public static void main(String[] args) {
-		_instance.runTest(null, null, true);
 		_instance.runTest("", "", true);
 		_instance.runTest("", "abc", true);
 		_instance.runTest("a", "b", false);
