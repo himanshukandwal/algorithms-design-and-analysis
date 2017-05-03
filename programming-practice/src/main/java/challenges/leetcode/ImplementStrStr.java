f8f49da6f7950262dc45a192f6f3e0a7f4b141c1
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-05-03 16:07:45 -0500

    updated solution for Implement strStr

--- programming-practice/src/main/java/challenges/leetcode/ImplementStrStr.java
+++ programming-practice/src/main/java/challenges/leetcode/ImplementStrStr.java
@@ -21,34 +21,14 @@ public class ImplementStrStr extends AbstractCustomTestRunner {
 	private static ImplementStrStr _instance = new ImplementStrStr();
 	
 	public int _strstr (String haystack, String needle) {
-	    int [] pre = new int [needle.length()];
-	    for (int idx = 1, jdx = 0; idx < needle.length(); idx ++) {
-	        while (jdx > 0 && needle.charAt(idx) != needle.charAt(jdx)) jdx = pre [jdx - 1];
-	        if (needle.charAt(idx) == needle.charAt(jdx)) pre [idx] = ++ jdx;
-	    }
-	        
-	    for (int idx = 0, jdx = 0; idx < haystack.length(); idx ++) {
-	        while (jdx > 0 && haystack.charAt(idx) != needle.charAt(jdx)) jdx = pre [jdx - 1];
-	        
-	        if (haystack.charAt(idx) == needle.charAt(jdx) && ++ jdx == needle.length()) 
-	            return idx - jdx + 1;
-	    }
-	    return -1;
-	}
-
-    public int _strStr(String haystack, String needle) {
-        int res = -1;
-        for (int idx = 0; idx <= haystack.length() - needle.length(); idx ++) {
-            boolean found = true; 
-            for (int jdx = 0; jdx < needle.length(); jdx ++) {
-                if (needle.charAt(jdx) != haystack.charAt(idx + jdx)) {
-                    found = false;
-                    break;
-                }
-            }
+		for (int idx = 0; idx <= haystack.length() - needle.length(); idx ++) {
+            boolean found = true;
+            for (int j = 0; j < needle.length(); j ++)
+                if (haystack.charAt (idx + j) != needle.charAt (j)) { found = false; break; }
+            
             if (found) return idx;
-        }
-        return res;
+        } 
+        return -1;
     }
 
 	// driver method
