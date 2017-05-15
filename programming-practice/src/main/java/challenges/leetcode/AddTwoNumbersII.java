ceda001430535d2e7259325bcc2bd8d750dba536
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-05-14 22:39:46 -0400

    updated solution for add two numbers II

--- programming-practice/src/main/java/challenges/leetcode/AddTwoNumbersII.java
+++ programming-practice/src/main/java/challenges/leetcode/AddTwoNumbersII.java
@@ -30,30 +30,23 @@ public class AddTwoNumbersII extends AbstractCustomTestRunner {
 	}
 	
 	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        Stack<Integer> list1 = new Stack<>(), list2 = new Stack<>(), list3 = new Stack<>();
+		Stack<Integer> list1 = new Stack<>(), list2 = new Stack<>();
         while (l1 != null) { list1.push (l1.val); l1 = l1.next; }
         while (l2 != null) { list2.push (l2.val); l2 = l2.next; }
         
-        int carry = 0;
+        int sum = 0;
+        ListNode list = new ListNode(0);
         while (list1.size() > 0 || list2.size() > 0) {
-            int sum = 0;
-            if (list1.size() > 0 && list2.size() > 0) {
-                sum = list1.pop () + list2.pop () + carry;
-                list3.push (sum % 10);
-            } else if (list1.size() > 0) {
-                sum = list1.pop () + carry;
-                list3.push (sum % 10);
-            } else {
-                sum = list2.pop () + carry;
-                list3.push (sum % 10);
-            }
-            carry = sum / 10;
+            if (!list1.empty()) sum += list1.pop();
+            if (!list2.empty()) sum += list2.pop();
+            
+            list.val = sum % 10;
+            ListNode head = new ListNode (sum / 10);
+            head.next = list;
+            list = head;
+            sum /= 10;
         }
-        if (carry > 0) list3.push (carry);
-        
-        ListNode ans = new ListNode (0), res = ans;
-        while (!list3.isEmpty())  res = res.next = new ListNode (list3.pop ()); 
-        return ans.next;
+        return list.val == 0 ? list.next : list;
     }
 	
 }
