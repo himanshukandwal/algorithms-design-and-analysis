e553d639e03f3723fc772758645c81e38b5ec4cc
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-05-19 01:04:28 -0400

    updated solution for Split Concatenated Strings

--- programming-practice/src/main/java/challenges/leetcode/SplitConcatenatedStrings.java
+++ programming-practice/src/main/java/challenges/leetcode/SplitConcatenatedStrings.java
@@ -42,19 +42,31 @@ public class SplitConcatenatedStrings extends AbstractCustomTestRunner {
 	private static SplitConcatenatedStrings _instance = new SplitConcatenatedStrings();
 
 	public String _splitLoopedString(String[] strs) {
-        for (int idx = 0; idx < strs.length; idx ++) {
-            String reverse = new StringBuilder(strs [idx]).reverse().toString();
-            if (strs [idx].compareTo (reverse) < 0) strs [idx] = reverse;
-        }
-        StringBuilder ans = new StringBuilder ();
-        for (int idx = 0; idx < strs.length; idx ++) ans.append (strs [idx]);
-        
-        String max = ans.toString(), local = max;
-        for (int idx = 0; idx < ans.length(); idx ++) {
-            local = local.substring (1) + local.charAt (0);
-            if (max.compareTo (local) < 0) max = local;
-        }
-        return max;
+		String stot = "";
+	    for (int i = 0; i < strs.length; i++) {
+	        String rever = new StringBuffer(strs[i]).reverse().toString();
+	        if (rever.compareTo(strs[i]) > 0) strs[i] = rever;
+	        stot = stot + strs[i];
+	    }
+	    int start = 0;
+	    String sol = stot;
+	    char maxch = 'a';
+	    for (int i = 0; i < strs.length; i++) {
+	        int n = strs[i].length();
+	        start += n;
+	        String rever = new StringBuffer(strs[i]).reverse().toString();
+	        String other_strs = stot.substring(start) + stot.substring(0, start - n);
+	        for (int j = 0; j < n; j++) {
+	            if (strs[i].charAt(j) - maxch >= 0) {
+	                maxch = strs[i].charAt(j);
+	                String s1 = strs[i].substring(j) + other_strs + strs[i].substring(0, j);
+	                String s2 = rever.substring(n-1-j) + other_strs + rever.substring(0, n-1-j);
+	                if (s1.compareTo(sol) > 0) sol = s1;
+	                if (s2.compareTo(sol) > 0) sol = s2;
+	            }
+	        }
+	    }
+	    return sol;
     }
 	
 	// driver method
