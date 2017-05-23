f299057d99ea0160a32948ad559f0915f2cdc297
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-05-22 21:32:55 -0400

    updated solution for Nuts & Bolts Problem

--- programming-practice/src/main/java/challenges/lintcode/NutsAndBoltsProblem.java
+++ programming-practice/src/main/java/challenges/lintcode/NutsAndBoltsProblem.java
@@ -30,5 +30,52 @@ import challenges.AbstractCustomTestRunner;
  * @author Hxkandwal
  */
 public class NutsAndBoltsProblem extends AbstractCustomTestRunner {
+	public interface NBComparator {
+		 public int cmp(String a, String b);
+	}
+	
+	public void sortNutsAndBolts(String[] nuts, String[] bolts, final NBComparator compare) {
+		if (nuts == null || bolts == null) return;
+		if (nuts.length != bolts.length) return;
+		qsort(nuts, bolts, compare, 0, nuts.length - 1);
+	}
 
+	private void qsort(String[] nuts, String[] bolts, NBComparator compare, int l, int u) {
+		if (l >= u) return;
+
+		// find the partition index for nuts with bolts[l]
+		int part_inx = partition(nuts, bolts[l], compare, l, u);
+		
+		// partition bolts with nuts[part_inx]
+		partition(bolts, nuts[part_inx], compare, l, u);
+		
+		// qsort recursively
+		qsort(nuts, bolts, compare, l, part_inx - 1);
+		qsort(nuts, bolts, compare, part_inx + 1, u);
+	}
+
+	private int partition(String[] str, String pivot, NBComparator compare, int l, int u) {
+		int m = l;
+		for (int i = l + 1; i <= u; i++) {
+			if (compare.cmp(str[i], pivot) == -1 || compare.cmp(pivot, str[i]) == 1) {
+				m++;
+				swap(str, i, m);
+			} else if (compare.cmp(str[i], pivot) == 0 || compare.cmp(pivot, str[i]) == 0) {
+				// swap nuts[l]/bolts[l] with pivot
+				swap(str, i, l);
+				i--;
+			}
+		}
+		
+		// move pivot to proper index
+		swap(str, m, l);
+		return m;
+	}
+
+	private void swap(String[] str, int l, int r) {
+		String temp = str[l];
+		str[l] = str[r];
+		str[r] = temp;
+	}
+	    
 }
