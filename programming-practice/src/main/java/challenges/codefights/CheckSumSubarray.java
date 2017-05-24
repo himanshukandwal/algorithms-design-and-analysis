937d111cac86be0f45c97d844f3dd0e86979b119
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-05-24 10:10:01 -0400

    added solution for Check Sum Sub-array

---
+++ programming-practice/src/main/java/challenges/codefights/CheckSumSubarray.java
@@ -0,0 +1,50 @@
+package challenges.codefights;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import challenges.AbstractCustomTestRunner;
+
+/**
+ * Check Sum Sub-array
+ * 
+ * You are given an array of integers arr and a target k. Your task is to figure out whether you can 
+ * pick a subarray of arr such that its elements add up to k.
+ * 
+ * Example:
+ * 		For arr = [0, 1, 2, 3, 4, 5] and k = 7, the answer should be checkSum(arr, k) = true.
+ * 
+ * 		The sum of the elements of the subarray [3, 4] is exactly 7, so the answer is true.
+ * 
+ * @author Hxkandwal
+ */
+public class CheckSumSubarray extends AbstractCustomTestRunner {
+	
+	private static CheckSumSubarray _instance = new CheckSumSubarray();
+
+	public boolean _checkSumSub(int[] arr, int k) {
+	    int s = 0, z = 0;
+	    Set<Integer> set = new HashSet<>();
+	    for (int e : arr)
+			z |= set.add(s += e) & set.remove(s - k) | s == k ? 1 : 0;
+	    return z != 0;
+	}
+
+	// driver method
+	public static void main(String[] args) {
+		_instance.runTest(new int[] { 0, 1, 2, 3, 4, 5 }, 7, true);
+	}
+
+	public void runTest(final int[] arr, final int k, final boolean expectedOutput) {
+		List<Object> answers = runAll(getClass(), new Object[] { arr, k });
+
+		for (Object answer : answers)
+			assertThat((Boolean) answer).isEqualTo(expectedOutput);
+
+		System.out.println("ok!");
+	}	
+
+}
