18855b6e268fb159b2c887e32c975fa58a447fc1
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-05-25 16:42:05 -0400

    updated solution for Three Sum

--- programming-practice/src/main/java/challenges/leetcode/ThreeSum.java
+++ programming-practice/src/main/java/challenges/leetcode/ThreeSum.java
@@ -51,17 +51,17 @@ public class ThreeSum extends AbstractCustomTestRunner {
 	public List<List<Integer>> _threeSumOther(int[] nums) {
 		Arrays.sort (nums);
         List<List<Integer>> ans = new ArrayList<List<Integer>>();
-        for (int i = 0; i < nums.length - 2; i ++) {
-            if (i == 0 || nums [i - 1] != nums [i]) {
-                int left = i + 1, right = nums.length - 1, target = -nums [i];
+        for (int idx = 0; idx < nums.length - 2; idx ++) {
+            if (idx == 0 || nums [idx] != nums [idx - 1]) {
+                int left = idx + 1, right = nums.length - 1, search = - nums [idx];
                 while (left < right) {
-                    if (nums [left] + nums [right] < target) left ++;
-                    else if (nums [left] + nums [right] > target) right --;
+                    if (nums [left] + nums [right] > search) right --;
+                    else if (nums [left] + nums [right] < search) left ++;
                     else { 
-                        ans.add (Arrays.asList (nums [i], nums [left], nums [right])); 
+                        ans.add (Arrays.asList (nums [idx], nums [left], nums [right])); 
                         while (left < right && nums [left] == nums [left + 1]) left ++;
                         while (left < right && nums [right] == nums [right - 1]) right --;
-                        left ++; right --;
+                        left ++; right --; 
                     }
                 }
             }
