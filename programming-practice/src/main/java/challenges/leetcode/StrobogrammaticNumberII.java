f53d92d0a644eb8e07d9c11e84878d701e55af9e
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-05-24 22:41:47 -0400

    updated solution for Strobogrammatic Number II

--- programming-practice/src/main/java/challenges/leetcode/StrobogrammaticNumberII.java
+++ programming-practice/src/main/java/challenges/leetcode/StrobogrammaticNumberII.java
@@ -23,6 +23,33 @@ public class StrobogrammaticNumberII extends AbstractCustomTestRunner {
 	
 	private static StrobogrammaticNumberII _instance = new StrobogrammaticNumberII();
 
+	public List<String> findStrobogrammaticCleaner(int n) {
+	    return helper(n, n);
+	}
+
+	// working our way outwards. Post order stitching.
+	public List<String> helper(int n, int m) {
+	    if (n == 0) return new ArrayList<String> (Arrays.asList(""));
+	    if (n == 1) return new ArrayList<String> (Arrays.asList("0", "1", "8"));
+	    
+	    List<String> list = helper(n - 2, m);
+	    
+	    List<String> res = new ArrayList<String>();
+	    
+	    for (int i = 0; i < list.size(); i++) {
+	        String s = list.get(i);
+	        
+	        if (n != m) res.add("0" + s + "0");
+	        
+	        res.add("1" + s + "1");
+	        res.add("6" + s + "9");
+	        res.add("8" + s + "8");
+	        res.add("9" + s + "6");
+	    }
+	    
+	    return res;
+	}
+	
 	public List<String> _findStrobogrammatic(int n) {
         List<String> ans = new ArrayList<> ();
         dfs (ans, "", n, 0);
