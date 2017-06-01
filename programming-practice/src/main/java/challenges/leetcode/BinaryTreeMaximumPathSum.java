f841b18d09fb3ad575e7687a08bc1906dd218f9a
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-05-31 22:37:02 -0400

    added solution for Binary Tree Maximum Path Sum

--- programming-practice/src/main/java/challenges/leetcode/BinaryTreeMaximumPathSum.java
+++ programming-practice/src/main/java/challenges/leetcode/BinaryTreeMaximumPathSum.java
@@ -1,5 +1,12 @@
 package challenges.leetcode;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import challenges.AbstractCustomTestRunner;
 
 /**
@@ -22,4 +29,40 @@ import challenges.AbstractCustomTestRunner;
  */
 public class BinaryTreeMaximumPathSum extends AbstractCustomTestRunner {
 
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		
+		public TreeNode(int x) { val = x; }
+	}
+	
+	public int maxPathSum(TreeNode root) {
+		TreeNode dummyHead = new TreeNode(0);
+		Map<TreeNode, List<Set<Integer>>> map = new HashMap<>();
+		map.put(dummyHead, Arrays.asList(new HashSet<>(), new HashSet<>()));
+		return maxPathSumInner(map, dummyHead, root, 0);
+	}
+	    
+    private int maxPathSumInner (Map<TreeNode, List<Set<Integer>>> map, TreeNode parent, TreeNode node, int max) {
+        if (node == null) return max;
+        Set<Integer> fset = new HashSet<>(), sset = new HashSet<>();
+        sset.add (node.val);
+        
+        for (Integer p : map.get (parent).get (0)) { 
+            fset.add (p + node.val); max = Math.max (max, p + node.val);
+        }
+        
+        map.put (node, Arrays.asList (fset, sset));
+        max = maxPathSumInner (map, node, node.left, max);
+        if (node.left == null) return max;
+        for (Integer p : map.get (node.left).get (1)) { 
+            sset.add (p + node.val); 
+            fset.add (p + node.val);
+            max = Math.max (max, p + node.val);
+        }
+        max = maxPathSumInner (map, node, node.right, max);
+        return max;
+    }
+	    
 }
