effef856e7d89dc716d63c3cf78445d7b5be683b
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-06-01 09:28:57 -0400

    added solution for Binary Tree Maximum Path Sum

--- programming-practice/src/main/java/challenges/leetcode/BinaryTreeMaximumPathSum.java
+++ programming-practice/src/main/java/challenges/leetcode/BinaryTreeMaximumPathSum.java
@@ -32,17 +32,17 @@ public class BinaryTreeMaximumPathSum extends AbstractCustomTestRunner {
 
 	public int maxPathSum(TreeNode root) {
 		int [] max = new int [1];
-		max [0] = Integer.MIN_VALUE;
-		maxPathSum (max, root);
-		return max [0];
-	}
-
-	private int maxPathSum(int[] max, TreeNode root) {
-		if (root == null) return 0;
-		int leftMax = Math.max (0, maxPathSum (max, root.left));
-		int rightMax = Math.max (0, maxPathSum (max, root.right));
-		max [0] = Math.max (max [0], root.val + leftMax + rightMax);
-		return root.val + Math.max (leftMax, rightMax);
-	}
+        max [0] = Integer.MIN_VALUE;
+        maxPathSumInner (root, max);
+        return max [0];
+    }
+    
+    private int maxPathSumInner (TreeNode node, int [] max) {
+        if (node == null) return 0;
+        int left = Math.max (0, maxPathSumInner (node.left, max));
+        int right = Math.max (0, maxPathSumInner (node.right, max));
+        max [0] = Math.max (max [0], node.val + left + right);
+        return node.val + Math.max (left, right);
+    }
  	
 }
