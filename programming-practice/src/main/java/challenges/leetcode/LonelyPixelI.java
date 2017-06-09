6da66c7684245dcf3a9443b103b106e30505a128
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-06-09 11:57:46 -0400

    updated solution for Lonely Pixel I

--- programming-practice/src/main/java/challenges/leetcode/LonelyPixelI.java
+++ programming-practice/src/main/java/challenges/leetcode/LonelyPixelI.java
@@ -38,20 +38,20 @@ public class LonelyPixelI extends AbstractCustomTestRunner {
 		int n = picture.length, m = picture[0].length;
 	    
         int firstRowCount = 0;
-        for (int i=0;i<n;i++) 
-            for (int j=0;j<m;j++) 
-                if (picture[i][j] == 'B') {   
-                    if (picture[0][j] < 'Y' && picture[0][j] != 'V') picture[0][j]++;
-                    if (i == 0) firstRowCount++;
-                    else if (picture[i][0] < 'Y' && picture[i][0] != 'V') picture[i][0]++;
+		for (int i = 0; i < n; i ++) 
+			for (int j = 0; j < m; j ++) 
+                if (picture [i][j] == 'B') {   
+                    if (picture [0][j] < 'Y' && picture [0][j] != 'V') picture [0][j]++;
+                    if (i == 0) firstRowCount ++;
+                    else if (picture [i][0] < 'Y' && picture [i][0] != 'V') picture [i][0]++;
                 }
     
         int count = 0;
-        for (int i=0;i<n;i++) 
-            for (int j=0;j<m;j++) 
-                if (picture[i][j] < 'W' && (picture[0][j] == 'C' || picture[0][j] == 'X')) { 
+		for (int i = 0; i < n; i ++) 
+			for (int j = 0; j < m; j ++) 
+				if (picture [i][j] < 'W' && (picture [0][j] == 'C' || picture [0][j] == 'X')) { 
                     if (i == 0) count += firstRowCount == 1 ? 1 : 0;
-                    else if (picture[i][0] == 'C' || picture[i][0] == 'X') count++;
+                    else if (picture [i][0] == 'C' || picture [i][0] == 'X') count ++;
                 }
                     
         return count;
