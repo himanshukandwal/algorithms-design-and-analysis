f1b482636d115a0b63c784a6f5f72339f18fcbe3
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-06-09 19:14:22 -0400

    added solution for Wiggle Sort

--- programming-practice/src/main/java/challenges/leetcode/WiggleSort.java
+++ programming-practice/src/main/java/challenges/leetcode/WiggleSort.java
@@ -28,7 +28,7 @@ public class WiggleSort extends AbstractCustomTestRunner {
 	public int[] _wiggleSort(int[] nums) {
 		for (int i = 1; i < nums.length; i ++) {
 	        int a = nums [i-1];
-			if ((i % 2 == 1) == (a > nums [i])) {
+			if ((i % 2 == 1) == (a > nums [i])) { 		// xor
 				nums [i - 1] = nums [i];
 				nums [i] = a;
 	        }
