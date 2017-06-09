00226e2568706bcdaf9c490e230ce9460269f19d
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-06-08 23:29:33 -0400

    added solution for Lonely Pixel I

--- programming-practice/src/main/java/challenges/leetcode/LonelyPixelI.java
+++ programming-practice/src/main/java/challenges/leetcode/LonelyPixelI.java
@@ -34,20 +34,35 @@ public class LonelyPixelI extends AbstractCustomTestRunner {
 	
 	private static LonelyPixelI _instance = new LonelyPixelI();
 
-	public int findLonelyPixel(char[][] picture) {
-		int[] rows = new int[picture.length];
-
-		for (int row = 0; row < picture.length; row++)
-			for (int col = 0; col < picture[0].length; col++)
-				if (picture[row][col] == 'B') rows[row] = (rows[row] != 0) ? -1 : 1;
-
-		int ans = 0;
-		for (int row = 0; row < rows.length; row ++) ans += (rows[row] > 0) ? 1 : 0;
-		return ans;
+	public int _findLonelyPixel(char[][] picture) {
+		int n = picture.length, m = picture[0].length;
+	    
+        int firstRowCount = 0;
+        for (int i=0;i<n;i++) 
+            for (int j=0;j<m;j++) 
+                if (picture[i][j] == 'B') {   
+                    if (picture[0][j] < 'Y' && picture[0][j] != 'V') picture[0][j]++;
+                    if (i == 0) firstRowCount++;
+                    else if (picture[i][0] < 'Y' && picture[i][0] != 'V') picture[i][0]++;
+                }
+    
+        int count = 0;
+        for (int i=0;i<n;i++) 
+            for (int j=0;j<m;j++) 
+                if (picture[i][j] < 'W' && (picture[0][j] == 'C' || picture[0][j] == 'X')) { 
+                    if (i == 0) count += firstRowCount == 1 ? 1 : 0;
+                    else if (picture[i][0] == 'C' || picture[i][0] == 'X') count++;
+                }
+                    
+        return count;
 	}
 
 	// driver method
 	public static void main(String[] args) {
+		_instance.runTest(new char[][] { "WWB".toCharArray(),
+			 							 "WBW".toCharArray(),
+			 							 "BWW".toCharArray() }, 3);
+		
 		_instance.runTest(new char[][] { "BBWBWBBBBW".toCharArray(),
 										 "WBWBBWWWWB".toCharArray(),
 										 "WBBBWWBWWB".toCharArray(),
