4d695d7b88f7ce96f9b262ee752b3a0b39568fa6
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-06-10 00:42:31 -0400

    updated solution for Decode String

--- programming-practice/src/main/java/challenges/leetcode/DecodeString.java
+++ programming-practice/src/main/java/challenges/leetcode/DecodeString.java
@@ -27,30 +27,30 @@ import challenges.AbstractCustomTestRunner;
  */
 public class DecodeString extends AbstractCustomTestRunner {
 
-	 public String decodeString(String s) {
-        if (s.length () == 0) return "";
-        Stack<Object[]> stk = new Stack<>();
-        
-        StringBuilder ans = new StringBuilder();
-        Integer cnum = null; StringBuilder cbuild = new StringBuilder();
-        for (int idx = 0; idx < s.length(); idx ++) {
-            char ch = s.charAt(idx);
-            
-            if (Character.isDigit(ch))
-                cnum = (ch - '0') + ((cnum == null) ? 0 : 10 * cnum); 
-            else if (ch == '[') {
-                stk.push (new Object[] { cnum, cbuild = new StringBuilder() });
-                cnum = null;
-            } else if (ch == ']') {
-                int rep = (Integer) stk.pop () [0];
-                String str = cbuild.toString();
-                for (int loop = 0; loop < rep - 1; loop ++) cbuild.append (str);
-                if (!stk.isEmpty())
-                    cbuild = ((StringBuilder) stk.peek()[1]).append (cbuild.toString());
-                else ans.append(cbuild.toString());
-            } else { if (!stk.isEmpty()) cbuild.append (ch); else ans.append (ch); }
-        }
-        return ans.toString();
-    }
+	public String decodeString(String s) {
+		StringBuilder ans = new StringBuilder();
+		Stack<Object[]> stack = new Stack<>();
+		int idx = 0, num = 0;
+		while (idx < s.length()) {
+			char ch = s.charAt(idx ++);
+			if (ch >= '0' && ch <= '9') num = num * 10 + (ch - '0');
+			else {
+				if (ch == '[') stack.push(new Object[] { num, "" });
+				else if (ch == ']') {
+					int val = (Integer) stack.peek()[0];
+					String str = (String) stack.pop()[1];
+					StringBuilder sb = new StringBuilder();
+					while (val-- > 0) sb.append(str);
+					if (stack.isEmpty()) ans.append(sb.toString());
+					else stack.peek()[1] = (String) stack.peek()[1] + sb.toString();
+				} else {
+					if (stack.isEmpty()) ans.append(String.valueOf(ch));
+					else stack.peek()[1] = (String) stack.peek()[1] + String.valueOf(ch);
+				}
+				num = 0;
+			}
+		}
+		return ans.toString();
+	}
 	
 }
