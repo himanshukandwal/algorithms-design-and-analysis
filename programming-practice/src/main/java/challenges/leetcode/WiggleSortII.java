2b4df30106f31eb7950344bc4d205ece6c8d96e6
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-06-12 16:13:30 -0400

    added solution for Wiggle Sort II

---
+++ programming-practice/src/main/java/challenges/leetcode/WiggleSortII.java
@@ -0,0 +1,60 @@
+package challenges.leetcode;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.List;
+
+import challenges.AbstractCustomTestRunner;
+
+/**
+ * 324. Wiggle Sort II
+ * 
+ * Given an unsorted array nums, reorder it such that nums[0] < nums[1] > nums[2] < nums[3]....
+ * 
+ * Example:
+ * 		(1) Given nums = [1, 5, 1, 1, 6, 4], one possible answer is [1, 4, 1, 5, 1, 6].
+ * 
+ *  	(2) Given nums = [1, 3, 2, 2, 3, 1], one possible answer is [2, 3, 1, 3, 1, 2].
+ *  
+ * Note: You may assume all input has valid answer.
+ * 
+ * Follow Up: Can you do it in O(n) time and/or in-place with O(1) extra space?
+ * 
+ * @author Hxkandwal
+ */
+public class WiggleSortII extends AbstractCustomTestRunner {
+	
+	private static WiggleSortII _instance = new WiggleSortII();
+
+	public int[] _wiggleSort(int[] nums) {
+		for (int idx = 1, oidx = 0; idx < nums.length; idx ++) {
+			while (idx < nums.length - 1 && (nums [oidx] == nums [idx] || (oidx % 2 == 1) == (nums [oidx] < nums [idx]))) idx ++;
+			swap (nums, idx, oidx + 1);
+			idx = ++ oidx;
+        }
+        return nums;
+    }
+    
+    private void swap (int [] nums, int to, int from) {
+        int val = nums [from];
+        nums [from] = nums [to];
+        nums [to] = val;
+    }
+
+	// driver method
+	public static void main(String[] args) {
+		_instance.runTest(new int[] { 1, 3, 2, 2, 3, 1 }, new int[] { 2, 3, 1, 3, 1, 2 });
+		_instance.runTest(new int[] { 4, 5, 5, 6 }, new int[] { 4, 5, 6, 5 });	
+		_instance.runTest(new int[] { 1, 2, 2, 1, 2, 1, 1, 1, 1, 2, 2, 2 }, new int[] { 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2 });
+	}
+
+	public void runTest(final int[] nums, final int[] expectedOutput) {
+		List<Object> answers = runAll(getClass(), new Object[] { nums });
+
+		for (Object answer : answers)
+			assertThat((int []) answer).isEqualTo(expectedOutput);
+		
+		System.out.println("ok!");
+	}
+	
+}
