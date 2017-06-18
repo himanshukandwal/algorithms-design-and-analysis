61217ff9a2500d0ee77cbc2a4a8a13a658ef5673
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-06-18 10:28:06 -0500

    updated solution for Minimum Factorization

--- programming-practice/src/main/java/challenges/leetcode/MinimumFactorization.java
+++ programming-practice/src/main/java/challenges/leetcode/MinimumFactorization.java
@@ -2,7 +2,6 @@ package challenges.leetcode;
 
 import static com.google.common.truth.Truth.assertThat;
 
-import java.util.Arrays;
 import java.util.List;
 
 import challenges.AbstractCustomTestRunner;
@@ -29,20 +28,33 @@ public class MinimumFactorization extends AbstractCustomTestRunner {
 	
 	private static MinimumFactorization _instance = new MinimumFactorization();
 	
+	/**
+	 * We try to decompose the problem in form of a tree, starting from the input number that can be branched 
+	 * in 2-9 ways (as the branch with value 1 means the input number itself). Our goal is to find the shortest 
+	 * path from the root to the leaf, while generating the minimal number. 
+	 * We try branching from the highest number first, as then we will construct the shortest path.
+	 *
+	 *	      48
+	 *	     /|\  
+	 *	    8 6 4.. 2  
+	 *	   /  |  \
+	 *	  6   8  6
+	 *           |
+	 *           2
+	 * 
+	 * Also assuming that if a number is divisible by 8, then no need to divide further by 4 and 2 (factors) and 
+	 * this is done by maintaining information about all the previous numbers seen, using a seen variable.
+	 */
 	public int _smallestFactorization(int a) {
-        if (a <= 1) return a;
+		if (a <= 1) return a;
         long ans = Long.MAX_VALUE;
-        
-        boolean [] map = new boolean [10];
-        Arrays.fill (map, true);
-        map [0] = map [1] = false;
-        
+            
+        int seen = 1;
         for (int idx = 9; idx > 1; idx --) 
-            if (map [idx] && a % idx == 0) {
-            	int val = _smallestFactorization (a/idx);
+            if (seen % idx != 0 && a % idx == 0) {
+                seen *= idx;
+                int val = _smallestFactorization (a/idx);
                 if (val > 0) ans = Math.min (ans, val == 1 ? idx : 10l * val + idx);
-                
-                for (int jdx = 2; jdx < idx; jdx ++) if (map [jdx]) map [jdx] = idx % jdx != 0;
             }
         
         return (ans == Long.MAX_VALUE || ans > Integer.MAX_VALUE) ? 0 : (int) ans;
