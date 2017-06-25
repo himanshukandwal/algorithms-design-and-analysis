c7313b02f96d906a669afebe9378cf933843d6c8
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-06-24 19:44:06 -0500

    added solution for Longest Palindromic Substring

--- programming-practice/src/main/java/challenges/leetcode/LongestPalindromicSubstring.java
+++ programming-practice/src/main/java/challenges/leetcode/LongestPalindromicSubstring.java
@@ -52,15 +52,15 @@ public class LongestPalindromicSubstring extends AbstractCustomTestRunner {
 		int len = s.length();
 		if (len < 2) return s;
 		
-	    for (int i = 0; i < len-1; i++) {
-	     	extendPalindrome(s, i, i);  //assume odd length, try to extend Palindrome as possible
-	     	extendPalindrome(s, i, i+1); //assume even length.
+		for (int i = 0; i < len - 1; i++) {
+	     	extendPalindrome (s, i, i);  //assume odd length, try to extend Palindrome as possible
+	     	extendPalindrome (s, i, i+1); //assume even length.
 	    }
 	    return s.substring(lo, lo + maxLen);
 	}
 
 	private void extendPalindrome(String s, int j, int k) {
-		while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) { j--; k++; }
+		while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) { j --; k ++; }
 		if (maxLen < k - j - 1) {
 			lo = j + 1;
 			maxLen = k - j - 1;
