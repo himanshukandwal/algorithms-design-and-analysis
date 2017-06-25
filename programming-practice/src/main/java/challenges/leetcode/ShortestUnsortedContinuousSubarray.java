3de794419a59db8c4b2fe66bdb5a56e1c87649a3
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-06-25 18:35:13 -0500

    added solution for Shortest Unsorted Continuous Subarray

---
+++ programming-practice/src/main/java/challenges/leetcode/ShortestUnsortedContinuousSubarray.java
@@ -0,0 +1,42 @@
+package challenges.leetcode;
+
+import challenges.AbstractCustomTestRunner;
+
+/**
+ * 581. Shortest Unsorted Continuous Subarray
+ * 
+ * Given an integer array, you need to find one continuous subarray that if you only sort this subarray in ascending order, then the 
+ * whole array will be sorted in ascending order, too.
+ * 
+ * You need to find the shortest such subarray and output its length.
+ * 
+ * Example 1:
+ * 		Input: [2, 6, 4, 8, 10, 9, 15]
+ * 		Output: 5
+ * 		Explanation: You need to sort [6, 4, 8, 10, 9] in ascending order to make the whole array sorted in ascending order.
+ * 
+ * Note:
+ * 	-	Then length of the input array is in range [1, 10,000].
+ * 	-	The input array may contain duplicates, so ascending order here means <=.
+ * 
+ * @author Hxkandwal
+ */
+public class ShortestUnsortedContinuousSubarray extends AbstractCustomTestRunner {
+
+	// get narrower and narrower
+	public int findUnsortedSubarray(int[] nums) {
+        int start = 0, end = nums.length - 1;
+        while (start < end) {
+            int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+            for (int idx = start; idx <= end; idx ++) {
+                min = Math.min (min, nums [idx]);
+                max = Math.max (max, nums [idx]);
+            }
+            if (nums [start] == min) start ++;
+            else if (nums [end] == max) end --;
+            else break;
+        }
+        return start >= end ? 0 : end - start + 1;
+    }
+	
+}
