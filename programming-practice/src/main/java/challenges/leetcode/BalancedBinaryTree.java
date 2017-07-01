7f09f09214ba0b142556580e23af3d02a7e9ddee
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-07-01 15:28:10 -0500

    updated solution for Balanced Binary Tree

--- programming-practice/src/main/java/challenges/leetcode/BalancedBinaryTree.java
+++ programming-practice/src/main/java/challenges/leetcode/BalancedBinaryTree.java
@@ -16,21 +16,18 @@ public class BalancedBinaryTree extends AbstractCustomTestRunner {
 	
 	public static class TreeNode {
 		int val;
-		TreeNode left;
-		TreeNode right;
-		
+		TreeNode left, right;
 		public TreeNode(int x) { val = x; }
 	}
 	
-	public boolean isBalanced(TreeNode root) {
-        if (root == null) return true;
-        if (Math.abs (maxDepth (root.left) - maxDepth (root.right)) > 1) return false;
-        return isBalanced (root.left) && isBalanced(root.right);
+	private int height (TreeNode node) {
+        if (node == null) return 0;
+        return Math.max (height (node.left), height (node.right)) + 1;
     }
     
-    private int maxDepth(TreeNode node) {
-        if (node == null) return 0;
-        return Math.max(maxDepth (node.left), maxDepth (node.right)) + 1;
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+        return Math.abs (height (root.left) - height (root.right)) <= 1 && isBalanced (root.left) && isBalanced (root.right);
     }
-
+    
 }
