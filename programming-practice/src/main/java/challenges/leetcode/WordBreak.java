0e8ddc3530b1067a4e7d6066c25ebfc3271aeb64
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-07-02 22:03:54 -0500

    added solution for Word Break

--- programming-practice/src/main/java/challenges/leetcode/WordBreak.java
+++ programming-practice/src/main/java/challenges/leetcode/WordBreak.java
@@ -2,6 +2,7 @@ package challenges.leetcode;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -9,7 +10,7 @@ import java.util.Set;
 import challenges.AbstractCustomTestRunner;
 
 /**
- * 140. Word Break II
+ * 139. Word Break
  * 
  * Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, determine if s can be segmented into a space-separated 
  * sequence of one or more dictionary words. You may assume the dictionary does not contain duplicate words.
@@ -19,40 +20,31 @@ import challenges.AbstractCustomTestRunner;
  * 		Return true because "leetcode" can be segmented as "leet code".
  * 
  * @author Hxkandwal
- *
  */
-@SuppressWarnings({ "unchecked", "rawtypes", "serial" })
 public class WordBreak extends AbstractCustomTestRunner {
 	
 	private static WordBreak _instance = new WordBreak();
 	
-    public static boolean _wordBreak(Set<String> hash, String s) {
-    	boolean[] f = new boolean [s.length() + 1];
-    	f [0] = true;
-    	
-    	for (int idx = 1; idx < f.length; idx ++) 
-    		for (int innerIdx = idx - 1; innerIdx >= 0; innerIdx --)
-    			if (f [idx] = (f [innerIdx] && hash.contains(s.substring (innerIdx, idx))))
-    				break;
-		
-		return f [f.length - 1];    
+	public boolean _wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>();
+        for (String str : wordDict) set.add (str);
+        boolean [] f = new boolean [s.length() + 1];
+        f [0] = true;
+        for (int idx = 1; idx <= s.length (); idx ++) {
+            for (int jdx = 0; jdx < idx; jdx ++)
+                if (f [jdx]) f [idx] |= set.contains (s.substring (jdx, idx));
+        }
+        return f [s.length()];
     }
 
 	// driver method
 	public static void main(String[] args) {
-		_instance.runTest(new HashSet() {{ add("ap"); add("ple"); }}, "apple", true);
-		_instance.runTest(new HashSet() {{ add("ale"); add("apple"); add("monkey"); add("plea"); }}, "apple", true);
-		_instance.runTest(new HashSet() {{ add("ale"); add("apple"); add("monkey"); add("plea"); }}, "banana", false);
-		_instance.runTest(new HashSet() {{ add("i"); add("like"); add("sam"); add("sung"); 
-										   add("samsung"); add("mobile"); add("ice"); add("go");
-										   add("cream"); add("icecream"); add("man"); add("mango"); }}, "ilikesamsung", true);
-		_instance.runTest(new HashSet() {{ add("i"); add("like"); add("sam"); add("sung"); 
-										   add("samsung"); add("mobile"); add("ice"); add("go");
-										   add("cream"); add("icecream"); add("man"); add("mango"); }}, "ilikesamsungOne", false);		
+		_instance.runTest("apple", Arrays.asList ("ap", "ple"), true);
+		_instance.runTest("leetcode", Arrays.asList ("leet", "code"), true);		
 	}
 
-	public void runTest(final Set<String> dictionary, final String input, final boolean expectedOutput) {
-		List<Object> answers = runAll(getClass(), new Object[] { dictionary, input });
+	public void runTest(final String s, final List<String> wordDict, final boolean expectedOutput) {
+		List<Object> answers = runAll(getClass(), new Object[] { s, wordDict });
 
 		for (Object answer : answers)
 			assertThat((Boolean) answer).isEqualTo(expectedOutput);
