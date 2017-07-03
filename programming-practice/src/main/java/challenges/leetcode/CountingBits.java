9473bfb6877b729a92750e71d2280884ac47bb81
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-07-02 21:36:33 -0500

    added solution for Counting Bits

--- programming-practice/src/main/java/challenges/leetcode/CountingBits.java
+++ programming-practice/src/main/java/challenges/leetcode/CountingBits.java
@@ -9,15 +9,17 @@ import challenges.AbstractCustomTestRunner;
 /**
  * 338. Counting Bits
  * 
- * Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their binary representation 
- * and return them as an array.
+ * Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their 
+ * binary representation and return them as an array.
  * 
  * Example:
  * 		For num = 5 you should return [0,1,1,2,1,2].
- * 		Follow up:
- * 			It is very easy to come up with a solution with run time O(n*sizeof(integer)). But can you do it in linear time O(n) /possibly in a single pass?
- * 			Space complexity should be O(n).
- * 			Can you do it like a boss? Do it without using any builtin function like __builtin_popcount in c++ or in any other language.
+ * 
+ * Follow up:
+ * 	-	It is very easy to come up with a solution with run time O(n*sizeof(integer)). But can you do it in linear time O(n)/
+ * 		possibly in a single pass?
+ * 	-	Space complexity should be O(n).
+ * 	-	Can you do it like a boss? Do it without using any builtin function like __builtin_popcount in c++ or in any other language.
  * 
  * @author Hxkandwal
  */
@@ -25,20 +27,13 @@ public class CountingBits extends AbstractCustomTestRunner {
 	
 	private static CountingBits _instance  = new CountingBits();
 	
-	private CountingBits() {}
-	
-	public static int[] _countBits(int num) {
-        int [] ans = new int [num + 1];
-        ans [0] = 0;
-        
-        for (int n = 1; n <= num; n ++) {
-        	int count = 0;
-        	for (int pos = 31; pos >= 0; pos --)
-        		count += (n >>> pos & 1) == 1 ? 1 : 0;
-        	
-        	ans [n] = count; 
-		}
-        
+	public int[] countBits(int num) {
+        int[] ans = new int [num + 1];
+        if (num >= 1) ans [1] = 1;
+        for (int idx = 2, pow = 1; idx <= num; idx ++) {
+            if (pow * 2 == idx) pow *= 2;
+            ans [idx] = 1 + ans [idx - pow];
+        }
         return ans;
     }
     
