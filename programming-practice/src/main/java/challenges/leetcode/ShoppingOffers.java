25a3a7e39d652663ff11c7b98ba770b0e515c566
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-07-11 22:53:41 -0500

    added solution for Shopping Offers

--- programming-practice/src/main/java/challenges/leetcode/ShoppingOffers.java
+++ programming-practice/src/main/java/challenges/leetcode/ShoppingOffers.java
@@ -1,5 +1,10 @@
 package challenges.leetcode;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import challenges.AbstractCustomTestRunner;
 
 /**
@@ -42,4 +47,35 @@ import challenges.AbstractCustomTestRunner;
  */
 public class ShoppingOffers extends AbstractCustomTestRunner {
 
+	public int shoppingOffers(List<Integer> price, List<List<Integer>> special, List<Integer> needs) {
+        return dfs (new HashMap<> (), price, special, needs);
+    }
+    
+    private int dfs (Map<String, Integer> map, List<Integer> price, List<List<Integer>> special, List<Integer> needs) {
+        if (map.containsKey (needs.toString())) return map.get (needs.toString());
+        
+        int sum = Integer.MAX_VALUE;
+        for (List<Integer> offer : special) {
+            List<Integer> reducedNeeds = reduce (offer, needs);
+            if (reducedNeeds != null) 
+                sum = Math.min (sum, offer.get (offer.size () - 1) + dfs (map, price, special, reducedNeeds));
+        }
+        
+        if (sum == Integer.MAX_VALUE) {
+            sum = 0;
+            for (int idx = 0; idx < needs.size(); idx ++) sum += needs.get (idx) * price.get (idx);
+        }
+        
+        map.put (needs.toString(), sum);
+        return sum;
+    }
+    
+    private List<Integer> reduce (List<Integer> offer, List<Integer> needs) {
+        List<Integer> reducedNeeds = new ArrayList<>();
+        for (int idx = 0; idx < needs.size (); idx ++) 
+            if (offer.get (idx) > needs.get (idx)) return null;
+            else reducedNeeds.add (needs.get (idx) - offer.get (idx));
+        return reducedNeeds;
+    }
+    
 }
