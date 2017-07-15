e14f14c3607ebf32b16c3196e2e5e354f68cefa4
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-07-14 23:26:53 -0500

    added solution for Distinct Subsequences

--- programming-practice/src/main/java/challenges/leetcode/DistinctSubsequences.java
+++ programming-practice/src/main/java/challenges/leetcode/DistinctSubsequences.java
@@ -25,13 +25,11 @@ public class DistinctSubsequences extends AbstractCustomTestRunner {
 	private static DistinctSubsequences _instance = new DistinctSubsequences();
 
 	public int _numDistinct(String s, String t) {
-		if (t.length() > s.length()) return 0;
-     	int [][] dp = new int [t.length() + 1][s.length() + 1];   
-    	for (int row = 0; row < t.length(); row ++)  
-    		for (int col = 0; col < s.length (); col ++)
-    			if (t.charAt (row) == s.charAt (col)) dp [row + 1][col + 1] = (row == 0 ? dp [row + 1][col] + 1 : dp [row + 1][col] + dp [row][col]);
-    			else dp [row + 1][col + 1] = dp [row + 1][col];
-        return dp [t.length()][s.length()];
+		int [][] dp = new int [t.length () + 1][s.length () + 1];
+        for (int row = 0; row < t.length (); row ++) 
+            for (int col = 0; col < s.length (); col ++)
+                dp [row + 1][col + 1] = dp [row + 1][col] + (t.charAt (row) == s.charAt (col) ? (row == 0 ? 1 : dp [row][col]) : 0);
+        return dp [t.length ()][s.length ()];
     }
 
 	// driver method
