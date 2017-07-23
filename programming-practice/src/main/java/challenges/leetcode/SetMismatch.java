d9c59e9a8274ddd3767f509882c90c69aaa5b3ff
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-07-23 13:51:04 -0500

    added solution for Set Mismatch

--- programming-practice/src/main/java/challenges/leetcode/SetMismatch.java
+++ programming-practice/src/main/java/challenges/leetcode/SetMismatch.java
@@ -1,6 +1,8 @@
 package challenges.leetcode;
 
-import java.util.Arrays;
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.List;
 
 import challenges.AbstractCustomTestRunner;
 
@@ -24,18 +26,45 @@ import challenges.AbstractCustomTestRunner;
  * @author Hxkandwal
  */
 public class SetMismatch extends AbstractCustomTestRunner {
+	
+	private static SetMismatch _instance = new SetMismatch();
 
-	public int[] findErrorNums(int[] nums) {
-        int [] ans = { -1, -1 };
-        Arrays.sort (nums);
-        for (int idx = 0; idx < nums.length; idx ++) {
-            if (idx > 0) {
-                if (nums [idx - 1] == nums [idx]) ans [0] = nums [idx];
-                else if (nums [idx] != nums [idx - 1] + 1) ans [1] = nums [idx - 1] + 1;
-            }
-        }
-        if (ans [1] < 0) ans [1] = (nums [0] == 1) ? nums.length : 1;
-        return ans;
+	/*
+	 *  put each element k to the k-1th position unless the k-1th is already occupied by k. 
+	 *  In that case we know k is a duplicate. In a second pass, we look for the ith position 
+	 *  where its value is not i+1, we know i+1 is the missing value.
+	 */
+	public int[] _findErrorNums(int[] nums) {
+		for (int i = 0; i < nums.length; i ++) 
+			while (nums [nums [i] - 1] != nums [i]) 
+				swap (nums, i, nums[i] - 1);
+		
+        for (int i = 0; i < nums.length; i ++) 
+			if (nums[i] != i + 1) return new int[] { nums [i], i + 1 };
+        
+        return null;
     }
+	
+	private void swap (int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+
+	// driver method
+	public static void main(String[] args) {
+		_instance.runTest(new int[] { 1, 2, 2, 4 }, new int[] { 2, 3 });
+		_instance.runTest(new int[] { 2, 2 }, new int[] { 2, 1 });
+		_instance.runTest(new int[] { 4, 3, 2, 2, 1 }, new int[] { 2, 5 });
+	}
+
+	public void runTest(final int [] nums, final int [] expectedOutput) {
+		List<Object> answers = runAll(getClass(), new Object[] { nums });
+
+		for (Object answer : answers)
+			assertThat((int []) answer).isEqualTo(expectedOutput);
+
+		System.out.println("ok!");
+	}
 	
 }
