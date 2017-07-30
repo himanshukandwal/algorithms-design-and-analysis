f5254f3064464b2203cfb428b88da45d65f3cab9
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-07-29 20:16:39 -0500

    initial draft for Bear and Different Names

---
+++ programming-practice/src/main/java/challenges/codeforces/vk_cup_2017/BearAndDifferentNames.java
@@ -0,0 +1,13 @@
+package challenges.codeforces.vk_cup_2017;
+
+/**
+ * A. Bear and Different Names
+ * 
+ * link: http://codeforces.com/contest/790/problem/A
+ * 
+ * @author Hxkandwal
+ */
+public class BearAndDifferentNames {
+
+	
+}
