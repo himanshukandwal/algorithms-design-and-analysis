7f71d4d3ad5e0306a5d900190c71c0acb7580650
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-07-30 22:13:11 -0500

    added solution for 2 Keys Keyboard

--- programming-practice/src/main/java/challenges/leetcode/TwoKeysKeyboard.java
+++ programming-practice/src/main/java/challenges/leetcode/TwoKeysKeyboard.java
@@ -60,11 +60,11 @@ public class TwoKeysKeyboard extends AbstractCustomTestRunner {
 
     // another approach
     public int minSteps(int n) {
-        int[] dp = new int[n+1];
-        for (int i = 2; i <= n; i++) {
-            dp[i] = i;
-            for (int j = i-1; j > 1; j--) {
-                if (i % j == 0) { dp[i] = dp[j] + (i/j); break; }
+        int [] dp = new int [n + 1];
+        for (int i = 2; i <= n; i ++) {
+            dp [i] = i;
+            for (int j = i - 1; j > 1; j --) {
+                if (i % j == 0) { dp [i] = dp [j] + (i / j); break; }
             }
         }
         return dp[n];
