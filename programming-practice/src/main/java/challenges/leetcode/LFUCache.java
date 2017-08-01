cbc0c2f14952b65725202f8738e2072af73e2727
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-08-01 01:15:07 -0500

    added solution for LFU Cache

--- programming-practice/src/main/java/challenges/leetcode/LFUCache.java
+++ programming-practice/src/main/java/challenges/leetcode/LFUCache.java
@@ -2,10 +2,7 @@ package challenges.leetcode;
 
 import static com.google.common.truth.Truth.assertThat;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import challenges.AbstractCustomTestRunner;
 
@@ -35,66 +32,52 @@ import challenges.AbstractCustomTestRunner;
  * 		cache.get(4);       // returns 4
  */
 public class LFUCache extends AbstractCustomTestRunner {
-	
-	int min, capacity;
-    Map<Integer, Integer> map;
-    Map<Integer, Map<Integer, Integer>> freq;
+
+    private Map<Integer, Integer> keyFreqMap = new HashMap <>();
+    private Map<Integer, LinkedHashMap<Integer, Integer>> freqMap = new HashMap <>();
+    private int size, minfrequency;
     
     public LFUCache(int capacity) {
-        this.min = 0;
-        this.map = new HashMap<>();
-        this.freq = new HashMap<>();
-        this.capacity = capacity;
+        this.size = capacity;
     }
     
-    private int update(int key, int v) {
-        int count = map.get(key);
-        Map<Integer, Integer> values = freq.get(count);
-        
-        //remove the key from values map
-        int ans = values.remove(key);
-        
-        //values map is empty remove freq bucket and update min
-        if (values.size() == 0) {
-            if (min == count) min = count + 1;
-            freq.remove(count);
+    private int update(int key, int value) {
+        int freq = keyFreqMap.get (key);
+        LinkedHashMap <Integer, Integer> map = freqMap.get (freq);
+        int oValue = map.remove (key);
+
+        if (map.size () == 0) {
+            if (minfrequency == freq) minfrequency = freq + 1;
+            freqMap.remove (freq);
         }
-        
-        count ++;
-        
-        //create new freq bucket and add key and ans to its values
-        if (!freq.containsKey(count)) freq.put(count, new LinkedHashMap<>());
-        freq.get(count).put(key, v == Integer.MIN_VALUE ? ans : v);
-        map.put(key, count);
-        return ans;
+
+        freq ++;
+        keyFreqMap.put (key, freq);
+        freqMap.computeIfAbsent (freq, k -> new LinkedHashMap<>()).put (key, value == Integer.MAX_VALUE ? oValue : value);
+        return value == Integer.MAX_VALUE ? oValue : value;
     }
     
     public int get(int key) {
-        if (!map.containsKey(key) || capacity == 0) return -1;
-        return update(key, Integer.MIN_VALUE);
+        return (keyFreqMap.containsKey(key)) ? update (key, Integer.MAX_VALUE) : -1;
     }
     
     public void put(int key, int value) {
-        if (capacity == 0) return;
-        if (map.containsKey(key)) {
-            update(key, value);
-            return;
+        if (size == 0) return;
+        if (keyFreqMap.containsKey (key)) update (key, value);
+        else {
+            if (keyFreqMap.size () == size) {
+                LinkedHashMap<Integer, Integer> map = freqMap.get (minfrequency);
+                Iterator<Integer> itr = map.keySet ().iterator();
+                int rkey = itr.next ();
+                itr.remove ();
+                keyFreqMap.remove (rkey);
+
+                if (map.size () == 0) freqMap.remove (minfrequency); minfrequency = -1;
+            }
+            minfrequency = 1;
+            keyFreqMap.put (key, 1);
+            freqMap.computeIfAbsent (1, k -> new LinkedHashMap<>()).put (key, value);
         }
-        if (map.size() == capacity) {
-            Map<Integer, Integer> values = freq.get(min);
-            Integer firstKey = values.keySet().iterator().next();
-            values.remove(firstKey);
-        
-            //values map is empty remove freq bucket
-            if(values.size() == 0) freq.remove(min);
-            map.remove(firstKey);
-        }
-        
-        //create new freq bucket and add key and value to its values
-        if (!freq.containsKey(1)) freq.put(1, new LinkedHashMap<>());
-        freq.get(1).put(key, value);
-        map.put(key, 1);
-        min = 1;
     }
     
     // driver method
@@ -102,23 +85,15 @@ public class LFUCache extends AbstractCustomTestRunner {
  		LFUCache cache = new LFUCache(2);
  		cache.put(1, 1);
  		cache.put(2, 2);
- 		System.out.println(cache.get(1));
+        assertThat(cache.get(1)).isEqualTo(1);
  		cache.put(3, 3);
- 		System.out.println(cache.get(2));
- 		System.out.println(cache.get(3));
+        assertThat(cache.get(2)).isEqualTo(-1);
+        assertThat(cache.get(3)).isEqualTo(3);
  		cache.put(4, 4);
- 		System.out.println(cache.get(1));
- 		System.out.println(cache.get(3));
- 		System.out.println(cache.get(4));
- 	}
-
- 	public void runTest(final int[] coins, final int amount, final int expectedOutput) {
- 		List<Object> answers = runAll(getClass(), new Object[] { coins, amount });
-
- 		for (Object answer : answers)
- 			assertThat((Integer) answer).isEqualTo(expectedOutput);
- 		
- 		System.out.println("ok!");
+        assertThat(cache.get(1)).isEqualTo(-1);
+        assertThat(cache.get(3)).isEqualTo(3);
+        assertThat(cache.get(4)).isEqualTo(4);
+        System.out.println("ok!");
  	}
      
 }
