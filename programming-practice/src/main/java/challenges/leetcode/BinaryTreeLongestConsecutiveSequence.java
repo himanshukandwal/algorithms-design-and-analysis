696f6d935cf74e3f58b330602c0af4d202fe15ce
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-08-12 11:28:48 -0500

    added solution for Binary Tree Longest Consecutive Sequence

--- programming-practice/src/main/java/challenges/leetcode/BinaryTreeLongestConsecutiveSequence.java
+++ programming-practice/src/main/java/challenges/leetcode/BinaryTreeLongestConsecutiveSequence.java
@@ -3,10 +3,33 @@ package challenges.leetcode;
 import challenges.AbstractCustomTestRunner;
 
 /**
+ * 298. Binary Tree Longest Consecutive Sequence
+ *
  * Given a binary tree, find the length of the longest consecutive sequence path.
  * 
- * The path refers to any sequence of nodes from some starting node to any node in the tree along the parent-child connections. 
+ * The path refers to any sequence of nodes from some starting node to any node in the tree along the parent-child connections.
  * The longest consecutive path need to be from parent to child (cannot be the reverse).
+ *
+ * For example,
+ *                 1
+ *                  \
+ *                  3
+ *                 / \
+ *                2   4
+ *                    \
+ *                    5
+ *
+ * Longest consecutive sequence path is 3-4-5, so return 3.
+ *
+ *                  2
+ *                   \
+ *                   3
+ *                  /
+ *                 2
+ *                /
+ *               1
+ *
+ * Longest consecutive sequence path is 2-3,not3-2-1, so return 2.
  * 
  * @author Hxkandwal
  */
@@ -20,30 +43,19 @@ public class BinaryTreeLongestConsecutiveSequence extends AbstractCustomTestRunn
 		public TreeNode(int x) { val = x; }
 	}
 	
-	public int longestConsecutive(TreeNode root) {
-        if (root == null) return 0;
-        return recurser (root, 1, 1);
+	public int _longestConsecutive(TreeNode root) {
+        int [] ans = new int [1];
+        if (root != null) longestConsecutiveInner (root, ans);
+        return ans [0];
     }
-    
-    private int recurser(TreeNode node, int current, int max) {
-        if (node.left == null && node.right == null) return max;
-        int lValue = 0, rValue = 0;
-        
-        if (node.left != null) {
-            if (node.left.val == node.val + 1) 
-                lValue = recurser (node.left, current + 1, Math.max(current + 1, max));
-            else
-                lValue = recurser (node.left, 1, Math.max(current, max));
-        }
-        
-        if (node.right != null) {
-            if (node.right.val == node.val + 1)
-                rValue = recurser (node.right, current + 1, Math.max(current + 1, max));
-            else
-                rValue = recurser (node.right, 1, Math.max(current, max));
-        }
-        
-        return Math.max (lValue, rValue);
+
+    public int longestConsecutiveInner (TreeNode node, int [] ans) {
+        if (node == null) return 0;
+        int left = longestConsecutiveInner (node.left, ans), right = longestConsecutiveInner (node.right, ans);
+        if (node.left  == null || node.val + 1 == node.left.val ) left ++; else left = 1;           // default value is 1 (for that node)
+        if (node.right == null || node.val + 1 == node.right.val) right ++; else right = 1;
+        ans [0] = Math.max (ans [0], Math.max (left, right));
+        return Math.max (left, right);
     }
 	
 }
