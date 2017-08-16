b7ae2eadf4c6e06baaab093e97a9bf47dda1b6a1
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-08-15 20:24:49 -0500

    added solution for Unique Binary Search Trees II

--- programming-practice/src/main/java/challenges/leetcode/UniqueBinarySearchTreesII.java
+++ programming-practice/src/main/java/challenges/leetcode/UniqueBinarySearchTreesII.java
@@ -2,6 +2,8 @@ package challenges.leetcode;
 
 import challenges.AbstractCustomTestRunner;
 
+import java.util.*;
+
 /**
  * 95. Unique Binary Search Trees II
  * 
@@ -20,4 +22,35 @@ import challenges.AbstractCustomTestRunner;
  */
 public class UniqueBinarySearchTreesII extends AbstractCustomTestRunner {
 
+    public static class TreeNode {
+        int val;
+        TreeNode left, right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public List<TreeNode> _generateTrees(int n) {
+        if (n == 0) return Arrays.asList();
+        return generateTreesInner (new HashMap<>(), 1, n);
+    }
+
+    private List<TreeNode> generateTreesInner (Map<String, List<TreeNode>> map, int start, int end) {
+        if (map.containsKey (start + "#" + end)) return map.get (start + "#" + end);
+        List<TreeNode> ans = new ArrayList<>();
+        if (start >= end) { ans.add (start > end ? null : new TreeNode (start)); return ans; }
+
+        for (int k = start; k <= end; k ++) {
+            List<TreeNode> left = generateTreesInner (map, start, k - 1), right = generateTreesInner (map, k + 1, end);
+
+            for (TreeNode l : left) {
+                for (TreeNode r : right) {
+                    TreeNode n = new TreeNode (k);
+                    n.left = l; n.right = r;
+                    ans.add (n);
+                }
+            }
+        }
+
+        map.put (start + "#" + end, ans);
+        return ans;
+    }
 }
