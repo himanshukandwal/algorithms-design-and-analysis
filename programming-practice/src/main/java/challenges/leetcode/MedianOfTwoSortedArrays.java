c2b96f9668b4adfcb0470f53c03e07dca7b6291d
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-08-20 07:52:22 -0500

    added solution for Median of Two Sorted Arrays

--- programming-practice/src/main/java/challenges/leetcode/MedianOfTwoSortedArrays.java
+++ programming-practice/src/main/java/challenges/leetcode/MedianOfTwoSortedArrays.java
@@ -36,11 +36,11 @@ public class MedianOfTwoSortedArrays extends AbstractCustomTestRunner {
             int mid1 = m + n - mid2;
             
             double l1 = (mid1 == 0) ? Integer.MIN_VALUE : nums1 [(mid1 - 1)/2];
-            double l2 = (mid2 == 0) ? Integer.MIN_VALUE : nums2 [(mid2 - 1)/2];
             double r1 = (mid1 == 2 * m) ? Integer.MAX_VALUE : nums1 [mid1/2];
+            double l2 = (mid2 == 0) ? Integer.MIN_VALUE : nums2 [(mid2 - 1)/2];
             double r2 = (mid2 == 2 * n) ? Integer.MAX_VALUE : nums2 [mid2/2];
             
-            if (l1 > r2) low = mid2 + 1;		
+            if (l1 > r2) low = mid2 + 1;
             else if (l2 > r1) high = mid2 - 1;	
             else return (Math.max(l1, l2) + Math.min(r1, r2)) / 2;
         }
