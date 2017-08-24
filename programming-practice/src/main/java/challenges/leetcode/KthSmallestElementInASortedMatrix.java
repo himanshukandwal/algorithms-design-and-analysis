d08f1f350ec7076afb5a222412658a2644482862
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-08-24 08:49:31 -0500

    added solution for Kth Smallest Element in a Sorted Matrix

--- programming-practice/src/main/java/challenges/leetcode/KthSmallestElementInASortedMatrix.java
+++ programming-practice/src/main/java/challenges/leetcode/KthSmallestElementInASortedMatrix.java
@@ -32,21 +32,18 @@ import challenges.AbstractCustomTestRunner;
 public class KthSmallestElementInASortedMatrix extends AbstractCustomTestRunner {
 	
 	private static KthSmallestElementInASortedMatrix _instance = new KthSmallestElementInASortedMatrix();
-	
-	private KthSmallestElementInASortedMatrix() {}
 
 	// Binary Search method.
 	public int _kthSmallest(int[][] matrix, int k) {
-        int lo = matrix[0][0], hi = matrix[matrix.length - 1][matrix[0].length - 1] + 1;//[lo, hi]
+        int n = matrix.length, lo = matrix[0][0], hi = matrix[n - 1][n - 1] + 1;//[lo, hi]
         while (lo < hi) {
             int mid = lo + (hi - lo) / 2;
-            int count = 0,  j = matrix[0].length - 1;
-			for (int i = 0; i < matrix.length; i++) {
+            int count = 0,  j = n - 1;
+			for (int i = 0; i < n; i++) {
 				while (j >= 0 && matrix[i][j] > mid) j--;
 				count += (j + 1);
 			}
-            if (count < k) lo = mid + 1;
-            else hi = mid;
+            if (count < k) lo = mid + 1; else hi = mid;
         }
         return lo;	
     }
