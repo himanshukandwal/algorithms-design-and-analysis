7cb5db677000c904186b374e5a5a73624381553c
Himanshu Kandwal <himanshuk.19@gmail.com>
2017-12-03 11:07:53 -0800

    added solution for Implement strStr()

--- programming-practice/src/main/java/challenges/leetcode/ImplementStrStr.java
+++ programming-practice/src/main/java/challenges/leetcode/ImplementStrStr.java
@@ -10,11 +10,18 @@ import challenges.AbstractCustomTestRunner;
  * 28. Implement strStr()
  * 
  * Implement strStr().
- * 
- * Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+ *
+ * Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+ *
+ * Example 1:
+ * 		Input: haystack = "hello", needle = "ll"
+ * 		Output: 2
+ *
+ * Example 2:
+ * 		Input: haystack = "aaaaa", needle = "bba"
+ * 		Output: -1
  *  
  * @author Hxkandwal
- *
  */
 public class ImplementStrStr extends AbstractCustomTestRunner {
 
