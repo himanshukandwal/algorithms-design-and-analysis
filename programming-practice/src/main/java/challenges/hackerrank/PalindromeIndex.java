afa5b8b16cf251e5642d83f59c875dbe00999dba
Himanshu Kandwal <himanshuk.19@gmail.com>
2018-01-03 23:17:59 -0800

    added solution for Palindrome Index

--- programming-practice/src/main/java/challenges/hackerrank/PalindromeIndex.java
+++ programming-practice/src/main/java/challenges/hackerrank/PalindromeIndex.java
@@ -10,15 +10,18 @@ import challenges.AbstractCustomTestRunner;
 public class PalindromeIndex extends AbstractCustomTestRunner {
 
     public int _palindromeIndex(String s) {
-        for (int idx = 0; idx < s.length(); idx ++) if (checkPalindrome(s, idx)) return idx;
-        return -1;
+        if (checkPalindrome(s)) return -1;
+        int ret = 0, i = 0, j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt (i) != s.charAt (j)) return checkPalindrome(s.substring(0, j)) ? j : i;
+            i ++; j --;
+        }
+        return ret;
     }
 
-    public boolean checkPalindrome(String s, int idx) {
+    public boolean checkPalindrome(String s) {
         int i = 0, j = s.length() - 1;
         while (i < j) {
-            if (i == idx) i ++;
-            if (j == idx) j --;
             if (s.charAt (i) != s.charAt (j)) break;
             i ++; j --;
         }
