045035a69ff3bfe5e1641d1dad58da702583d433
Himanshu Kandwal <himanshuk.19@gmail.com>
2018-04-16 21:06:31 -0700

    added solution for Most Common Word

---
+++ programming-practice/src/main/java/challenges/leetcode/MostCommonWord.java
@@ -0,0 +1,63 @@
+package challenges.leetcode;
+
+import challenges.AbstractCustomTestRunner;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * 819. Most Common Word
+ *
+ * Given a paragraph and a list of banned words, return the most frequent word that is not in the list of banned words.  It is guaranteed there is at least one word that isn't banned, and that the answer is unique.
+ *
+ * Words in the list of banned words are given in lowercase, and free of punctuation.  Words in the paragraph are not case sensitive.  The answer is in lowercase.
+ * Example:
+ *
+ *  Input: paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
+ *         banned = ["hit"]
+ *  Output: "ball"
+ *
+ *  Explanation: "hit" occurs 3 times, but it is a banned word.
+ *               "ball" occurs twice (and no other word does), so it is the most frequent non-banned word in the paragraph.
+ *               Note that words in the paragraph are not case sensitive, that punctuation is ignored (even if adjacent to words, such as "ball,"), and that "hit" isn't the answer even though it occurs more because it is banned.
+ *
+ * Note:
+ *
+ *   1 <= paragraph.length <= 1000.
+ *   1 <= banned.length <= 100.
+ *   1 <= banned[i].length <= 10.
+ *
+ * The answer is unique, and written in lowercase (even if its occurrences in paragraph may have uppercase symbols, and even if it is a proper noun.) paragraph only consists of letters, spaces, or the punctuation symbols !?',;.
+ * Different words in paragraph are always separated by a space.
+ * There are no hyphens or hyphenated words.
+ * Words only consist of letters, never apostrophes or other punctuation symbols.
+ *
+ * @author hxkandwal
+ */
+public class MostCommonWord extends AbstractCustomTestRunner {
+
+    public String mostCommonWord(String paragraph, String[] banned) {
+        Map<String, Integer> map = new HashMap<>();
+        paragraph = paragraph.replaceAll("[^a-zA-Z ]", "").toLowerCase();
+
+        Set ban = new HashSet<>();
+        for (String b : banned) ban.add(b);
+
+        int max = 0;
+        String ans = null;
+        for (String s: paragraph.split("\\s+")) {
+            s = s.trim();
+            if (s.length() > 0 && !ban.contains(s)) {
+                map.put (s, map.getOrDefault(s, 0) + 1);
+                if(map.get(s) > max) {
+                    max = map.get(s);
+                    ans = s;
+                }
+            }
+        }
+        return ans;
+    }
+
+}
