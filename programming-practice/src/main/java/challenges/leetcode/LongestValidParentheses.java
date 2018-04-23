8b332270fa9a0f5e429b1649f828cdba01e9f056
Himanshu Kandwal <himanshuk.19@gmail.com>
2018-04-22 19:45:12 -0700

    added other solutions for Longest Valid Parentheses

--- programming-practice/src/main/java/challenges/leetcode/LongestValidParentheses.java
+++ programming-practice/src/main/java/challenges/leetcode/LongestValidParentheses.java
@@ -42,6 +42,29 @@ public class LongestValidParentheses extends AbstractCustomTestRunner {
         }
         return max;
 	}
+
+	// other implementation of above
+	public int longestValidParenthesesOtherStack(String s) {
+		if (s.length() <= 1) return 0;
+		Stack<int[]> stk = new Stack<>();
+		int max = 0;
+		for (int idx = 0; idx < s.length(); idx ++) {
+			if (s.charAt (idx) == ')') {
+				while (!stk.isEmpty() && stk.peek()[1] != 0) stk.pop();
+				if (!stk.isEmpty()) {
+					stk.peek()[1] += idx - stk.peek()[0] + 1;
+
+					// try to stitch level back. ()() case
+					if (stk.size() >=2 && stk.peek()[0] == (stk.get(stk.size() - 2)[0] + stk.get(stk.size() - 2)[1])) {
+						stk.get(stk.size() - 2)[1] += stk.peek()[1];
+						stk.pop();
+					}
+					max = Math.max(max, stk.peek()[1]);
+				}
+			} else stk.push(new int [] { idx, 0});
+		}
+		return max;
+	}
 	
 	public int _longestValidParenthesesDP(String s) {
 		int [] dp = new int [s.length()];
@@ -51,6 +74,32 @@ public class LongestValidParentheses extends AbstractCustomTestRunner {
                 max = Math.max (max, dp [idx] = (dp [idx - 1] + 2 + (idx - dp [idx - 1] - 2 >= 0 ? dp [idx - dp [idx - 1] - 2]: 0)));
         }
         return max;
+	}
+
+	// TLE (DP)
+	public int longestValidParentheses(String s) {
+		if (s.length() == 0) return 0;
+		int [][] dp = new int [s.length() + 1][s.length() + 1];
+		int max = 0;
+		for (int r = 0; r < s.length(); r ++) {
+			for (int c = r - 1; c >= 0; c --) {
+				char rch = s.charAt(r), cch = s.charAt(c);
+				if (rch == ')' && cch == '(') {
+					// expand
+					if (r - c == 1) dp [r + 1][c + 1] = 2;
+						// converge
+					else if (dp [r][c + 2] != 0) dp [r + 1][c + 1] = dp [r][c + 2] + 2;
+					else {
+						// stitch
+						for (int idx = c + 1; idx < r; idx ++)
+							if (dp [idx + 1][c + 1] != 0 && dp [r + 1][idx + 2] != 0)
+								dp [r + 1][c + 1] = Math.max (dp [r + 1][c + 1], dp [idx + 1][c + 1] + dp [r + 1][idx + 2]);
+					}
+					max = Math.max (max, dp [r + 1][c + 1]);
+				}
+			}
+		}
+		return max;
 	}
 
 	// driver method
