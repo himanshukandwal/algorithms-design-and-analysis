2dabd3950002e2029dbce5536e00c0c1ba401f71
Himanshu Kandwal <himanshuk.19@gmail.com>
2018-05-02 22:54:05 -0700

    added solution for Next Closest Time

--- programming-practice/src/main/java/challenges/leetcode/NextClosestTime.java
+++ programming-practice/src/main/java/challenges/leetcode/NextClosestTime.java
@@ -27,31 +27,29 @@ import java.util.Arrays;
 public class NextClosestTime extends AbstractCustomTestRunner {
 
     public String nextClosestTime(String time) {
-        time = time.replace(":", "");
-        int [] limits = new int [] { 2, 9, 5, 9 }; // limits
+        time = time.replace (":", "");
+        int[] limits = new int [] { 2, 9, 5, 9 };
         int[] sorted = new int [4];
-
-        for (int idx = 0, jdx = 0; idx < time.length(); idx ++) sorted [jdx ++] = (time.charAt (idx) - '0');
-        Arrays.sort (sorted);
+        for (int idx = 0; idx < time.length(); idx ++) sorted [idx] = time.charAt(idx) - '0';
+        Arrays.sort(sorted);
 
         String ans = "";
         int idx = time.length() - 1;
         for (; idx >= 0; idx --) {
             int num = time.charAt(idx) - '0';
-
             if (num == limits [idx]) continue;
 
-            int sIdx = 0;
-            while (sIdx < sorted.length) if (sorted [sIdx] <= num) sIdx ++; else break;
+            int bIdx = 0;
+            while (bIdx < sorted.length) if (sorted [bIdx] <= num) bIdx ++; else break;
 
-            if (sIdx < sorted.length && limits [idx] >= sorted [sIdx]) {
-                ans = time.substring(0, idx) + String.valueOf(sorted [sIdx]);
+            if (bIdx < sorted.length && sorted [bIdx] <= limits [idx]) {
+                ans = time.substring(0, idx) + sorted [bIdx];
                 break;
             }
         }
-        if (idx >= 1 && Integer.valueOf(ans.substring(0, 2)) > 23) { ans = ans.substring (0, 1); idx = 0; }
-        for (idx = idx + 1; idx < time.length(); idx ++) ans += String.valueOf(sorted [0]);
+
+        if (idx >= 1 && Integer.valueOf(ans.substring(0, 2)) > 23) { ans = ans.substring(0, 1); idx = 0; }
+        for (idx= idx + 1; idx < time.length(); idx ++) ans += sorted [0];
         return ans.substring(0, 2) + ":" + ans.substring(2);
     }
-
 }
