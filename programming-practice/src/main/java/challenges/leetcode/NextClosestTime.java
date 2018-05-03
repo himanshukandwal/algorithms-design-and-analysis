58e5ac57ab22ed5c37a113a7f15de6104448d6d4
Himanshu Kandwal <himanshuk.19@gmail.com>
2018-05-02 23:02:12 -0700

    added solution for Next Closest Time

--- programming-practice/src/main/java/challenges/leetcode/NextClosestTime.java
+++ programming-practice/src/main/java/challenges/leetcode/NextClosestTime.java
@@ -28,8 +28,13 @@ import java.util.Set;
  */
 public class NextClosestTime extends AbstractCustomTestRunner {
 
+    /*
+     * Simulate the clock going forward by one minute. Each time it moves forward, if all the digits are allowed, then return the current time.
+     * The natural way to represent the time is as an integer t in the range 0 <= t < 24 * 60. Then the hours are t / 60, the minutes are t % 60,
+     * and each digit of the hours and minutes can be found by hours / 10, hours % 10 etc.
+     */
     public String nextClosestTimeBetter(String time) {
-        int cur = 60 * Integer.parseInt(time.substring(0, 2));
+        int cur = 60 * Integer.parseInt(time.substring(0, 2));      // bring everything to minutes
         cur += Integer.parseInt(time.substring(3));
         Set<Integer> allowed = new HashSet<>();
         for (char c: time.toCharArray()) if (c != ':') {
@@ -37,7 +42,7 @@ public class NextClosestTime extends AbstractCustomTestRunner {
         }
 
         while (true) {
-            cur = (cur + 1) % (24 * 60);
+            cur = (cur + 1) % (24 * 60);  // mod by total number of minutes in a day
             int[] digits = new int[]{cur / 60 / 10, cur / 60 % 10, cur % 60 / 10, cur % 60 % 10};
             search : {
                 for (int d: digits) if (!allowed.contains(d)) break search;
