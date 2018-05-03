7175a1a31befbb418981427997494da540bf812b
Himanshu Kandwal <himanshuk.19@gmail.com>
2018-05-02 19:50:00 -0700

    added solution for K Empty Slots'

---
+++ programming-practice/src/main/java/challenges/leetcode/KEmptySlots.java
@@ -0,0 +1,96 @@
+package challenges.leetcode;
+
+import challenges.AbstractCustomTestRunner;
+
+/**
+ * 683. K Empty Slots
+ *
+ * There is a garden with N slots. In each slot, there is a flower. The N flowers will bloom one by one in N days. In each day,
+ * there will be exactly one flower blooming and it will be in the status of blooming since then.
+ *
+ * Given an array flowers consists of number from 1 to N. Each number in the array represents the place where the flower will open in that day.
+ *
+ * For example, flowers[i] = x means that the unique flower that blooms at day i will be at position x, where i and x will be in the range from 1 to N.
+ *
+ * Also given an integer k, you need to output in which day there exists two flowers in the status of blooming, and also the number of flowers between
+ * them is k and these flowers are not blooming.
+ *
+ * If there isn't such day, output -1.
+ *
+ * Example 1:
+ *      Input:  flowers: [1,3,2]
+ *              k: 1
+ *      Output: 2
+ *      Explanation: In the second day, the first and the third flower have become blooming.
+ *
+ * Example 2:
+ *      Input:  flowers: [1,2,3]
+ *              k: 1
+ *      Output: -1
+ *
+ * Note: The given array will be in the range [1, 20000].
+ *
+ * @author hxkandwal
+ */
+public class KEmptySlots extends AbstractCustomTestRunner {
+
+    class Node {
+        public int val;
+        Node left, right;
+        public Node (int val) { this.val = val; }
+    }
+
+    public int kEmptySlots(int[] flowers, int k) {
+        Node root = null;
+
+        for (int idx = 0; idx < flowers.length; idx ++) {
+            int bloomIdx = flowers [idx];
+            if (root == null) root = new Node (bloomIdx);
+            else {
+                int min = Integer.MIN_VALUE, max = Integer.MAX_VALUE;
+                Node n = root, p = null;
+                while (n != null) {
+                    p = n;
+                    if (bloomIdx > n.val) {
+                        min = n.val;
+                        n = n.right;
+                    } else {
+                        max = n.val;
+                        n = n.left;
+                    }
+                }
+
+                if (Math.abs (max - bloomIdx) == (k + 1) || Math.abs (min - bloomIdx) == (k + 1)) return idx + 1;
+                if (bloomIdx > p.val) p.right = new Node (bloomIdx);
+                else p.left = new Node (bloomIdx);
+            }
+        }
+        return -1;
+    }
+
+    // Better approach
+    public int kEmptySlotsBetter(int[] flowers, int k) {
+        int[] days = new int[flowers.length];
+        for (int i = 0; i < flowers.length; i++) {
+            days[flowers[i] - 1] = i + 1;
+        }
+
+        int ans = Integer.MAX_VALUE;
+        int left = 0, right = k+1;
+
+        search: while (right < days.length) {
+            for (int i = left+1; i < right; ++i) {
+                if (days[i] < days[left] || days[i] < days[right]) {
+                    left = i;
+                    right = i + k + 1;
+                    continue search;
+                }
+            }
+            ans = Math.min(ans, Math.max(days[left], days[right]));
+            left = right;
+            right = left + k + 1;
+        }
+
+        return ans < Integer.MAX_VALUE ? ans : -1;
+    }
+}
