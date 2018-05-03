9fc6854e10b773637e4f4fd2f10d551fb386c4de
Himanshu Kandwal <himanshuk.19@gmail.com>
2018-05-02 23:00:46 -0700

    added solution for Next Closest Time

--- programming-practice/src/main/java/challenges/leetcode/NextClosestTime.java
+++ programming-practice/src/main/java/challenges/leetcode/NextClosestTime.java
@@ -3,6 +3,8 @@ package challenges.leetcode;
 import challenges.AbstractCustomTestRunner;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * 681. Next Closest Time
@@ -25,6 +27,24 @@ import java.util.Arrays;
  * @author hxkandwal
  */
 public class NextClosestTime extends AbstractCustomTestRunner {
+
+    public String nextClosestTimeBetter(String time) {
+        int cur = 60 * Integer.parseInt(time.substring(0, 2));
+        cur += Integer.parseInt(time.substring(3));
+        Set<Integer> allowed = new HashSet<>();
+        for (char c: time.toCharArray()) if (c != ':') {
+            allowed.add(c - '0');
+        }
+
+        while (true) {
+            cur = (cur + 1) % (24 * 60);
+            int[] digits = new int[]{cur / 60 / 10, cur / 60 % 10, cur % 60 / 10, cur % 60 % 10};
+            search : {
+                for (int d: digits) if (!allowed.contains(d)) break search;
+                return String.format("%02d:%02d", cur / 60, cur % 60);
+            }
+        }
+    }
 
     public String nextClosestTime(String time) {
         time = time.replace (":", "");
