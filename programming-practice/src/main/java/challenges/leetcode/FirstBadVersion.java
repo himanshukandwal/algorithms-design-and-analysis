1e1bb32b4d556ca48c4bcb08e6530e90f2656e69
Himanshu Kandwal <himanshuk.19@gmail.com>
2018-05-28 18:35:47 -0700

    added solution for First Bad Version

--- programming-practice/src/main/java/challenges/leetcode/FirstBadVersion.java
+++ programming-practice/src/main/java/challenges/leetcode/FirstBadVersion.java
@@ -17,15 +17,15 @@ package challenges.leetcode;
  *
  */
 public class FirstBadVersion {
-    
-    public int findFirstBadVersion2(int n) {
-    	 int start = 1, end = n;
-         while (start < end) {
-             int mid = start + (end-start) / 2;
-             if (isBadVersion (mid)) end = mid;
-             else start = mid + 1;
-         }
-         return start;
+
+    public int _firstBadVersion(int n) {
+        int l = 1, r = n;
+        while (l < r) {
+            int m = l + (r - l)/2;
+            if (isBadVersion(m)) r = m;
+            else l = m + 1;
+        }
+        return l;
     }
     
     // mocked API method.
