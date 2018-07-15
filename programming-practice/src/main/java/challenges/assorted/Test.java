81eda0d438c62880e46b2de370de8fc545446a13
Himanshu Kandwal <himanshuk.19@gmail.com>
2018-07-15 13:40:39 -0700

    added sample tests

--- programming-practice/src/main/java/challenges/assorted/Test.java
+++ programming-practice/src/main/java/challenges/assorted/Test.java
@@ -47,7 +47,12 @@ public class Test {
 //													new UserSession(4, 7), new UserSession(5, 8), new UserSession(2, 3))));
 
 //		System.out.print(t.reorder(5, Arrays.asList("a1 9 2 3 1", "g1 act car", "zo4 4 7", "ab1 off key dog", "a8 act zoo")));
-		System.out.print(t.reorder(Arrays.asList("a1 9 2 3 1", "g0 act car a", "g0 act car", "g2 act car", "go1 act car", "zo4 4 7", "ab1 off key dog", "a8 act zoo")));
+//		System.out.print(t.reorder(Arrays.asList("a1 9 2 3 1", "g0 act car a", "g0 act car", "g2 act car", "go1 act car", "zo4 4 7", "ab1 off key dog", "a8 act zoo")));
+
+//		System.out.println(Arrays.toString(counts(new int [] { 1, 2, 3 }, new int [] { 2, 4 })));
+//		System.out.println(Arrays.toString(counts(new int [] { 1, 4, 2, 4 }, new int [] { 3, 5 })));
+
+		System.out.println(t.longestChain(new String[] { "a", "b", "ba", "bca", "bda", "bdca" }));
 	}
 
 	public List<String> reorder(List<String> lines) {
@@ -64,4 +69,41 @@ public class Test {
 		return reorderedLines;
 	}
 
+	// ------------------------------------------ test 2 ------------------------------------------ //
+
+	static int[] counts(int[] nums, int[] maxes) {
+		Arrays.sort(nums);
+		int[] ans = new int [maxes.length];
+
+		for (int idx = 0; idx < maxes.length; idx ++) {
+			int jdx = Arrays.binarySearch(nums, maxes [idx]);
+			if (jdx < 0) jdx = - (jdx + 1) - 1;
+			ans [idx] = jdx + 1;
+		}
+		return ans;
+	}
+
+	int longestChain(String words[]) {
+		int max = 0;
+		Set<String> set = new HashSet<>();
+		Map<String, Integer> dp = new HashMap<>();
+		for (String w : words) set.add(w);
+		for (String w : words) {
+			max = Math.max(max, dfs(dp, set, w, 0));
+		}
+		return max;
+	}
+
+	private int dfs (Map<String, Integer> dp, Set<String> set, String w, int len) {
+		if (dp.containsKey(w)) return len + dp.get(w);
+		if (w.length() == 0 || !set.contains(w)) return len;
+		len ++;
+		int gmax = 0;
+		for (int idx = 0; idx < w.length(); idx ++) {
+			int lmax = dfs (dp, set, w.substring(0, idx) + w.substring(idx + 1), len);
+			gmax = Math.max(gmax, lmax);
+		}
+		dp.put(w, gmax);
+		return gmax;
+	}
 }
