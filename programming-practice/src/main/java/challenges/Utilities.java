b73e6e6a3a95ca5d887384eb02bd060e2d73a624
Himanshu Kandwal <himanshuk.19@gmail.com>
2018-11-11 19:37:31 -0800

    added SegmentTreeRMQ util

--- programming-practice/src/main/java/challenges/Utilities.java
+++ programming-practice/src/main/java/challenges/Utilities.java
@@ -1,5 +1,6 @@
 package challenges;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Stack;
@@ -68,6 +69,97 @@ public class Utilities {
 		centerNode.setRight(makeBSTInner(array, midIdx + 1, endIdx));
 		
 		return centerNode;
+	}
+
+	public static class SegmentTreeRMQ {
+		public int M, H, N;
+		public int[] st;
+
+		public SegmentTreeRMQ(int n)
+		{
+			N = n;
+			M = Integer.highestOneBit(Math.max(N-1, 1))<<2;
+			H = M>>>1;
+			st = new int[M];
+			Arrays.fill(st, 0, M, Integer.MAX_VALUE);
+		}
+
+		public SegmentTreeRMQ(int[] a)
+		{
+			N = a.length;
+			M = Integer.highestOneBit(Math.max(N-1, 1))<<2;
+			H = M>>>1;
+			st = new int[M];
+			for(int i = 0;i < N;i++){
+				st[H+i] = a[i];
+			}
+			Arrays.fill(st, H+N, M, Integer.MAX_VALUE);
+			for(int i = H-1;i >= 1;i--)propagate(i);
+		}
+
+		public void update(int pos, int x)
+		{
+			st[H+pos] = x;
+			for(int i = (H+pos)>>>1;i >= 1;i >>>= 1)propagate(i);
+		}
+
+		private void propagate(int i)
+		{
+			st[i] = Math.min(st[2*i], st[2*i+1]);
+		}
+
+		public int min(int l, int r){ return l >= r ? 0 : min(l, r, 0, H, 1);}
+
+		private int min(int l, int r, int cl, int cr, int cur)
+		{
+			if(l <= cl && cr <= r){
+				return st[cur];
+			}else{
+				int mid = cl+cr>>>1;
+				int ret = Integer.MAX_VALUE;
+				if(cl < r && l < mid){
+					ret = Math.min(ret, min(l, r, cl, mid, 2*cur));
+				}
+				if(mid < r && l < cr){
+					ret = Math.min(ret, min(l, r, mid, cr, 2*cur+1));
+				}
+				return ret;
+			}
+		}
+
+		public int firstle(int l, int v) {
+			int cur = H+l;
+			while(true){
+				if(st[cur] <= v){
+					if(cur < H){
+						cur = 2*cur;
+					}else{
+						return cur-H;
+					}
+				}else{
+					cur++;
+					if((cur&cur-1) == 0)return -1;
+					if((cur&1)==0)cur>>>=1;
+				}
+			}
+		}
+
+		public int lastle(int l, int v) {
+			int cur = H+l;
+			while(true){
+				if(st[cur] <= v){
+					if(cur < H){
+						cur = 2*cur+1;
+					}else{
+						return cur-H;
+					}
+				}else{
+					if((cur&cur-1) == 0)return -1;
+					cur--;
+					if((cur&1)==1)cur>>>=1;
+				}
+			}
+		}
 	}
 
 }
