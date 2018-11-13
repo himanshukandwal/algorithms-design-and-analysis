0867591bf20699ac11cdd81d114c5775bb9a2fcb
Himanshu Kandwal <himanshuk.19@gmail.com>
2018-11-12 20:11:44 -0800

    added solution for Count of Smaller Numbers After Self

--- programming-practice/src/main/java/challenges/leetcode/CountOfSmallerNumbersAfterSelf.java
+++ programming-practice/src/main/java/challenges/leetcode/CountOfSmallerNumbersAfterSelf.java
@@ -1,9 +1,10 @@
 package challenges.leetcode;
 
-import java.util.LinkedList;
-import java.util.List;
-
 import challenges.AbstractCustomTestRunner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * 315. Count of Smaller Numbers After Self
@@ -27,53 +28,47 @@ import challenges.AbstractCustomTestRunner;
  */
 public class CountOfSmallerNumbersAfterSelf extends AbstractCustomTestRunner {
 
-	private static CountOfSmallerNumbersAfterSelf _instance = new CountOfSmallerNumbersAfterSelf();
-
 	public List<Integer> countSmaller(int[] nums) {
-		LinkedList<Integer> ans = new LinkedList<>();
-		if (nums.length == 0) return ans;
+		FenwickTree ft = new FenwickTree(nums.length);
+		int[] sorted = nums.clone();
+		Arrays.sort (sorted);
+		int [] index = new int [sorted.length];
+		for (int idx = 0; idx < nums.length; idx ++)
+			index [idx] = Arrays.binarySearch(sorted, nums [idx]);
 
-		int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
-		for (int num : nums) { min = Math.min(min, num); max = Math.max(max, num); }
-		
-		if (min <= 0) { 
-			min = (-min + 1);
-			for (int idx = 0; idx < nums.length; idx++) nums[idx] += min;
-			max += min;
-		}
-
-		FenwickTree ft = new FenwickTree(max);
-		for (int idx = nums.length - 1; idx >= 0; idx--) {
-			ans.addFirst(ft.sum(nums[idx] - 2));
-			ft.add(nums[idx] - 1, 1);
+		List<Integer> ans = new ArrayList<>();
+		for (int idx = nums.length - 1; idx >= 0; idx --) {
+			ft.update (index [idx], 1);
+			ans.add (0, ft.sum (index [idx] - 1));
 		}
 		return ans;
 	}
 
 	class FenwickTree {
-		private int size;
-		private int[] array;
+		int [] arr;
 
 		public FenwickTree(int size) {
-			this.size = size + 1;
-			this.array = new int[size + 1];
+			arr = new int [size + 1];
 		}
 
-		public void add(int index, int val) {
-			index++;
-			int idx = index;
-			while (idx < size) {
-				array[idx] += val;
-				idx += idx & (~idx + 1); // -x == ~x + 1
+		public void update (int idx, int value) {
+			idx ++;
+			while (idx < arr.length) {
+				arr [idx] += value;
+				idx += idx & (~idx + 1);
 			}
 		}
 
-		public int sum(int index) {
-			index++;
+		public int sum (int l, int r) {
+			return sum (r) - sum (l - 1);
+		}
+
+		public int sum (int idx) {
+			idx ++;
 			int sum = 0;
-			while (index > 0) {
-				sum += array[index];
-				index -= index & (~index + 1); // -x == ~x + 1
+			while (idx >= 1) {
+				sum += arr [idx];
+				idx -= idx & (~idx + 1);
 			}
 			return sum;
 		}
