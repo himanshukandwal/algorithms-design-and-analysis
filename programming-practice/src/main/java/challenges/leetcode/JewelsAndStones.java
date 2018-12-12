5eb53d11345a9afc200f5f7ba67f46a4e3a40e8b
Himanshu Kandwal <himanshuk.19@gmail.com>
2018-12-11 18:12:48 -0800

    added solution for Jewels and Stones

--- programming-practice/src/main/java/challenges/leetcode/JewelsAndStones.java
+++ programming-practice/src/main/java/challenges/leetcode/JewelsAndStones.java
@@ -3,7 +3,9 @@ package challenges.leetcode;
 import challenges.AbstractCustomTestRunner;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static com.google.common.truth.Truth.assertThat;
 
@@ -63,6 +65,18 @@ public class JewelsAndStones extends AbstractCustomTestRunner {
             else r = m - 1;
         }
         return (s [l] == c) ? l : -1;
+    }
+
+    public int _numJewelsInStonesQuicker(String J, String S) {
+        Set<Character> Jset = new HashSet();
+        for (char j: J.toCharArray())
+            Jset.add(j);
+
+        int ans = 0;
+        for (char s: S.toCharArray())
+            if (Jset.contains(s))
+                ans++;
+        return ans;
     }
 
     // driver method
