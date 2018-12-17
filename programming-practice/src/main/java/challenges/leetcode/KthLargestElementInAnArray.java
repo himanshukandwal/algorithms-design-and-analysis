d1a14fb22ef420c2db9046eefe4d6dc8d30bcb31
Himanshu Kandwal <himanshuk.19@gmail.com>
2018-12-16 23:48:14 -0800

    added solution for Kth Largest Element in an Array

--- programming-practice/src/main/java/challenges/leetcode/KthLargestElementInAnArray.java
+++ programming-practice/src/main/java/challenges/leetcode/KthLargestElementInAnArray.java
@@ -22,39 +22,39 @@ import challenges.AbstractCustomTestRunner;
 public class KthLargestElementInAnArray extends AbstractCustomTestRunner {
 	
 	private static KthLargestElementInAnArray _instance = new KthLargestElementInAnArray();
-	
-	public int _findKthLargest(int[] nums, int k) {
-        return find (nums, k - 1, 0, nums.length - 1);
+
+    public int _findKthLargest(int[] nums, int k) {
+        if (nums == null || nums.length == 0) return -1;
+        return findKthLargestInner(nums, k - 1, 0, nums.length - 1);
     }
-    
-    private int find (int [] nums, int k, int start, int end) {
-        if (start >= end) return nums [start];
-        
-        int r = end, low = start, high = end - 1;
-        while (low < high) {
-            if (nums [low] > nums [r]) low ++;
-            else swap (nums, low, high --);
+
+    private int findKthLargestInner(int[] nums, int k, int left, int  right) {
+        if (left >= right) return nums [left];
+        swap (nums, left + (right - left)/2, right);
+        int l = left, r = right;
+        while (l < r) {
+            while (l < r && nums [l] > nums [right]) l ++;
+            while (r > l && nums [r] <= nums [right]) r --;
+            if (l < r) {
+                swap (nums, l, r);
+                l ++; r --;
+            }
         }
-        r = low + (nums [low] > nums [r] ? 1 : 0);
-        swap (nums, r, end);
-        
-        if (r > k)  return find (nums, k, start, r - 1);
-        else if (r < k) return find (nums, k, r + 1, end);
-        else return nums [r];
+        if (nums [r] > nums [right]) r ++;
+        swap (nums, r, right);
+        if (r > k) return findKthLargestInner(nums, k, left, r - 1);
+        else if (r < k) return findKthLargestInner(nums, k, r + 1, right);
+        return nums [r];
     }
-    
-    private void swap (int [] nums, int from, int to) {
-        int num = nums [from];
+
+    private void swap(int[] nums, int from, int to) {
+        int temp = nums [from];
         nums [from] = nums [to];
-        nums [to] = num;
+        nums [to] = temp;
     }
     
    	// driver method
    	public static void main(String[] args) {
-   		_instance.runTest(new int[] { 1 }, 1, 1);
-   		_instance.runTest(new int[] { 1, 1, 2 }, 2, 1);
-   		_instance.runTest(new int[] { 2, 1 }, 2, 1);
-   		_instance.runTest(new int[] { -1, 2, 0 }, 2, 0);
 		_instance.runTest(new int[] { 3, 2, 1, 5, 6, 4 }, 2, 5);
    	}
 
