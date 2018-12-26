843f9062a0bd4994b6706821ff695227696175a7
Himanshu Kandwal <himanshuk.19@gmail.com>
2018-12-25 20:11:16 -0800

    added queries file

--- programming-practice/src/main/resources/queries.sql
+++ programming-practice/src/main/resources/queries.sql
@@ -5,3 +5,12 @@ BEGIN
     from ( select @num := @num + 1 as k, name from leaderboard, (select @num := 0) x order by score desc) lb
     where lb.k >= 4 and lb.k <= 8;
 END
+
+-- link: https://app.codesignal.com/arcade/db/always-leave-table-in-order/aQJquGtwg4rgXwfqH/description
+/*Please add ; after each select statement*/
+CREATE PROCEDURE mischievousNephews()
+BEGIN
+	select weekday(mischief_date) as weekday, mischief_date, author, title
+    from mischief
+    order by weekday(mischief_date), field(author, 'Huey', 'Dewey', 'Louie'), mischief_date, title;
+END
