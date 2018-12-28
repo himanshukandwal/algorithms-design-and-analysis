12965e6cd8b1df524d090e8ff54d5558cb6209ca
Himanshu Kandwal <himanshuk.19@gmail.com>
2018-12-28 12:11:03 -0800

    added solution for Common Character Count

--- programming-practice/src/main/java/challenges/codefights/CommonCharacterCount.java
+++ programming-practice/src/main/java/challenges/codefights/CommonCharacterCount.java
@@ -17,5 +17,14 @@ import challenges.AbstractCustomTestRunner;
  */
 public class CommonCharacterCount extends AbstractCustomTestRunner {
 
+    int commonCharacterCount(String s1, String s2) {
+        int ans = 0;
+        int[] arr1 = new int [256], arr2 = new int [256];
+
+        for (char c : s1.toCharArray()) arr1 [c] ++;
+        for (char c : s2.toCharArray()) arr2 [c] ++;
+        for (int idx = 0; idx < 256; idx ++) ans += Math.min (arr1 [idx], arr2 [idx]);
+        return ans;
+    }
 
 }
