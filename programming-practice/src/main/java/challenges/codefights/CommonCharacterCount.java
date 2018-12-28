62b4556abdd2c2546282b602cbeaf20e1dce1b70
Himanshu Kandwal <himanshuk.19@gmail.com>
2018-12-28 12:10:50 -0800

    added solution for

---
+++ programming-practice/src/main/java/challenges/codefights/CommonCharacterCount.java
@@ -0,0 +1,21 @@
+package challenges.codefights;
+
+import challenges.AbstractCustomTestRunner;
+
+/**
+ * Common Character Count
+ *
+ * Given two strings, find the number of common characters between them.
+ *
+ * Example:
+ *  For s1 = "aabcc" and s2 = "adcaa", the output should be commonCharacterCount(s1, s2) = 3.
+ *  Strings have 3 common characters - 2 "a"s and 1 "c".
+ *
+ * link: https://app.codesignal.com/arcade/intro/level-3/JKKuHJknZNj4YGL32/description
+ *
+ * @author Hxkandwal
+ */
+public class CommonCharacterCount extends AbstractCustomTestRunner {
+
+
+}
