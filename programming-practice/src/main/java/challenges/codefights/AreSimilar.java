69302129207e1feef2106b2a9eb7182e8c658702
Himanshu Kandwal <himanshuk.19@gmail.com>
2018-12-31 00:18:32 -0800

    added solution for Are Similar

--- programming-practice/src/main/java/challenges/codefights/AreSimilar.java
+++ programming-practice/src/main/java/challenges/codefights/AreSimilar.java
@@ -36,4 +36,15 @@ public class AreSimilar extends AbstractCustomTestRunner {
         return err <= 2 && last < 0;
     }
 
+    boolean areSimilarOther(int[] a, int[] b) {
+        int rs = 0, s1 = 1, s2 = 1;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != b[i]) rs++;
+            s1 *= a[i];
+            s2 *= b[i];
+        }
+        return rs <= 2 && s1 == s2;
+    }
+
+
 }
