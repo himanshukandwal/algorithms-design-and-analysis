13102bb64c26642d175ed29596b94067543208b4
Himanshu Kandwal <himanshuk.19@gmail.com>
2019-02-03 09:25:25 -0800

    added solution for Vertical Order Traversal of a Binary Tree

--- programming-practice/src/main/java/challenges/leetcode/VerticalOrderTraversalOfABinaryTree.java
+++ programming-practice/src/main/java/challenges/leetcode/VerticalOrderTraversalOfABinaryTree.java
@@ -75,4 +75,58 @@ public class VerticalOrderTraversalOfABinaryTree extends AbstractCustomTestRunne
         return ans;
     }
 
+    // better solution
+    class Location implements Comparable<Location>{
+        int x, y, val;
+        Location(int x, int y, int val) {
+            this.x = x;
+            this.y = y;
+            this.val = val;
+        }
+
+        @Override
+        public int compareTo(Location that) {
+            if (this.x != that.x)
+                return Integer.compare(this.x, that.x);
+            else if (this.y != that.y)
+                return Integer.compare(this.y, that.y);
+            else
+                return Integer.compare(this.val, that.val);
+        }
+    }
+
+    public List<List<Integer>> _verticalTraversalBetter(TreeNode root) {
+        // Each location is a node's x position, y position, and value
+        List<Location> locations = new ArrayList<>();
+
+        dfs(locations, root, 0, 0);
+        Collections.sort(locations);
+
+        List<List<Integer>> ans = new ArrayList();
+        ans.add(new ArrayList<Integer>());
+
+        int prev = locations.get(0).x;
+
+        for (Location loc: locations) {
+            // If the x value changed, it's part of a new report.
+            if (loc.x != prev) {
+                prev = loc.x;
+                ans.add(new ArrayList<Integer>());
+            }
+
+            // We always add the node's value to the latest report.
+            ans.get(ans.size() - 1).add(loc.val);
+        }
+
+        return ans;
+    }
+
+    public void dfs(List<Location> locations, TreeNode node, int x, int y) {
+        if (node != null) {
+            locations.add(new Location(x, y, node.val));
+            dfs(locations, node.left, x-1, y+1);
+            dfs(locations, node.right, x+1, y+1);
+        }
+    }
+
 }
