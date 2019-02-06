6868592aa9ecabbe7e947e1737686101e98b3bf7
Himanshu Kandwal <himanshuk.19@gmail.com>
2019-02-06 07:34:39 -0800

    added solution for Seats

---
+++ programming-practice/src/main/java/challenges/interviewbit/Seats.java
@@ -0,0 +1,87 @@
+package challenges.interviewbit;
+
+import challenges.AbstractCustomTestRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.google.common.truth.Truth.assertThat;
+
+/**
+ * Seats
+ *
+ * There is a row of seats. Assume that it contains N seats adjacent to each other. There is a group of people who are already seated in that row randomly. i.e. some are
+ * sitting together & some are scattered.
+ *
+ * An occupied seat is marked with a character 'x' and an unoccupied seat is marked with a dot ('.')
+ *
+ * Now your target is to make the whole group sit together i.e. next to each other, without having any vacant seat between them in such a way that the total number of hops
+ * or jumps to move them should be minimum.
+ *
+ * Return minimum value % MOD where MOD = 10000003
+ *
+ * Example
+ *      Here is the row having 15 seats represented by the String (0, 1, 2, 3, ......... , 14) -
+ *               . . . . x . . x x . . . x . .
+ *
+ *      Now to make them sit together one of approaches is -
+ *               . . . . . . x x x x . . . . .
+ *
+ *      Following are the steps to achieve this -
+ *          1 - Move the person sitting at 4th index to 6th index -
+ *              Number of jumps by him =   (6 - 4) = 2
+ *          2 - Bring the person sitting at 12th index to 9th index -
+ *              Number of jumps by him = (12 - 9) = 3
+ *
+ *      So now the total number of jumps made = ( 2 + 3 ) % MOD = 5 which is the minimum possible jumps to make them seat together.
+ *      There are also other ways to make them sit together but the number of jumps will exceed 5 and that will not be minimum.
+ *
+ *      For example bring them all towards the starting of the row i.e. start placing them from index 0.
+ *      In that case the total number of jumps will be ( 4 + 6 + 6 + 9 ) % MOD = 25 which is very costly and not an optimized way to do this movement
+ *
+ * link: https://www.interviewbit.com/problems/seats/
+ *
+ * @author Hxkandwal
+ */
+public class Seats extends AbstractCustomTestRunner {
+
+    private static Seats _instance = new Seats();
+
+    private static final int MOD = 10000003;
+
+    public int _seats(String A) {
+        List<Integer> pos = new ArrayList<>();
+        for (int idx = 0; idx < A.length(); idx ++)
+            if (A.charAt(idx) == 'x') pos.add (idx);
+
+        if (pos.size() <= 1) return 0;
+
+        int ans = 0, mid = pos.size()/2, mval = pos.get (mid);
+        for (int idx = mid - 1, items = 1; idx >= 0; idx --, items ++) {
+            int diff = mval - pos.get (idx) - items;
+            ans = (ans + diff) % MOD;
+        }
+
+        for (int idx = mid + 1, items = 1; idx < pos.size(); idx ++, items ++) {
+            int diff = pos.get (idx) - mval - items;
+            ans = (ans + diff) % MOD;
+        }
+
+        return ans;
+    }
+
+    // driver method
+    public static void main(String[] args) {
+        _instance.runTest("....x..xx...x..", 5);
+        _instance.runTest(".....", 0);
+    }
+
+    public void runTest(final String a, final int expectedOutput) {
+        List<Object> answers = runAll(getClass(), new Object[] { a });
+
+        for (Object answer : answers)
+            assertThat((Integer) answer).isEqualTo(expectedOutput);
+
+        System.out.println("ok!");
+    }
+}
