57744e500961bdede3c49b97929fdca0250be327
Himanshu Kandwal <himanshuk.19@gmail.com>
2019-02-16 17:53:48 -0800

    added solution for Locate Obstacle

--- programming-practice/src/main/java/challenges/assorted/LocateObstacle.java
+++ programming-practice/src/main/java/challenges/assorted/LocateObstacle.java
@@ -4,6 +4,24 @@ import java.util.*;
 
 public class LocateObstacle {
 
+    /**
+     * In this program we are visualizing the given LOT as a graph and traversing it in a breadth-first-search manner, to find the shortest path to the Obstacle.
+     * We start from the root (top-left corner) of a graph and explore all of the neighboring nodes at the current depth prior to moving on to the nodes present
+     * at the next depth level.
+     *
+     * We are using an iterative approach by using a QUEUE (First in first out) to maintain the order and depth of the traversal, and a SEEN set to check whether
+     * a node has been discovered before enqueueing it. We also check other constraints that are given to us before enqueue operation such as the location should
+     * be within the lot and should not be a trench (0).
+     *
+     * For the horizontal and vertical movements, we are employing two set of arrays (RDIR and CDIR), that help us to easily obtain the valid movement combinations
+     * : [(1, 0), (-1, 0), (0, 1), (0, -1)]
+     *
+     * During this traversal if at any point we find that the dequeued node is the location of the obstacle (9), we return the depth at which it's discovered. The
+     * depth from the root (starting point) is maintained by a variable DEPTH.
+     *
+     * Time complexity: O(numRows * numColumns)
+     * Space complexity: O(numRows * numColumns)
+     */
     public static int locateObstacle(int r, int c, List<List<Integer>> grid) {
         if (r == 0) return -1;
         int [] rdir = { 1, 0, -1, 0 };
