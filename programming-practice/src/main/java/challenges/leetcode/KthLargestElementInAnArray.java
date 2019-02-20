9f1575ab5dd783f9485a707513e6746d02138cbe
Himanshu Kandwal <himanshuk.19@gmail.com>
2019-02-19 23:31:04 -0800

    added solution for Kth Largest Element in an Array

--- programming-practice/src/main/java/challenges/leetcode/KthLargestElementInAnArray.java
+++ programming-practice/src/main/java/challenges/leetcode/KthLargestElementInAnArray.java
@@ -25,26 +25,22 @@ public class KthLargestElementInAnArray extends AbstractCustomTestRunner {
 
     public int _findKthLargest(int[] nums, int k) {
         if (nums == null || nums.length == 0) return -1;
-        return findKthLargestInner(nums, k - 1, 0, nums.length - 1);
+        return findKthLargest(nums, 0, nums.length - 1, k - 1);
     }
 
-    private int findKthLargestInner(int[] nums, int k, int left, int  right) {
-        if (left >= right) return nums [left];
-        swap (nums, left + (right - left)/2, right);
-        int l = left, r = right;
-        while (l < r) {
-            while (l < r && nums [l] > nums [right]) l ++;
-            while (r > l && nums [r] <= nums [right]) r --;
-            if (l < r) {
-                swap (nums, l, r);
-                l ++; r --;
-            }
+    private int findKthLargest(int[] nums, int i, int j, int k) {
+        if (i >= j) return nums [i];
+        int pi = j, p = nums [j];
+        int s = i, e = j;
+        while (s < e) {
+            while (s < e && nums [s] > p) s ++;
+            while (s < e && nums [e] <= p) e --;
+            if (s < e) swap (nums, s, e);
         }
-        if (nums [r] > nums [right]) r ++;
-        swap (nums, r, right);
-        if (r > k) return findKthLargestInner(nums, k, left, r - 1);
-        else if (r < k) return findKthLargestInner(nums, k, r + 1, right);
-        return nums [r];
+        swap (nums, pi, e);
+        if (e > k) return findKthLargest(nums, i, e - 1, k);
+        else if (e < k) return findKthLargest(nums, e + 1, j, k);
+        else return nums [e];
     }
 
     private void swap(int[] nums, int from, int to) {
