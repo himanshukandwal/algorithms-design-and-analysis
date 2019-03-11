2ad11cb55e8cf1dbf1931b2f84c665e04af085a6
Himanshu Kandwal <himanshuk.19@gmail.com>
2019-03-11 12:49:22 -0700

    added solution for Strobogrammatic Number II

--- programming-practice/src/main/java/challenges/leetcode/StrobogrammaticNumberII.java
+++ programming-practice/src/main/java/challenges/leetcode/StrobogrammaticNumberII.java
@@ -1,12 +1,12 @@
 package challenges.leetcode;
 
-import static com.google.common.truth.Truth.assertThat;
+import challenges.AbstractCustomTestRunner;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import challenges.AbstractCustomTestRunner;
+import static com.google.common.truth.Truth.assertThat;
 
 /**
  * 247. Strobogrammatic Number II
@@ -28,47 +28,29 @@ public class StrobogrammaticNumberII extends AbstractCustomTestRunner {
     }
     
 	// working our way outwards. Post order stitching. backtracking.
-    private List<String> dfs (int len, int n) {
-        if (len == 0) return Arrays.asList ("");
-        if (len == 1) return Arrays.asList ("0", "1", "8");
-        
-        List<String> ans = new ArrayList<>();
-        for (String num : dfs (len - 2, n)) {
-            if (len != n) ans.add ("0" + num + "0");
-            ans.add ("6" + num + "9");
-            ans.add ("9" + num + "6");
-            ans.add ("8" + num + "8");
-            ans.add ("1" + num + "1");
-        }
-        return ans;
+    public List<String> _findStrobogrammatic(int n) {
+        return dfs (n, n);
     }
 
-	public List<String> _findStrobogrammatic(int n) {
-        List<String> ans = new ArrayList<> ();
-        dfs (ans, "", n, 0);
-        return ans;
-    }
-    
-    private void dfs (List<String> ans, String build, int n, int index) {
-        if (build.length () == n) ans.add (build);
-        else {
-            if (build.length () + 2 <= n) {
-                dfs (ans, build.substring (0, index) + '6' + "" + '9' + build.substring (index), n, index + 1);
-                dfs (ans, build.substring (0, index) + '9' + "" + '6' + build.substring (index), n, index + 1);
-                if (build.length() > 0) dfs (ans, build.substring (0, index) + '0' + "" + '0' + build.substring (index), n, index + 1);
-                dfs (ans, build.substring (0, index) + '1' + "" + '1' + build.substring (index), n, index + 1);
-                dfs (ans, build.substring (0, index) + '8' + "" + '8' + build.substring (index), n, index + 1);
-            } else {
-            	dfs (ans, build.substring (0, index) + '0' + build.substring (index), n, index + 1);
-            	dfs (ans, build.substring (0, index) + '1' + build.substring (index), n, index + 1);
-            	dfs (ans, build.substring (0, index) + '8' + build.substring (index), n, index + 1);
-            }
+    private List<String> dfs (int len, int n) {
+        if (len == 0) return Arrays.asList("");
+        if (len == 1) return Arrays.asList("0", "1", "8");
+
+        List<String> ans = new ArrayList<>();
+        for (String s : dfs(len - 2, n)) {
+            if (len != n) ans.add ('0' + s + '0');
+            ans.add ('1' + s + '1');
+            ans.add ('6' + s + '9');
+            ans.add ('8' + s + '8');
+            ans.add ('9' + s + '6');
         }
+
+        return ans;
     }
     
 	// driver method
 	public static void main(String[] args) {
-		_instance.runTest(2, Arrays.asList("69", "96", "11", "88"));
+		_instance.runTest(2, Arrays.asList("11", "69", "88", "96"));
 	}
 
 	@SuppressWarnings("unchecked")
