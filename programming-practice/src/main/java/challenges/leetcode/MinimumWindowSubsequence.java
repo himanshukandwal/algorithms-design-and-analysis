87c688a4ed233e8cb9f39ce7138867608ecf5269
Himanshu Kandwal <himanshuk.19@gmail.com>
2019-03-17 17:13:38 -0700

    added solution for Minimum Window Subsequence

--- programming-practice/src/main/java/challenges/leetcode/MinimumWindowSubsequence.java
+++ programming-practice/src/main/java/challenges/leetcode/MinimumWindowSubsequence.java
@@ -30,7 +30,26 @@ public class MinimumWindowSubsequence extends AbstractCustomTestRunner {
 
     private static MinimumWindowSubsequence _instance = new MinimumWindowSubsequence();
 
-    public String _minWindow(String S, String T) {
+    // Dynamic programming solution
+    public String _minWindowDP(String S, String T) {
+        int m = S.length(), n = T.length();
+        int [][] dp = new int [n + 1][m + 1];
+        for (int idx = 0; idx <= m; idx ++) dp [0][idx] = idx + 1;
+
+        for (int r = 0; r < T.length(); r ++)
+            for (int c = r; c < S.length(); c ++)
+                dp [r + 1][c + 1] = (S.charAt(c) == T.charAt(r)) ? dp [r][c] : dp [r + 1][c];
+
+        String ans = S;
+        for (int c = 0; c <= m; c ++)
+            if (dp [n][c] > 0 && ans.length() > S.substring (dp [n][c] - 1, c).length())
+                ans = S.substring (dp [n][c] - 1, c);
+
+        return ans.equals(S) ? "" : ans;
+    }
+
+    // simple scanning approach (using the idea from IsSubsequence)
+    public String _minWindowInitial(String S, String T) {
         int tidx = 0;
         String ans = S;
         for (int idx = 0, start = 0; start < S.length(); idx ++) {
@@ -50,8 +69,34 @@ public class MinimumWindowSubsequence extends AbstractCustomTestRunner {
         return ans.equals(S) ? "" : ans;
     }
 
+    // fastest approach
+    public String _minWindowFastest(String S, String T) {
+        int i = -1;
+        String res = "";
+        while (true) {
+            for (char c: T.toCharArray()) {
+                i = S.indexOf(c, i + 1);
+                if (i == -1) return res;
+            }
+            /**
+             * this is exactly what I was thinking, run isSubsequence algo, the the last index for T in S would be good, but we are not sure about the first
+             * index in S. such as:
+             * "daabaac", abc => forward search: "aabaac", backward search: "abaac" (2nd pass)
+             * "dabbabc", abc => forward search: "abbabc", backward search: "abc"   (2nd pass)
+             *
+             * maybe we can use KMP after we are done with the two passes to save some time if we have suffix same as prefix. case: aaabaaa.
+             * */
+            int I = ++ i;
+            for (int j = T.length() - 1; j > -1; j --)
+                i = S.lastIndexOf(T.charAt(j), i - 1);
+
+            if (res.equals("") || res.length() > I - i) res = S.substring(i ++, I); // move ahead as well (like we did in our initial approach, once we match the sequence.
+        }
+    }
+
     // driver method
     public static void main(String[] args) {
+        _instance.runTest("abcdebdde","bde", "bcde");
         _instance.runTest(
                 "dlnizvdvourutyysrupkgnjfppstcriscwfbfxggepvrzqxyuniktgbpcujntijkrcbvtkupwwmsylrxwhmvxbxebrwqqyllyzhkspiywlgngbcvpdughbnidjovegnyqyhfnnqxwvgaxpgyokcgmxuloirvgwqluiffxtqtojqzmjejbwpxbeejzdhnyrliorpmjopuef",
                 "vfvyzhpnme",
