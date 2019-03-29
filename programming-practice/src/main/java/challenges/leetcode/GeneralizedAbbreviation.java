9d19e022a5fb77ac2d5791f2c0b9ddce988c25a0
Himanshu Kandwal <himanshuk.19@gmail.com>
2019-03-29 01:12:01 -0700

    added solution for Generalized Abbreviation

--- programming-practice/src/main/java/challenges/leetcode/GeneralizedAbbreviation.java
+++ programming-practice/src/main/java/challenges/leetcode/GeneralizedAbbreviation.java
@@ -13,7 +13,7 @@ import java.util.List;
  * Note: The order of the output does not matter.
  *
  * Example:
- *          Input: "word"
+ *          Input:  "word"
  *          Output: ["word", "1ord", "w1rd", "wo1d", "wor1", "2rd", "w2d", "wo2", "1o1d", "1or1", "w1r1", "1o2", "2r1", "3d", "w3", "4"]
  *
  * @author Hxkandwal
