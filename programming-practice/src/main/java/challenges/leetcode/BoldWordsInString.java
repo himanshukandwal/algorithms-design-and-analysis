d58488b94bbac867251877c099fc4fb3006ea6e9
Himanshu Kandwal <himanshuk.19@gmail.com>
2019-03-31 02:31:44 -0700

    added solution for Bold Words in String

--- programming-practice/src/main/java/challenges/leetcode/BoldWordsInString.java
+++ programming-practice/src/main/java/challenges/leetcode/BoldWordsInString.java
@@ -2,6 +2,7 @@ package challenges.leetcode;
 
 import challenges.AbstractCustomTestRunner;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 /**
@@ -74,6 +75,26 @@ public class BoldWordsInString extends AbstractCustomTestRunner {
             ans.insert(s, "<b>");
         }
         return ans.toString();
+    }
+
+    // other approach
+    public String _boldWordsOther(String[] words, String S) {
+        boolean[] bold = new boolean[S.length() + 1];
+        for (String w : words) {
+            int start = S.indexOf(w, 0);
+            while (start != -1) {
+                Arrays.fill(bold, start, start + w.length(), true);
+                start = S.indexOf(w, start + 1);
+            }
+        }
+
+        StringBuilder r = new StringBuilder().append(bold[0] ? "<b>" : "");
+        for (int i = 0; i < bold.length - 1; i++) {
+            r.append(S.charAt(i));
+            if (!bold[i] && bold[i + 1]) r.append("<b>");
+            else if (bold[i] && !bold[i + 1]) r.append("</b>");
+        }
+        return r.toString();
     }
 
 }
