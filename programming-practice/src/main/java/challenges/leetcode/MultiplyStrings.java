64d0eef2d69f5b847dae19bac98007acaad5b729
Himanshu Kandwal <himanshuk.19@gmail.com>
2019-03-30 19:39:45 -0700

    added solution for Multiply Strings

--- programming-practice/src/main/java/challenges/leetcode/MultiplyStrings.java
+++ programming-practice/src/main/java/challenges/leetcode/MultiplyStrings.java
@@ -1,10 +1,10 @@
 package challenges.leetcode;
 
-import static com.google.common.truth.Truth.assertThat;
+import challenges.AbstractCustomTestRunner;
 
 import java.util.List;
 
-import challenges.AbstractCustomTestRunner;
+import static com.google.common.truth.Truth.assertThat;
 
 /**
  * 43. Multiply Strings
@@ -24,64 +24,30 @@ public class MultiplyStrings extends AbstractCustomTestRunner {
 	
 	private static MultiplyStrings _instance = new MultiplyStrings();
 	
-    public static String _multiply(String s1, String s2) {
-    	StringBuilder ans = new StringBuilder();
-        
-        int idx2 = s2.length() - 1;
-        while (idx2 >= 0) {
-            int ch2 = s2.charAt(idx2) - '0';
-            
-            int idx1 = s1.length() - 1, localcarry = 0;
-            StringBuilder localProduct = new StringBuilder();
-            
-            while (idx1 >= 0) {
-                int ch1 = s1.charAt(idx1 --) - '0';
-                int innerProduct = ch1 * ch2 + localcarry;
-                localcarry = innerProduct / 10;
-                innerProduct %= 10;
-                localProduct.append (String.valueOf(innerProduct));
+    public static String _multiply(String num1, String num2) {
+        int m = num1.length(), n = num2.length();
+        int [] ans = new int [m + n];
+
+        for (int i = m - 1; i >= 0; i --) {
+            int x = num1.charAt(i) - '0';
+            for (int j = n - 1; j >= 0; j --) {
+                int y = num2.charAt(j) - '0';
+                int p1 = i + j, p2 = i + j + 1;
+                int sum = x * y + ans [p2];
+
+                ans [p2] = sum % 10;
+                ans [p1] += sum / 10;
             }
-            
-            if (localcarry > 0) localProduct.append (String.valueOf(localcarry));
-            
-            if (ans.length() == 0) 
-                ans.append (localProduct.toString());
-            else {
-                localcarry = 0;
-                int iidx1 = s2.length() - idx2 - 1, iidx2 = 0;
-                
-                while (iidx1 < ans.length() && iidx2 < localProduct.length()) {
-                    int cch1 = ans.charAt(iidx1 ++) - '0';
-                    int cch2 = localProduct.charAt(iidx2 ++) - '0';
-                    int sum = cch1 + cch2 + localcarry;
-                    localcarry = sum / 10;
-                    sum %= 10;
-                    ans.setCharAt (iidx1 - 1, (char) (sum + '0'));
-                }
-                
-                while (iidx2 < localProduct.length()) {
-                    int sum = localProduct.charAt(iidx2 ++) - '0' + localcarry;
-                    localcarry = sum / 10;
-                    sum %= 10;
-                    ans.append (sum);
-                }
-                
-                if (localcarry > 0) ans.append (localcarry);
-            }
-            idx2 --;
         }
-        
-        return (Long.valueOf(ans.toString()) == 0) ? "0" : ans.reverse().toString();
+
+        StringBuilder res = new StringBuilder();
+        for (int a : ans) if (res.length() > 0 || a > 0) res.append(a);
+        return res.length() == 0 ? "0" : res.toString();
     }
     
 	// driver method
 	public static void main(String[] args) {
 		_instance.runTest("1", "1", "1");
-		_instance.runTest("1", "0", "0");
-		_instance.runTest("10", "0", "0");
-		_instance.runTest("1", "2", "2");
-		_instance.runTest("2", "1", "2");
-		_instance.runTest("2", "2", "4");
 		_instance.runTest("9", "99", "891");
 		_instance.runTest("123", "456", "56088");
 		_instance.runTest("999", "999", "998001");
