91014f1c9eb906b501230c8993138bf80ea4ed2d
Himanshu Kandwal <himanshuk.19@gmail.com>
2019-04-13 19:09:30 -0700

    added solution for Global and Local Inversions

--- programming-practice/src/main/java/challenges/leetcode/GlobalAndLocalInversions.java
+++ programming-practice/src/main/java/challenges/leetcode/GlobalAndLocalInversions.java
@@ -64,4 +64,54 @@ public class GlobalAndLocalInversions extends AbstractCustomTestRunner {
         }
         return l == g;
     }
+
+    // other way using value index reverse array
+    public boolean _isIdealPermutationVIRArray(int[] A) {
+        // A local inversion is also a global inversion.
+        // Thus, we only need to check if our permutation has any one non-local inversion
+        // (A[i] > A[j], i < j) with j - i > 1.
+
+        // Fenwick tree is used to count All, however, problem here is simple to check if there is
+        // any single one. Hence, FT is an overkill here.
+
+        // value index reverse array
+        int [] arr = new int [5000];
+        for (int idx = A.length - 1; idx >= 0; idx --) {
+            int a = A [idx];
+            // can also employ range queries (mutable) if this is not good enough.
+            for (int j = a - 1; j >= 0; j --)
+                if (arr [j] > 0 && arr [j] - idx > 1) return false;
+            arr [a] = idx;
+        }
+        return true;
+    }
+
+    public boolean _isIdealPermutationFaster(int[] A) {
+        /**
+         * In performing a brute force, we repeatedly want to check whether there is some j >= i+2
+         * for which A[i] > A[j]. This is the same as checking for A[i] > min(A[i+2:]).
+         * If we knew these minimums min(A[0:]), min(A[1:]), min(A[2:]), ... we could make this
+         * check quickly.
+         */
+        int minIdx = A.length - 1;
+        for (int idx = A.length - 3; idx >= 0; idx --) {
+            if (A [idx] > A [minIdx]) return false;
+            if (A [idx + 1] < A [minIdx]) minIdx = idx + 1;
+        }
+        return true;
+    }
+
+    /**
+     * Let's try to characterize all ideal permutations: ones that do not have non-local inversions. Where does the 0 go?
+     *
+     * If the 0 occurs at index 2 or greater, then A[0] > A[2] = 0 is a non-local inversion. So 0 can only occur at index 0 or 1.
+     * If A[1] = 0, then we must have A[0] = 1 otherwise A[0] > A[j] = 1 is a non-local inversion. Otherwise, A[0] = 0.
+     * To recap, the possibilities are either A = [0] + (ideal permutation of 1...N-1) or A = [1, 0] + (ideal permutation of 2...N-1).
+     */
+    public boolean _isIdealPermutationCoolest(int[] A) {
+        for (int i = 0; i < A.length; ++ i)
+            if (Math.abs(A[i] - i) > 1)
+                return false;
+        return true;
+    }
 }
