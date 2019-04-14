0b9ac30d99ac5ed202207cf51639ea93d3491ecb
Himanshu Kandwal <himanshuk.19@gmail.com>
2019-04-14 00:34:14 -0700

    added solution for Longest Turbulent Subarray

--- programming-practice/src/main/java/challenges/leetcode/LongestTurbulentSubarray.java
+++ programming-practice/src/main/java/challenges/leetcode/LongestTurbulentSubarray.java
@@ -51,4 +51,43 @@ public class LongestTurbulentSubarray extends AbstractCustomTestRunner {
         }
         return ans;
     }
+
+    /**
+     * Evidently, we only care about the comparisons between adjacent elements. If the comparisons are represented by -1, 0, 1 (for <, =, >), then we want the longest
+     * sequence of alternating 1, -1, 1, -1, ... (starting with either 1 or -1).
+     * These alternating comparisons form contiguous blocks. We know when the next block ends: when it is the last two elements being compared, or when the sequence isn't
+     * alternating. For example, take an array like A = [9,4,2,10,7,8,8,1,9]. The comparisons are [1,1,-1,1,-1,0,-1,1]. The blocks are [1], [1,-1,1,-1], [0], [-1,1].
+     **/
+    // using java comparator
+    public int _maxTurbulenceSizeComparator(int[] A) {
+        int ans = 1;
+        for (int idx = 1, start = 0; idx < A.length; idx ++) {
+            int c = Integer.compare(A [idx - 1], A [idx]);
+            if (c == 0) start = idx;
+            else {
+                if (idx == A.length - 1 || c * Integer.compare (A [idx], A [idx + 1]) != -1) {
+                    ans = Math.max (ans, idx - start + 1);
+                    start = idx;
+                }
+            }
+        }
+        return ans;
+    }
+
+    // using two counter to keep track of local sequences.
+    public int _maxTurbulenceSizeSequences(int[] A) {
+        int inc = 1, dec = 1, ans = 1;
+        for (int idx = 1; idx < A.length; idx ++) {
+            if (A [idx - 1] > A [idx]) {
+                dec = inc + 1;              // these are the points, not the edge length
+                inc = 1;
+            }
+            else if (A [idx - 1] < A [idx]) {
+                inc = dec + 1;
+                dec = 1;
+            } else inc = dec = 1;
+            ans = Math.max (ans, Math.max (inc, dec));
+        }
+        return ans;
+    }
 }
