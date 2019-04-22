dbd35563073c9934dad0cc64d44ca403c6cdd210
Himanshu Kandwal <himanshuk.19@gmail.com>
2019-04-21 22:22:02 -0700

    added solution for Longest Substring with At Most K Distinct Characters

--- programming-practice/src/main/java/challenges/leetcode/LongestSubstringWithAtMostKDistinctCharacters.java
+++ programming-practice/src/main/java/challenges/leetcode/LongestSubstringWithAtMostKDistinctCharacters.java
@@ -13,15 +13,15 @@ import challenges.AbstractCustomTestRunner;
  */
 public class LongestSubstringWithAtMostKDistinctCharacters extends AbstractCustomTestRunner {
 
-	public int lengthOfLongestSubstringKDistinctOptimized(String s, int k) {
-        int[] arr = new int [256];
-
-        int ans = 0;
-        for (int idx = 0, start = 0, uc = 0; idx < s.length(); idx ++) {
-            char c = s.charAt(idx);
-            if (arr [c] ++ == 0) uc ++;
-            while (uc > k) if (arr [s.charAt(start ++)] -- == 1) uc --;
-            ans = Math.max (ans, idx - start + 1);
+	public int _lengthOfLongestSubstringKDistinctOptimized(String s, int k) {
+        int [] arr = new int [256];
+        int counter = 0, ans = 0;
+        for (int idx = 0, start = 0; idx < s.length(); idx ++) {
+            if (arr [s.charAt(idx)] ++ == 0) counter ++;
+            while (counter > k) {
+                if (--arr [s.charAt(start ++)] == 0) counter --;
+            }
+            ans = Math.max(ans, idx - start + 1);
         }
         return ans;
     }
