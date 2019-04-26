3c9d34d0ff84525423851c7cd409d91e792301e1
Himanshu Kandwal <himanshuk.19@gmail.com>
2019-04-25 21:02:23 -0700

    added solution for Alphabet Soup (Beginner/Novice)

---
+++ programming-practice/src/main/java/challenges/assorted/AlphabetArrange.java
@@ -0,0 +1,28 @@
+package challenges.assorted;
+
+import challenges.AbstractCustomTestRunner;
+
+import java.util.Scanner;
+
+/**
+ * Alphabet Soup (Beginner/Novice)
+ *
+ * "eLEPhAnt" -> "AEehLnPt"
+ */
+public class AlphabetArrange extends AbstractCustomTestRunner {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.next();
+
+        int [] arr = new int [256];
+        for (char c : s.toCharArray()) arr [c] ++;
+        int dis = 'a' - 'A';
+        StringBuilder ans = new StringBuilder();
+        for (char c = 'A'; c <= 'Z'; c ++) {
+            while (arr [c] -- > 0) ans.append(c);
+            while (arr [c + dis] -- > 0) ans.append((char) (c + dis));
+        }
+        System.out.println(ans.toString());
+    }
+}
